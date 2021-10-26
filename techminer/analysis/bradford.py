3e3b7e60782baadd5cfe07a1feb778c9c8ba4107
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-10-26 15:11:51 +0000

    Bradford analysis

--- techminer/analysis/bradford.py
+++ techminer/analysis/bradford.py
@@ -1,101 +1,145 @@
-from os.path import isfile
+"""
+Bradford Law's Algorithm
+
+"""
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+from techminer.utils.datastore import load_datastore
 from techminer.utils.explode import explode
 
 
-def bradford_plot(datastorepath="./", max_items=5, ax=None, cmap="Greys", **kwarg):
+class Bradford:
     """
-    Bradford Law plot.
+    Executes Braddorf analysis of core sources.
+
 
     """
 
-    if datastorepath[-1] != "/":
-        datastorepath = datastorepath + "/"
+    def __init__(self, datastorepath="./"):
+        self.datastorepath = datastorepath
+        self._core_sources = None
+        self._sources = None
 
-    datastorefile = datastorepath + "datastore.csv"
-    if isfile(datastorefile):
-        datastore = pd.read_csv(datastorefile)
-    else:
-        raise FileNotFoundError("The file {} does not exist.".format(datastorefile))
+    def _compute_core_sources(self):
+        """
+        Compute the Bradford analysis.
+        """
+        datastore = load_datastore(datastorepath=self.datastorepath)
+        datastore = datastore.assign(num_documents=1)
+        datastore = explode(
+            datastore[
+                [
+                    "publication_name",
+                    "num_documents",
+                    "global_citations",
+                    "id",
+                ]
+            ],
+            "publication_name",
+        )
+        sources = datastore.groupby("publication_name", as_index=False).agg(
+            {
+                "num_documents": np.sum,
+                "global_citations": np.sum,
+            }
+        )
+        sources["global_citations"] = sources["global_citations"].map(int)
+        sources = sources.sort_values(
+            by=["num_documents", "global_citations"], ascending=False
+        )
+        sources = sources.reset_index()
+        sources = sources.assign(cum_num_documents=sources.num_documents.cumsum())
+        self._sources = sources
+        core_documents = int(len(datastore) / 3)
+        core_sources = sources[sources.cum_num_documents <= core_documents]
+        self._core_sources = core_sources
 
-    if ax is None:
-        ax = plt.gca()
+        self._core_sources.pop("index")
+        self._sources.pop("index")
 
-    datastore["num_documents"] = 1
-    datastore = explode(
-        datastore[
-            [
-                "publication_name",
-                "num_documents",
-                "global_citations",
-                "id",
-            ]
-        ],
-        "publication_name",
-    )
-    result = datastore.groupby("publication_name", as_index=False).agg(
-        {
-            "num_documents": np.sum,
-            "global_citations": np.sum,
-        }
-    )
-    result["global_citations"] = result["global_citations"].map(lambda w: int(w))
-    result = result.sort_values(
-        by=["num_documents", "global_citations"], ascending=False
-    )
-    result = result.reset_index()
-    result["cum_num_documents"] = result.num_documents.cumsum()
+    @property
+    def sources_(self):
+        """
+        Returns the sources.
+        """
+        if self._sources is None:
+            self._compute_core_sources()
+        return self._sources
 
-    cmap = plt.cm.get_cmap(cmap)
-    color = cmap(0.6)
+    @property
+    def core_sources_(self):
+        """
+        Returns the list of core sources
+        """
+        if self._core_sources is None:
+            self._compute_core_sources()
+        return self._core_sources
 
-    out = ax.plot(
-        list(range(1, len(result) + 1)),
-        result.num_documents.tolist(),
-        linestyle="-",
-        linewidth=3,
-        color="k",
-    )
-    ax.fill_between(
-        list(range(1, len(result) + 1)),
-        result.num_documents.tolist(),
-        color=color,
-        alpha=0.6,
-    )
+    @property
+    def core_sources_names_(self):
+        """
+        Returns the list of core sources names
 
-    ##
-    ## Compute core sources
-    ##
-    core_documents = int(len(datastore) / 3)
-    core_sources = result[
-        result.cum_num_documents <= core_documents
-    ].publication_name.head(max_items)
-    core_sources = core_sources.map(lambda w: w[0:27] + " [...]" if len(w) > 30 else w)
+        """
+        return self._core_sources.publication_name
 
-    ax.fill_between(
-        [1, len(core_sources) + 1],
-        [ax.get_ylim()[1], ax.get_ylim()[1]],
-        color=color,
-        alpha=0.2,
-    )
+    def plot(self, max_items=5, cmap="Greys", figsize=(8, 6), fontsize=11):
+        """
+        Plot the Bradford analysis.
 
-    for x in ["top", "right", "left", "bottom"]:
-        ax.spines[x].set_visible(False)
+        """
+        if self.core_sources_ is None:
+            self._compute_core_sources()
 
-    ax.set_xscale("log")
+        core_sources_names = self.core_sources_names_.head(max_items)
+        core_sources_names = core_sources_names.map(
+            lambda w: w[0:27] + " [...]" if len(w) > 30 else w
+        )
 
-    ax.grid(axis="y", color="gray", linestyle=":")
-    ax.grid(axis="x", color="gray", linestyle=":")
-    ax.set_ylabel("Num Documnets")
+        matplotlib.rc("font", size=fontsize)
+        fig, axs = plt.subplots(figsize=figsize)
 
-    ax.tick_params(axis="x", labelrotation=90)
+        cmap = plt.cm.get_cmap(cmap)
+        color = cmap(0.6)
 
-    ax.set_xticks(list(range(1, len(core_sources) + 1)))
-    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax.set_xticklabels(core_sources.tolist())
+        axs.plot(
+            list(range(1, len(self.sources_) + 1)),
+            self.sources_.num_documents.tolist(),
+            linestyle="-",
+            linewidth=3,
+            color="k",
+        )
 
-    return out
+        axs.fill_between(
+            list(range(1, len(self.sources_) + 1)),
+            self.sources_.num_documents.tolist(),
+            color=color,
+            alpha=0.6,
+        )
+
+        axs.fill_between(
+            [1, len(self.core_sources_) + 1],
+            [axs.get_ylim()[1], axs.get_ylim()[1]],
+            color=color,
+            alpha=0.2,
+        )
+
+        for side in ["top", "right", "left", "bottom"]:
+            axs.spines[side].set_visible(False)
+
+        axs.set_xscale("log")
+
+        axs.grid(axis="y", color="gray", linestyle=":")
+        axs.grid(axis="x", color="gray", linestyle=":")
+        axs.set_ylabel("Num Documents")
+
+        axs.tick_params(axis="x", labelrotation=90)
+
+        axs.set_xticks(list(range(1, len(core_sources_names) + 1)))
+        axs.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+        axs.set_xticklabels(core_sources_names.tolist())
+
+        fig.set_tight_layout(True)
+        return axs
