45f4c8f34ac1109f52da1ef7573b4e9b68c88305
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-24 12:03:25 -0500

    Update impact_indicators.py

--- techminer/impact_indicators.py
+++ techminer/impact_indicators.py
@@ -7,19 +7,20 @@ Impact indicators
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/tests/data/"
 >>> impact_indicators(directory, "countries").head()
-                h_index  g_index  num_documents  global_citations  m_index  \\
-united states        23        7            187              1614     3.29   
-china                18        6            186              1159     2.25   
-united kingdom       16        6            148              1081     2.29   
-south korea          13        6             65               754     1.62   
-germany              12        5             61               626     1.71   
-.
-                global_citations_per_year  avg_global_citations  
-united states                      230.57                  8.63  
-china                              144.88                  6.23  
-united kingdom                     154.43                  7.30  
-south korea                         94.25                 11.60  
-germany                             89.43                 10.26
+            num_documents  ...  avg_global_citations
+armenia                 1  ...                  1.00
+australia              81  ...                  8.70
+austria                 9  ...                  3.00
+bahrain                31  ...                  3.42
+bangladesh              1  ...                  0.00
+<BLANKLINE>
+[5 rows x 9 columns]
+
+>>> impact_indicators(directory, "countries").columns
+Index(['num_documents', 'global_citations', 'first_pb_year', 'age', 'h_index',
+       'g_index', 'm_index', 'global_citations_per_year',
+       'avg_global_citations'],
+      dtype='object')
 
 """
 
@@ -27,7 +28,7 @@ import numpy as np
 import pandas as pd
 
 from .column_indicators import column_indicators
-from .utils import explode, load_filtered_documents, load_stopwords
+from .utils import explode, load_filtered_documents
 
 
 def impact_indicators(directory, column, sep="; "):
@@ -64,9 +65,12 @@ def impact_indicators(directory, column, sep="; "):
     columns_to_explode = [
         column,
         "global_citations",
+        "pub_year",
     ]
     detailed_citations = documents[columns_to_explode]
-    detailed_citations = explode(detailed_citations[columns_to_explode], column, sep)
+    detailed_citations[column] = detailed_citations[column].str.split(sep)
+    detailed_citations = detailed_citations.explode(column)
+    detailed_citations = detailed_citations.reset_index(drop=True)
     detailed_citations = detailed_citations.assign(
         cumcount_=detailed_citations.sort_values("global_citations", ascending=False)
         .groupby(column)
@@ -74,11 +78,30 @@ def impact_indicators(directory, column, sep="; "):
         + 1
     )
     detailed_citations = detailed_citations.sort_values(
-        [column, "global_citations", "cumcount_"], ascending=[False, False, True]
+        [column, "global_citations", "cumcount_"], ascending=[True, False, True]
     )
     detailed_citations = detailed_citations.assign(
         cumcount_2=detailed_citations.cumcount_.map(lambda w: w * w)
     )
+
+    detailed_citations["first_pb_year"] = detailed_citations.groupby(column)[
+        "pub_year"
+    ].transform("min")
+
+    detailed_citations["age"] = (
+        documents.pub_year.max() - detailed_citations["first_pb_year"] + 1
+    )
+    ages = detailed_citations.groupby(column, as_index=True).agg({"age": np.max})
+
+    global_citations = detailed_citations.groupby(column, as_index=True).agg(
+        {"global_citations": np.sum}
+    )
+
+    first_pb_year = detailed_citations.groupby(column, as_index=True).agg(
+        {"first_pb_year": np.min}
+    )
+
+    num_documents = detailed_citations.groupby(column, as_index=True).size()
 
     h_indexes = detailed_citations.query("global_citations >= cumcount_")
     h_indexes = h_indexes.groupby(column, as_index=True).agg({"cumcount_": np.max})
@@ -88,32 +111,18 @@ def impact_indicators(directory, column, sep="; "):
     g_indexes = g_indexes.groupby(column, as_index=True).agg({"cumcount_": np.max})
     g_indexes = g_indexes.rename(columns={"cumcount_": "g_index"})
 
-    age = documents[
-        [
-            column,
-            "pub_year",
-        ]
-    ]
-    age = explode(age, column, sep)
-    age = (
-        age.groupby(column, as_index=True)
-        .agg({"pub_year": np.min})
-        .rename(columns={"pub_year": "age"})
-    )
-    age = documents.pub_year.max() - age.age + 1
-
-    column_indicators_ = column_indicators(directory=directory, column=column, sep=sep)
-
-    num_documents = column_indicators_.num_documents
-    global_citations = column_indicators_.global_citations
-
     indicators = pd.concat(
-        [h_indexes, g_indexes, age, num_documents, global_citations],
+        [num_documents, global_citations, first_pb_year, ages, h_indexes, g_indexes],
         axis=1,
         sort=False,
     )
 
+    indicators = indicators.rename(columns={0: "num_documents"})
+    indicators = indicators.fillna(0)
+
     indicators = indicators.assign(m_index=indicators.h_index / indicators.age)
+    indicators["m_index"] = indicators.m_index.round(decimals=2)
+
     indicators = indicators.assign(
         global_citations_per_year=indicators.global_citations / indicators.age
     )
@@ -121,21 +130,19 @@ def impact_indicators(directory, column, sep="; "):
         avg_global_citations=indicators.global_citations / indicators.num_documents
     )
 
-    indicators = indicators.fillna(0)
-
-    indicators["m_index"] = indicators.m_index.round(decimals=2)
     indicators[
         "global_citations_per_year"
     ] = indicators.global_citations_per_year.round(decimals=2)
+
     indicators["avg_global_citations"] = indicators.avg_global_citations.round(
         decimals=2
     )
 
-    indicators = indicators.drop(columns=["age"])
-
     indicators["h_index"] = indicators["h_index"].astype(int)
     indicators["g_index"] = indicators["g_index"].astype(int)
+    indicators["age"] = indicators["age"].astype(int)
 
-    indicators = indicators.sort_values("h_index", ascending=False)
+    # indicators = indicators.sort_values("h_index", ascending=False)
+    indicators = indicators.sort_index(axis="index")
 
     return indicators
