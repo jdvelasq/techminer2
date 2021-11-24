4b040052772f45e2d3078edd868b70f48ee1245b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-23 19:27:38 -0500

    init review

--- _to_review/expand_ax_limits.py
+++
@@ -1,7 +0,0 @@
-def expand_ax_limits(ax, factor=0.15):
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
-    dx = factor * (xlim[1] - xlim[0])
-    dy = factor * (ylim[1] - ylim[0])
-    ax.set_xlim(xlim[0] - dx, xlim[1] + dx)
-    ax.set_ylim(ylim[0] - dy, ylim[1] + dy)
--- techminer/--to_check/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .adds_counters_to_axis import *
-from .explode import *
-from .io import *
-from .keywords import *
-from .map import *
-from .text import *
