e69c3cf2ef6c52be4d69c89570f8314076d24461
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-26 21:19:40 -0500

    Update load_all_documents.py

--- techminer/utils/load_all_documents.py
+++ techminer/utils/load_all_documents.py
@@ -22,7 +22,7 @@ def load_all_documents(directory):
 
     filename = os.path.join(directory, "documents.csv")
     if not os.path.isfile(filename):
-        raise FileNotFoundError(f"The file '{filename}' does not exist.")
+        raise FileNotFoundError(f"The file {filename} does not exist.")
     documents = pd.read_csv(filename, sep=",", encoding="utf-8")
 
     return documents
