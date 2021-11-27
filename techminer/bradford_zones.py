889875b85011e3ba92cb41ccb503d16fe46fdb74
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-26 22:10:45 -0500

    Update bradford_zones.py

--- techminer/bradford_zones.py
+++ techminer/bradford_zones.py
@@ -6,22 +6,22 @@ Bradford Law's zones
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/data/"
 >>> bradford_zones(directory).head()
-                                     iso_source_name  ...    zone
-0                                     SUSTAINABILITY  ...  zone 1
-1                              ADV INTELL SYS COMPUT  ...  zone 1
-2                           LECT NOTES NETWORKS SYST  ...  zone 1
-3  PERVASIVEHEALTH: PERVASIVE COMPUTING TECHNOL H...  ...  zone 1
-4                              LECT NOTES COMPUT SCI  ...  zone 1
+         iso_source_name  num_documents  ...  cum_num_documents    zone
+0         SUSTAINABILITY             15  ...                 15  zone 1
+1        FINANCIAL INNOV             11  ...                 26  zone 1
+2              ECONOMIST             11  ...                 37  zone 1
+3  LECT NOTES COMPUT SCI             10  ...                 47  zone 1
+4    J PAYM STRATEG SYST             10  ...                 57  zone 1
 <BLANKLINE>
 [5 rows x 5 columns]
 
 >>> bradford_zones(directory).tail()
-                             iso_source_name  ...    zone
-759            WIRELESS COMMUN MOBILE COMPUT  ...  zone 3
-760                     WIRELESS PERS COMMUN  ...  zone 3
-761                 WOMEN CONSUM AND PARADOX  ...  zone 3
-762                  WORLD ECONOMY INT RELAT  ...  zone 3
-763  WORLD REV ENTREP MANAGE SUSTAINABLE DEV  ...  zone 3
+                                      iso_source_name  ...    zone
+452  VIDE TEHNOLOGIJA RESURSI - ENVIRON, TECHNOL RSRC  ...  zone 3
+453                     WIRELESS COMMUN MOBILE COMPUT  ...  zone 3
+454                              WIRELESS PERS COMMUN  ...  zone 3
+455                           WORLD ECONOMY INT RELAT  ...  zone 3
+456                              WORLD SCI SER FINANC  ...  zone 3
 <BLANKLINE>
 [5 rows x 5 columns]
 
@@ -33,7 +33,7 @@ import numpy as np
 from .utils import load_filtered_documents
 
 
-def bradford_zones(directory):
+def bradford_zones(directory="./"):
 
     documents = load_filtered_documents(directory)
     documents = documents.assign(num_documents=1)
