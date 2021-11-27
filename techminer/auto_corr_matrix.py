d976d8985874d98bc97cbff2f1ab391f839ab7ae
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-26 22:10:35 -0500

    Update auto_corr_matrix.py

--- techminer/auto_corr_matrix.py
+++ techminer/auto_corr_matrix.py
@@ -6,30 +6,16 @@ Returns an auto-correlation matrix.
 
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/data/"
->>> auto_corr_matrix(directory, column='authors', min_occ=6)
-authors                 Rabbani MR  Arner DW  ...  Surjandy Schwienbacher A
-#d                              10        9   ...        6               6 
-#c                             72        154  ...       19              56 
-authors          #d #c                        ...                          
-Rabbani MR       10 72    1.000000 -0.119334  ... -0.095658       -0.095658
-Arner DW         9  154  -0.119334  1.000000  ... -0.090181       -0.090181
-Buckley RP       7  151  -0.103951  0.871096  ... -0.078556       -0.078556
-Tan B            7  113  -0.103951 -0.097998  ... -0.078556       -0.078556
-Reyes-Mercado P  7  0    -0.103951 -0.097998  ... -0.078556       -0.078556
-Gozman DP        7  91   -0.103951 -0.097998  ... -0.078556       -0.078556
-Wojcik D         7  51   -0.103951 -0.097998  ... -0.078556       -0.078556
-Khan S           6  50    0.755701 -0.090181  ... -0.072289       -0.072289
-Serrano W        6  15   -0.095658 -0.090181  ... -0.072289       -0.072289
-Fernando E       6  19   -0.095658 -0.090181  ...  1.000000       -0.072289
-Wonglimpiyarat J 6  55   -0.095658 -0.090181  ... -0.072289       -0.072289
-Ashta A          6  41   -0.095658 -0.090181  ... -0.072289       -0.072289
-Ozili PK         6  143  -0.095658 -0.090181  ... -0.072289       -0.072289
-Giudici P        6  35   -0.095658 -0.090181  ... -0.072289       -0.072289
-Zetzsche D       6  67   -0.095658  0.652974  ... -0.072289       -0.072289
-Surjandy         6  19   -0.095658 -0.090181  ...  1.000000       -0.072289
-Schwienbacher A  6  56   -0.095658 -0.090181  ... -0.072289        1.000000
-<BLANKLINE>
-[17 rows x 17 columns]
+>>> auto_corr_matrix(column='authors', min_occ=6, directory=directory)
+authors                Rabbani MR Reyes-Mercado P    Khan S  Arner DW
+#d                              8               7         6         6
+#c                            65              0         52        125
+authors         #d #c                                                
+Rabbani MR      8  65    1.000000       -0.554700  0.806226 -0.496139
+Reyes-Mercado P 7  0    -0.554700        1.000000 -0.447214 -0.447214
+Khan S          6  52    0.806226       -0.447214  1.000000 -0.400000
+Arner DW        6  125  -0.496139       -0.447214 -0.400000  1.000000
+
 
 
 """
@@ -43,13 +29,13 @@ from .tf_matrix import tf_matrix
 
 
 def auto_corr_matrix(
-    directory,
     column,
     method="pearson",
     min_occ=1,
     max_occ=None,
     scheme=None,
     sep="; ",
+    directory="./",
 ):
     """
     Returns a co-occurrence matrix.
