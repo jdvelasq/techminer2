acf0e59e2deb24404784a384fd6e29928e9fe314
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-11-26 21:19:31 -0500

    Update coverage.py

--- techminer/coverage.py
+++ techminer/coverage.py
@@ -3,45 +3,43 @@ Coverage report
 ===============================================================================
 
 >>> from techminer import *
->>> directory = "/workspaces/techminer-api/data/"
->>> coverage(directory)
+>>> coverage("/workspaces/techminer-api/data/")
                     column  number of terms coverage (%)
-0                 abstract             1649      100.00%
-1             affiliations             1605       97.33%
-2          author_keywords             1411       85.57%
-3                  authors             1627       98.67%
-4               authors_id             1627       98.67%
-5        bradford_law_zone             1649      100.00%
-6                countries             1599       96.97%
-7       country_1st_author             1599       96.97%
-8              document_id             1649      100.00%
-9           document_title             1649      100.00%
-10           document_type             1649      100.00%
-11                     doi             1374       83.32%
-12      frac_num_documents             1649      100.00%
-13        global_citations             1649      100.00%
-14       global_references             1560       94.60%
-15          index_keywords              646       39.18%
-16  institution_1st_author             1515       91.87%
-17            institutions             1515       91.87%
-18                    isbn              428       25.96%
-19         iso_source_name             1649      100.00%
-20                    issn             1350       81.87%
-21         local_citations             1649      100.00%
-22        local_references             1000       60.64%
-23             num_authors             1649      100.00%
-24   num_global_references             1649      100.00%
-25              page_start             1174       71.19%
-26                pub_year             1649      100.00%
-27               pubmed_id               11        0.67%
-28     raw_author_keywords             1411       85.57%
-29       raw_authors_names             1628       98.73%
-30      raw_index_keywords              646       39.18%
-31            raw_keywords              591       35.84%
-32               record_no             1649      100.00%
-33             source_name             1649      100.00%
-34                  volume             1190       72.16%
-
+0                 abstract              826      100.00%
+1             affiliations              795       96.25%
+2          author_keywords              620       75.06%
+3                  authors              807       97.70%
+4               authors_id              807       97.70%
+5        bradford_law_zone              826      100.00%
+6                countries              792       95.88%
+7       country_1st_author              792       95.88%
+8              document_id              826      100.00%
+9           document_title              826      100.00%
+10           document_type              826      100.00%
+11                     doi              666       80.63%
+12      frac_num_documents              826      100.00%
+13        global_citations              826      100.00%
+14       global_references              766       92.74%
+15          index_keywords              293       35.47%
+16  institution_1st_author              747       90.44%
+17            institutions              747       90.44%
+18                    isbn              211       25.54%
+19         iso_source_name              826      100.00%
+20                    issn              665       80.51%
+21         local_citations              826      100.00%
+22        local_references              555       67.19%
+23             num_authors              826      100.00%
+24   num_global_references              826      100.00%
+25              page_start              590       71.43%
+26                pub_year              826      100.00%
+27               pubmed_id                5        0.61%
+28     raw_author_keywords              620       75.06%
+29       raw_authors_names              808       97.82%
+30      raw_index_keywords              293       35.47%
+31            raw_keywords              251       30.39%
+32               record_no              826      100.00%
+33             source_name              826      100.00%
+34                  volume              577       69.85%
 
 """
 
@@ -50,7 +48,7 @@ import pandas as pd
 from .utils import load_filtered_documents
 
 
-def coverage(directory=None):
+def coverage(directory="./"):
     """
     Returns an coverage report of the dataset.
 
