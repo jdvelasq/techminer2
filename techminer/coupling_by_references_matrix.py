faff7b03581b5b804e8494857e51cc0d04446da7
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-02 08:11:06 -0500

    Update coupling_by_references_matrix.py

--- techminer/coupling_by_references_matrix.py
+++ techminer/coupling_by_references_matrix.py
@@ -4,19 +4,45 @@ Coupling by references matrix
 
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/data/"
->>> coupling_by_references_matrix(
+>>> coupling_matrix = coupling_by_references_matrix(
 ...     top_n=15,
 ...     directory=directory,
-... ).head()
-citing_id  2016-0000  2016-0001  2017-0000  ...  2019-0001  2019-0002  2019-0003
-citing_id                                   ...                                 
-2016-0000         13          0          0  ...          0          0          0
-2016-0001          0          6          0  ...          0          0          0
-2017-0000          0          0         31  ...          0          0          0
-2017-0001          0          0          0  ...          0          0          0
-2018-0000          0          0          0  ...          0          0          0
+... )
+>>> coupling_matrix.head()
+                 2016-0000 2016-0001 2017-0000  ... 2019-0001 2019-0002 2019-0003
+                       106       43        146  ...       75        60        44 
+                        14        7         15  ...        14        7         14
+2016-0000 106 14        13         0         0  ...         0         0         0
+2016-0001 43  7          0         6         0  ...         0         0         0
+2017-0000 146 15         0         0        31  ...         0         0         0
+2017-0001 101 16         0         0         0  ...         0         0         0
+2018-0000 220 32         0         0         0  ...         0         0         0
 <BLANKLINE>
 [5 rows x 15 columns]
+
+
+>>> coupling_network = co_occurrence_network(coupling_matrix)
+>>> network_communities(coupling_network)
+cluster           CLUST_0           CLUST_1  ...           CLUST_4           CLUST_5
+rn                                           ...                                    
+0        2016-0000 106:14  2018-0000 220:32  ...  2019-0001 075:14  2019-0002 060:07
+1        2018-0001 076:10  2017-0001 101:16  ...                                    
+2        2018-0003 067:06  2018-0004 063:08  ...                                    
+3        2018-0005 062:09                    ...                                    
+4        2019-0003 044:14                    ...                                    
+5        2016-0001 043:07                    ...                                    
+<BLANKLINE>
+[6 rows x 6 columns]
+
+>>> file_name = "/workspaces/techminer-api/sphinx/images/coupling_references_network.png"
+>>> network_plot(coupling_network).savefig(file_name)
+
+.. image:: images/coupling_references_network.png
+    :width: 550px
+    :align: center
+
+
+
 
 
 """
@@ -63,10 +89,34 @@ def coupling_by_references_matrix(
         matrix_in_columns.values, matrix_in_columns.transpose().values
     )
 
+    # ---< index based on citations >----------------------------------------------------
+    record_no2global_citations = dict(
+        zip(documents.record_no, documents.global_citations)
+    )
+    record_no2local_citations = dict(
+        zip(documents.record_no, documents.local_citations)
+    )
+    global_citations = [
+        record_no2global_citations[record_no] for record_no in matrix_in_columns.index
+    ]
+    local_citations = [
+        record_no2local_citations[record_no] for record_no in matrix_in_columns.index
+    ]
+    new_index = pd.MultiIndex.from_tuples(
+        [
+            (record_no, global_citation, local_citation)
+            for record_no, global_citation, local_citation in zip(
+                matrix_in_columns.index, global_citations, local_citations
+            )
+        ],
+    )
+
+    # -----------------------------------------------------------------------------------
+
     coupling_matrix = pd.DataFrame(
         matrix_values,
-        columns=matrix_in_columns.index,
-        index=matrix_in_columns.index,
+        columns=new_index,
+        index=new_index,
     )
 
     # ---< remove rows and columns with no associations >---------------------------------
