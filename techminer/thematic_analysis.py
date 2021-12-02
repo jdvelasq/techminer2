6770b0934f74523664067e8954e21b390d96ec3c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-02 14:38:21 -0500

    Update thematic_analysis.py

--- techminer/thematic_analysis.py
+++ techminer/thematic_analysis.py
@@ -25,13 +25,36 @@ Visualization with n-1 clusters
 
 
 >>> from techminer import *
+>>> from sklearn.cluster import KMeans
+>>> from sklearn.manifold import MDS
 >>> directory = "/workspaces/techminer-api/data/"
->>> tfidf_matrix = TfIdfMatrix(directory, 'author_keywords', min_occ=13)
->>> thematic_analysis(tfidf_matrix, clustering_method=KMeans(n_clusters=6), manifold_method=MDS()).themes_by_words_
->>> thematic_analysis(tfidf_matrix, clustering_method=KMeans(n_clusters=6), manifold_method=MDS()).map()
+>>> tfidf_matrix = tf_idf_matrix('author_keywords', min_occ=3, directory=directory)
+>>> thematic_analysis(
+...     tfidf_matrix, 
+...     clustering_method=KMeans(n_clusters=6, random_state=0), 
+...     manifold_method=MDS(random_state=0)
+... ).themes_by_words_
+author_keywords    fintech  ... open innovation
+#d                     139  ...             3  
+#c                    1285  ...            12  
+THEME_0           3.531425  ...        0.000000
+THEME_1           4.109095  ...        0.000000
+THEME_2          15.040339  ...        1.521395
+THEME_3          21.000000  ...        0.000000
+THEME_4           2.715340  ...        0.000000
+THEME_5           2.014328  ...        0.628626
+<BLANKLINE>
+[6 rows x 59 columns]
+
+>>> file_name = "/workspaces/techminer-api/sphinx/images/thematic_analysis_map.png"
+>>> thematic_analysis(
+...     tfidf_matrix, 
+...     clustering_method=KMeans(n_clusters=6), 
+...     manifold_method=MDS()
+... ).map().savefig(file_name)
 
 .. image:: images/thematic_analysis_map.png
-    :width: 700px
+    :width: 650px
     :align: center
 
 """
