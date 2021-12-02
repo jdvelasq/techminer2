95b672924984cb7266d7c80984ce5e37f21e8f35
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-01 23:26:15 -0500

    Create most_relevant_authors.py

---
+++ techminer/most_relevant_authors.py
@@ -0,0 +1,37 @@
+"""
+Most relevant authors
+===============================================================================
+
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/most_relevant_authors.png"
+>>> most_relevant_authors(directory=directory).savefig(file_name)
+
+.. image:: images/most_relevant_authors.png
+    :width: 500px
+    :align: center
+
+
+"""
+from .cleveland_dot_chart import cleveland_dot_chart
+from .column_indicators import column_indicators
+
+
+def most_relevant_authors(
+    top_n=20,
+    color="k",
+    figsize=(6, 6),
+    directory="./",
+):
+    indicators = column_indicators(directory=directory, column="authors").num_documents
+    indicators = indicators.sort_values(ascending=False).head(top_n)
+
+    return cleveland_dot_chart(
+        indicators,
+        figsize=figsize,
+        color=color,
+        title="Most relevant authors",
+        xlabel="Num Documents",
+        ylabel="Author",
+    )
