dd5a74cfc3714b27cf4db3d73eb35f14c1d22762
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-03 12:12:05 -0500

    Update country_scientific_production.py

--- app/country_scientific_production.py
+++ app/country_scientific_production.py
@@ -1,49 +1,49 @@
-import base64
-from io import BytesIO
+# import base64
+# from io import BytesIO
 
-from flask import render_template
+# from flask import render_template
 
-from app import app
-from techminer import *
+# from app import app
+# from techminer import *
 
-directory = "data/"
+# directory = "data/"
 
 
-@app.route("/country_scientific_production")
-def country_scientific_production():
+# @app.route("/country_scientific_production")
+# def country_scientific_production():
 
-    # -------------------------------------------------------------------------
-    # World map
-    table = column_indicators(directory, "countries")
+#     # -------------------------------------------------------------------------
+#     # World map
+#     table = column_indicators(directory, "countries")
 
-    fig = world_map(
-        table.num_documents,
-        cmap="Pastel2",
-        figsize=(14, 6),
-        title="Country scientific production",
-    )
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    worldmap_plot = base64.b64encode(buf.getbuffer()).decode("ascii")
+#     fig = world_map(
+#         table.num_documents,
+#         cmap="Pastel2",
+#         figsize=(14, 6),
+#         title="Country scientific production",
+#     )
+#     buf = BytesIO()
+#     fig.savefig(buf, format="png")
+#     worldmap_plot = base64.b64encode(buf.getbuffer()).decode("ascii")
 
-    # -------------------------------------------------------------------------
+#     # -------------------------------------------------------------------------
 
-    scientific_production_table = table.copy()
+#     scientific_production_table = table.copy()
 
-    print(scientific_production_table.to_html(header="true"))
-    html = (
-        scientific_production_table.to_html(header="true")
-        .replace('class="dataframe"', 'class="table table-striped  header-fixed"')
-        .replace('style="text-align: right;"', "")
-    )
+#     print(scientific_production_table.to_html(header="true"))
+#     html = (
+#         scientific_production_table.to_html(header="true")
+#         .replace('class="dataframe"', 'class="table table-striped  header-fixed"')
+#         .replace('style="text-align: right;"', "")
+#     )
 
-    scientific_production_table = [html]
+#     scientific_production_table = [html]
 
-    # -------------------------------------------------------------------------
+#     # -------------------------------------------------------------------------
 
-    return render_template(
-        "country_scientific_production.html",
-        title="Country",
-        worldmap_plot=worldmap_plot,
-        tables=scientific_production_table,
-    )
+#     return render_template(
+#         "country_scientific_production.html",
+#         title="Country",
+#         worldmap_plot=worldmap_plot,
+#         tables=scientific_production_table,
+#     )
