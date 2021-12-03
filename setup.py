eae4d8c44ca0d7ddf0f490448f3521d89da8938b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-03 08:10:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from setuptools.command.build_py import build_py
 
 class BuildPyCommand(build_py):
     def run(self):
+        #
         import nltk
 
-        nltk.download("stopwords")
-        nltk.download("wordnet")
-        nltk.download("averaged_perceptron_tagger")
-        nltk.download("punkt")
-
+        #
+        # nltk.download("stopwords")
+        # nltk.download("wordnet")
+        # nltk.download("averaged_perceptron_tagger")
+        # nltk.download("punkt")
+        #
         build_py.run(self)
 
 
@@ -21,29 +23,29 @@ setup(
     author="Juan D. Velasquez",
     author_email="jdvelasq@unal.edu.co",
     license="MIT",
-    url="http://github.com/jdvelasq/techminer",
+    url="http://github.com/jdvelasq/techminer-api",
     description="Tech Mining of Bibliograpy",
     long_description="Tech Mining of Bibliograpy",
     keywords="bibliograpy",
     platforms="any",
     provides=["techminer"],
     install_requires=[
-        "cdlib==0.2.5",
         "graphviz==0.10.1",
         "ipykernel==4.10.1",
-        "ipywidgets==7.6.5",
-        "matplotlib==3.1.0",
-        "networkx==2.5.1",
+        "matplotlib==3.2.2",
+        "networkx==2.6.3",
         "nltk==3.2.5",
         "numpy==1.19.5",
         "pandas==1.1.5",
-        "python-Levenshtein",
-        "pyvis",
-        "scikit-learn==0.22.2.post1",
+        "scikit-learn==1.0.1",
         "seaborn==0.11.2",
-        "squarify==0.4.3",
-        "textblob",
+        "textblob==0.15.3",
+        "python-Levenshtein==0.12.2",
         "wordcloud==1.5.0",
+        # not in colab:
+        "cdlib==0.2.5",
+        "squarify==0.4.3",
+        "python-Levenshtein==0.12.2",
     ],
     packages=[
         "techminer",
