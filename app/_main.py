88ecb1d07b789315ebf43231456f56155d1fe347
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-03 12:12:02 -0500

    Update _main.py

--- app/_main.py
+++ app/_main.py
@@ -1,23 +1,23 @@
-from flask import Flask, request, flash, render_template
+# from flask import Flask, request, flash, render_template
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "you-will-never-guess"
-
-
-@app.route("/", methods=("GET", "POST"))
-@app.route("/index", methods=("GET", "POST"))
-def index():
-
-    selected = None
-
-    if request.method == "POST":
-        selected = request.form["region"]
-        flash(selected)
-
-    return render_template("index.html")
+# app = Flask(__name__)
+# app.config["SECRET_KEY"] = "you-will-never-guess"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+# @app.route("/", methods=("GET", "POST"))
+# @app.route("/index", methods=("GET", "POST"))
+# def index():
 
-#
+#     selected = None
+
+#     if request.method == "POST":
+#         selected = request.form["region"]
+#         flash(selected)
+
+#     return render_template("index.html")
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True)
+
+# #
