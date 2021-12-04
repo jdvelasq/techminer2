42ca88a0e5ac734538a8b99ac4b5ac5099afffd9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-03 21:22:35 -0500

    Create authors_production_over_time.py

---
+++ techminer/authors_production_over_time.py
@@ -0,0 +1,30 @@
+"""
+Author's production over time
+===============================================================================
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/author_production_time.png"
+>>> authors_production_over_time(min_occ=2, directory=directory).savefig(file_name)
+
+
+.. image:: images/author_production_time.png
+    :width: 500px
+    :align: center
+
+"""
+
+
+from .annual_occurrence_matrix import annual_occurrence_matrix
+from .dotted_timeline_chart import dotted_timeline_chart
+
+
+def authors_production_over_time(
+    min_occ=10,
+    figsize=(6, 6),
+    directory="./",
+):
+    production = annual_occurrence_matrix(
+        "authors", min_occ=min_occ, directory=directory
+    )
+    return dotted_timeline_chart(production, figsize=figsize)
