60ee5de27ebcb310b0694c413bf33416bd653926
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-03 21:26:03 -0500

    Update most_relevant_institutions.py

--- techminer/most_relevant_institutions.py
+++ techminer/most_relevant_institutions.py
@@ -9,8 +9,20 @@ Most relevant institutions
 >>> most_relevant_institutions(directory=directory).savefig(file_name)
 
 .. image:: images/most_relevant_institutions.png
-    :width: 500px
+    :width: 550px
     :align: center
+
+
+>>> most_relevant_institutions(directory=directory, plot=False).head()
+                                                num_documents  ...  local_citations
+institutions                                                   ...                 
+Bina Nusantara University IDN                               6  ...                7
+University of Sydney AUS                                    5  ...               25
+Singapore Management University SGP                         5  ...               35
+Universitas Indonesia IDN                                   4  ...                5
+Poznan University of Economic and Business POL              4  ...                4
+<BLANKLINE>
+[5 rows x 3 columns]
 
 
 """
@@ -23,10 +35,14 @@ from .column_indicators import column_indicators
 def most_relevant_institutions(
     top_n=20,
     color="k",
-    figsize=(10, 6),
+    figsize=(8, 6),
     directory="./",
+    plot=True,
 ):
-    indicators = column_indicators("institutions", directory=directory).num_documents
+    indicators = column_indicators("institutions", directory=directory)
+    if plot is False:
+        return indicators
+    indicators = indicators.num_documents
     indicators = indicators.sort_values(ascending=False).head(top_n)
     return cleveland_dot_chart(
         indicators,
