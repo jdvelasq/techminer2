dce1de56638c1e1322da1c75cbca12114a61ac79
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-06 11:33:41 -0500

    Update index_keywords_co_occurrence_network.py

--- techminer/index_keywords_co_occurrence_network.py
+++ techminer/index_keywords_co_occurrence_network.py
@@ -35,7 +35,7 @@ def index_keywords_co_occurrence_network(
         column="index_keywords",
         min_occ=min_occ,
         max_occ=max_occ,
-        association=normalization,
+        normalization=normalization,
         directory=directory,
     )
 
