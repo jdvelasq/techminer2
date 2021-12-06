a098f2df636baf88e32d4f9decf7ee7607cdd846
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-06 11:33:36 -0500

    Update index_keywords_co_occurrence_communities.py

--- techminer/index_keywords_co_occurrence_communities.py
+++ techminer/index_keywords_co_occurrence_communities.py
@@ -46,7 +46,7 @@ def index_keywords_co_occurrence_communities(
         column="index_keywords",
         min_occ=min_occ,
         max_occ=max_occ,
-        association=normalization,
+        normalization=normalization,
         directory=directory,
     )
 
