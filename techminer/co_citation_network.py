fa781be82f8cea504b14bf097bb594ea5dcc93f5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-05 22:37:03 -0500

    Create co_citation_network.py

---
+++ techminer/co_citation_network.py
@@ -0,0 +1,49 @@
+"""
+Co-citation network
+===============================================================================
+
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/co_citation_network.png"
+>>> co_citation_network(directory=directory).savefig(file_name)
+
+.. image:: images/co_citation_network.png
+    :width: 600px
+    :align: center
+
+
+
+"""
+
+from .co_citation_matrix import co_citation_matrix
+from .co_occurrence_network import co_occurrence_network
+from .network_plot import network_plot
+
+
+def co_citation_network(
+    top_n=50,
+    clustering_method="louvain",
+    figsize=(8, 8),
+    k=0.2,
+    iterations=50,
+    max_labels=10,
+    directory="./",
+):
+    matrix = co_citation_matrix(
+        top_n=top_n,
+        directory=directory,
+    )
+
+    network = co_occurrence_network(
+        co_occurrence_matrix=matrix,
+        clustering_method=clustering_method,
+    )
+
+    return network_plot(
+        network,
+        figsize=figsize,
+        k=k,
+        iterations=iterations,
+        max_labels=max_labels,
+    )
