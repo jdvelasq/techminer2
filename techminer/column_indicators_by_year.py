2c646c9884dddbe70e884b6586416d7925900154
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-07 18:43:24 -0500

    Update column_indicators_by_year.py

--- techminer/column_indicators_by_year.py
+++ techminer/column_indicators_by_year.py
@@ -5,52 +5,74 @@ Column indicators by year
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/data/"
 >>> column_indicators_by_year(directory, 'authors').head(10)
-              pub_year  num_documents  global_citations  local_citations
-authors                                                                 
-Dolata M          2016              1                43                7
-Hong S            2016              1                 2                1
-Hung J-L          2016              1                24                4
-Kim K             2016              1                 2                1
-Kotarba M         2016              1                16                3
-Luo B             2016              1                24                4
-Schueffel P       2016              1               106               14
-Schwabe G         2016              1                43                7
-Zavolokina L      2016              1                43                7
-Brooks S          2017              1               146               15
+                       num_documents  ...  local_citations_per_year
+authors      pub_year                 ...                          
+Dolata M     2016                  1  ...                     1.167
+Hong S       2016                  1  ...                     0.167
+Hung J-L     2016                  1  ...                     0.667
+Kim K        2016                  1  ...                     0.167
+Kotarba M    2016                  1  ...                     0.500
+Luo B        2016                  1  ...                     0.667
+Schueffel P  2016                  1  ...                     2.333
+Schwabe G    2016                  1  ...                     1.167
+Zavolokina L 2016                  1  ...                     1.167
+Brooks S     2017                  1  ...                     3.000
+<BLANKLINE>
+[10 rows x 6 columns]
 
 """
+
+import pandas as pd
+
 from .utils import load_filtered_documents
 
 
 def column_indicators_by_year(directory=None, column="authors"):
-    """
-    Counts the number of terms by record per year.
 
-    :param dirpath_or_records: path to the directory or the records object
-    :param column: column to be used to count the terms
-    :param sep: separator to be used to split the column
-    :return: a pandas.Series with the number of terms by record.
-    """
-
-    report = load_filtered_documents(directory)
-    report = report.assign(num_documents=1)
-    report = report[
+    indicators = load_filtered_documents(directory)
+    indicators = indicators.assign(num_documents=1)
+    indicators[column] = indicators[column].str.split(";")
+    indicators = indicators.explode(column)
+    indicators[column] = indicators[column].str.strip()
+    indicators = indicators.reset_index(drop=True)
+    indicators = indicators[
         [column, "num_documents", "global_citations", "local_citations", "pub_year"]
     ].copy()
-
-    if sep is not None:
-        report[column] = report[column].str.split(sep)
-        report = report.explode(column)
-
-    report = (
-        report.groupby([column, "pub_year"], as_index=False)
+    max_pub_year = indicators.pub_year.max()
+    indicators = (
+        indicators.groupby([column, "pub_year"], as_index=False)
         .sum()
         .sort_values(by=["pub_year", column], ascending=True)
     )
-    report["num_documents"] = report.num_documents.astype(int)
-    report["global_citations"] = report.global_citations.astype(int)
-    report["local_citations"] = report.local_citations.astype(int)
-    report = report.dropna()
-    report = report.set_index(column)
+    indicators["age"] = max_pub_year - indicators.pub_year + 1
 
-    return report
+    indicators = indicators.assign(
+        global_citations_per_year=indicators.global_citations / indicators.age
+    )
+
+    indicators = indicators.assign(
+        local_citations_per_year=indicators.local_citations / indicators.age
+    )
+
+    indicators["global_citations_per_year"] = indicators[
+        "global_citations_per_year"
+    ].round(3)
+    indicators["local_citations_per_year"] = indicators[
+        "local_citations_per_year"
+    ].round(3)
+
+    indicators["num_documents"] = indicators.num_documents.astype(int)
+    indicators["global_citations"] = indicators.global_citations.astype(int)
+    indicators["local_citations"] = indicators.local_citations.astype(int)
+    indicators = indicators.dropna()
+
+    index = [
+        (name, year) for name, year in zip(indicators[column], indicators.pub_year)
+    ]
+    index = pd.MultiIndex.from_tuples(index, names=[column, "pub_year"])
+    indicators.index = index
+
+    indicators.pop(column)
+    indicators.pop("pub_year")
+
+    return indicators
