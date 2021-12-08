74e821bd76f2e7d097e7a49e3e5f67353c93a3cf
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-07 20:51:13 -0500

    Create collaboration_degree_plot.py

---
+++ techminer/collaboration_degree_plot.py
@@ -0,0 +1,37 @@
+"""
+Collaboration degree plot
+===============================================================================
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/collaboration_degree_plot.png"
+>>> collaboration_degree_plot('authors', min_occ=2, directory=directory).savefig(file_name)
+
+.. image:: images/collaboration_degree_plot.png
+    :width: 700px
+    :align: center
+
+"""
+
+from .co_occurrence_degree_plot import co_occurrence_degree_plot
+
+
+def collaboration_degree_plot(
+    column,
+    min_occ=2,
+    normalization="association",
+    clustering_method="louvain",
+    figsize=(8, 8),
+    directory="./",
+):
+    if column not in ["authors", "institutions", "countries"]:
+        raise ValueError("The column must be 'authors', 'institutions' or 'countries'")
+
+    return co_occurrence_degree_plot(
+        column=column,
+        min_occ=min_occ,
+        normalization=normalization,
+        clustering_method=clustering_method,
+        figsize=figsize,
+        directory=directory,
+    )
