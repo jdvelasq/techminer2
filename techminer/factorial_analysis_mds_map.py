288d12f58256db0a10bf4a8203bc0c55d2935469
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-09 10:27:17 -0500

    Create factorial_analysis_mds_map.py

---
+++ techminer/factorial_analysis_mds_map.py
@@ -0,0 +1,69 @@
+"""
+Factorial analysis using MDS and agglomerative clustering --- map
+===============================================================================
+
+Factorial analysis based on the clustering applyed to the dimensionality reduction 
+of the co-occurrence matrix.
+
+Based on the bibliometrix/R/conceptualStructure.R code.
+
+
+**Algorithm**
+
+1. Compute the co-occurrence matrix
+2. Apply the dimensionality reduction using MDS.
+3. Apply the clustering algorithm.
+4. Visualize the results.
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/factorial_analysis_mds_map.png"
+>>> factorial_analysis_mds_map(
+...     'author_keywords', 
+...     min_occ=2, 
+...     n_clusters=4, 
+...     directory=directory,
+... ).savefig(file_name)
+
+.. image:: images/factorial_analysis_mds_map.png
+    :width: 700px
+    :align: center
+
+"""
+
+import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.manifold import MDS
+
+from .co_occurrence_matrix import co_occurrence_matrix
+from .factorial_analysis_manifold import Factorial_analysis_manifold
+
+
+def factorial_analysis_mds_map(
+    column,
+    min_occ=2,
+    max_occ=None,
+    n_clusters=2,
+    top_n=5,
+    figsize=(8, 8),
+    random_state=0,
+    directory="./",
+):
+    coc_matrix = co_occurrence_matrix(
+        column=column,
+        min_occ=min_occ,
+        max_occ=max_occ,
+        normalization=None,
+        directory=directory,
+    )
+
+    manifold_method = MDS(n_components=2, random_state=random_state)
+    clustering_method = AgglomerativeClustering(n_clusters=n_clusters)
+
+    estimator = Factorial_analysis_manifold(
+        matrix=coc_matrix,
+        manifold_method=manifold_method,
+        clustering_method=clustering_method,
+    )
+
+    return estimator.map(top_n=top_n, figsize=figsize)
