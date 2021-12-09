c3bf657f9c243308c8ef7654363d78ebb25e70e7
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-09 10:27:42 -0500

    Update column_trends.py

--- techminer/column_trends.py
+++ techminer/column_trends.py
@@ -8,7 +8,7 @@ Column trends
 >>> column_trends('author_keywords', directory=directory).savefig(file_name)
 
 .. image:: images/column_trends.png
-    :width: 650px
+    :width: 700px
     :align: center
 
 >>> column_trends('author_keywords', directory=directory, plot=False).head()
