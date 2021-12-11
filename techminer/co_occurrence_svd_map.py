3aaa0cfb2083d7e58a48d0d9e44814132b0a15d0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-11 11:00:55 -0500

    Update co_occurrence_svd_map.py

--- techminer/co_occurrence_svd_map.py
+++ techminer/co_occurrence_svd_map.py
@@ -16,16 +16,32 @@ The plot is based on the SVD technique used in T-LAB's comparative analysis.
 >>> from techminer import *
 >>> directory = "/workspaces/techminer-api/data/"
 >>> file_name = "/workspaces/techminer-api/sphinx/images/co_occurrence_svd_map.png"
->>> matrix = co_occurrence_matrix('author_keywords', min_occ=15, directory=directory)
->>> co_occurrence_svd_map(matrix).savefig(file_name)
+>>> co_occurrence_svd_map(
+...     column='author_keywords',
+...     min_occ=5,    
+...     directory=directory,
+... ).savefig(file_name)
 
 
 .. image:: images/co_occurrence_svd_map.png
     :width: 700px
     :align: center
 
-
-
+>>> co_occurrence_svd_map(
+...     column='author_keywords',
+...     min_occ=5,    
+...     directory=directory,
+...     plot=False,
+... ).head()
+                            dim0      dim1  ...     dim18     dim19
+author_keywords                             ...                    
+fintech                 1.266169 -0.123487  ... -0.011774  0.072038
+financial technologies  0.666967 -0.342768  ... -0.274869 -0.127540
+financial inclusion     0.664505 -0.311517  ...  0.056878  0.324649
+blockchain              0.622238 -0.488122  ... -0.037378 -0.059448
+innovation              0.691683  0.476721  ... -0.222151  0.091783
+<BLANKLINE>
+[5 rows x 20 columns]
 
 """
 import matplotlib.pyplot as plt
@@ -34,21 +50,36 @@ from matplotlib.ticker import AutoMinorLocator
 from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import MDS
 
+from .co_occurrence_matrix import co_occurrence_matrix
+from .map_chart import map_chart
+
 
 def co_occurrence_svd_map(
-    matrix,
+    column,
+    min_occ=1,
     max_terms=150,
     dim_x=0,
     dim_y=1,
+    directory="./",
     figsize=(7, 7),
-    n_iter=5,
+    svd__n_iter=5,
     random_state=0,
+    plot=True,
 ):
+
+    matrix = co_occurrence_matrix(
+        column=column,
+        min_occ=min_occ,
+        normalization="salton",
+        directory=directory,
+    )
 
     max_dimensions = min(20, len(matrix.columns) - 1)
 
     decomposed_matrix = TruncatedSVD(
-        n_components=max_dimensions, n_iter=n_iter, random_state=random_state
+        n_components=max_dimensions,
+        n_iter=svd__n_iter,
+        random_state=random_state,
     ).fit_transform(matrix)
 
     if decomposed_matrix.shape[0] > max_terms:
@@ -64,44 +95,14 @@ def co_occurrence_svd_map(
         index=labels,
     )
 
-    fig = plt.Figure(figsize=figsize)
-    ax = fig.subplots()
-    ax.tick_params(axis="x", labelsize=7)
-    ax.tick_params(axis="y", labelsize=7)
+    if plot is False:
+        return decomposed_matrix
 
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    ax.tick_params(which="mayor", color="k", length=5)
-    ax.tick_params(which="minor", color="k", length=2)
-
-    x_min = decomposed_matrix[f"dim{dim_x}"].min()
-    x_max = decomposed_matrix[f"dim{dim_x}"].max()
-    y_min = decomposed_matrix[f"dim{dim_y}"].min()
-    y_max = decomposed_matrix[f"dim{dim_y}"].max()
-
-    ax.axis([x_min, x_max, y_min, y_max])
-    ax.axhline(0, color="gray", linestyle="--")
-    ax.axvline(0, color="gray", linestyle="--")
-
-    for label, values in decomposed_matrix.iterrows():
-
-        ax.text(
-            x=values[f"dim{dim_x}"],
-            y=values[f"dim{dim_y}"],
-            s=label,
-            fontsize=8,
-            horizontalalignment="center",
-            verticalalignment="center",
-            alpha=0.9,
-            weight="bold",
-        )
-
-    ax.set_xlabel(f"Dimension {dim_x}", fontsize=8)
-    ax.set_ylabel(f"Dimension {dim_y}", fontsize=8)
-
-    for side in ["top", "right", "bottom", "left"]:
-        ax.spines[side].set_visible(False)
-
-    fig.set_tight_layout(True)
-
-    return fig
+    return map_chart(
+        data=decomposed_matrix,
+        dim_x=dim_x,
+        dim_y=dim_y,
+        max_items=max_terms,
+        figsize=figsize,
+        color="k",
+    )
