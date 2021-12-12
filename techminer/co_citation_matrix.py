3d23ab954604d1d441adc7e905458c048fcb7bda
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-11 22:18:08 -0500

    Update co_citation_matrix.py

--- techminer/co_citation_matrix.py
+++ techminer/co_citation_matrix.py
@@ -18,24 +18,6 @@ Co-citation Matrix
 <BLANKLINE>
 [5 rows x 50 columns]
 
->>> co_citation_network = co_occurrence_network(matrix)
->>> network_communities(co_citation_network).head()
-cluster             CLUST_0             CLUST_1             CLUST_2
-rn                                                                 
-0        2014-0003 01606:06  1989-0000 25830:14  2016-0044 00172:07
-1        2013-0009 00553:06  2003-0002 16860:09  2017-0046 00146:15
-2        2018-0020 00220:31  2000-0000 09760:07  2018-0054 00126:14
-3        2018-0022 00213:38  2009-0013 00876:09  2018-0123 00067:08
-4        2017-0038 00181:21  2013-0015 00452:06  2018-0134 00062:08
-
->>> file_name = "/workspaces/techminer-api/sphinx/images/co_citation_network.png"
->>> network_plot(co_citation_network).savefig(file_name)
-
-.. image:: images/co_citation_network.png
-    :width: 700px
-    :align: center
-
-
 
 """
 from os.path import join
