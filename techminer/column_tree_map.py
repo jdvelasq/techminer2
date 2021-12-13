2b666e9680cd20a47d2f728f169c64d8567c86d5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-12 21:54:34 -0500

    Create column_tree_map.py

---
+++ techminer/column_tree_map.py
@@ -0,0 +1,54 @@
+"""
+Column Tree Map
+===============================================================================
+
+>>> from techminer import *
+>>> directory = "/workspaces/techminer-api/data/"
+>>> file_name = "/workspaces/techminer-api/sphinx/images/column_tree_map.png"
+>>> column_tree_map('author_keywords', 15, directory=directory).savefig(file_name)
+
+.. image:: images/column_pie_chart.png
+    :width: 700px
+    :align: center
+
+
+>>> column_tree_map('author_keywords', 15, directory=directory, plot=False).head()
+                        num_documents  global_citations  local_citations
+author_keywords                                                         
+fintech                           139              1285              218
+financial technologies             28               225               41
+financial inclusion                17               339               61
+blockchain                         17               149               22
+innovation                         13               249               46
+
+"""
+
+
+from .column_indicators import column_indicators
+from .tree_map import tree_map
+
+
+def column_tree_map(
+    column,
+    top_n=10,
+    figsize=(8, 6),
+    directory="./",
+    cmap="Blues",
+    plot=True,
+):
+
+    indicators = column_indicators(column=column, directory=directory)
+    indicators = indicators.sort_values(by="num_documents", ascending=False)
+
+    if plot is False:
+        return indicators
+
+    series = indicators.num_documents.head(top_n)
+    darkness = indicators.global_citations.head(top_n)
+
+    return tree_map(
+        series=series,
+        darkness=series,
+        cmap=cmap,
+        figsize=figsize,
+    )
