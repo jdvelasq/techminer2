c84a023af9d8e76e978ff0d24cc68b692b54f5a3
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:41:33 -0500

    Update corresponding_authors_country.py

--- techminer2/corresponding_authors_country.py
+++ techminer2/corresponding_authors_country.py
@@ -5,7 +5,7 @@ Corresponding Author's Country
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/corresponding_authors_country.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/corresponding_authors_country.png"
 >>> corresponding_authors_country(directory=directory).savefig(file_name)
 
 .. image:: images/corresponding_authors_country.png
