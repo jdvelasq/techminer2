e3343d43de52da7e0cf39254af664772e9465dee
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:41:45 -0500

    Update extract_custom_topics.py

--- techminer2/extract_custom_topics.py
+++ techminer2/extract_custom_topics.py
@@ -7,7 +7,7 @@ Extract and plot the user custom topics of the selected column.
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/extract_custom_topics.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/extract_custom_topics.png"
 >>> custom_topics = find_string(
 ...     "author_keywords", 
 ...     contains='fintech',
