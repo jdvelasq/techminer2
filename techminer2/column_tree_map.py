ed1936ffae06913b2e1c2ee062b7bcc71d7c53f9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:43:33 -0500

    Update column_tree_map.py

--- techminer2/column_tree_map.py
+++ techminer2/column_tree_map.py
@@ -4,7 +4,7 @@ Column Tree Map
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/column_tree_map.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/column_tree_map.png"
 >>> column_tree_map('author_keywords', 15, directory=directory).savefig(file_name)
 
 .. image:: images/column_tree_map.png
