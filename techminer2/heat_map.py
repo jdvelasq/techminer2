5801c8b7b554e36dd6c64ebcc096999af04134ef
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:42:02 -0500

    Update heat_map.py

--- techminer2/heat_map.py
+++ techminer2/heat_map.py
@@ -5,7 +5,7 @@ Heat Map
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
 >>> coc_matrix = co_occurrence_matrix(directory, 'authors', min_occ=5)
->>> file_name = "/workspaces/techminer-api/sphinx/images/co_occurrence_heat_map.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/co_occurrence_heat_map.png"
 >>> heat_map(coc_matrix, cmap='Blues').savefig(file_name)
 
 .. image:: images/co_occurrence_heat_map.png
