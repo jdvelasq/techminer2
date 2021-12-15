cb6252bebf645b37187adfb8b27325edd0321418
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:43:14 -0500

    Update thematic_map_network.py

--- techminer2/thematic_map_network.py
+++ techminer2/thematic_map_network.py
@@ -4,7 +4,7 @@ Thematic Map / Graph
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/thematic_map_network.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/thematic_map_network.png"
 >>> thematic_map_network('author_keywords', directory=directory).savefig(file_name)
 
 .. image:: images/thematic_map_network.png
