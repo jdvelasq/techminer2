b40db4e57754e4830149005a38c11a668677ec9b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:41:43 -0500

    Update dotted_gantt_chart.py

--- techminer2/dotted_gantt_chart.py
+++ techminer2/dotted_gantt_chart.py
@@ -5,7 +5,7 @@ Dotted Gantt Chart
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/dotted_gantt_chart.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/dotted_gantt_chart.png"
 >>> data = annual_occurrence_matrix('iso_source_name',  min_occ=8, directory=directory)
 >>> dotted_gantt_chart(data, color='grey', figsize=(8, 6)).savefig(file_name)
 
