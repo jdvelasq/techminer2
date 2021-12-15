2c48cc1e71d6b0bb67beb3c46871162e6f5e8ebd
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:39:33 -0500

    Update co_citation_network_graph.py

--- techminer2/co_citation_network_graph.py
+++ techminer2/co_citation_network_graph.py
@@ -5,7 +5,7 @@ Co-citation Network / Graph
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/co_citation_network_graph.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/co_citation_network_graph.png"
 >>> co_citation_network_graph(directory=directory).savefig(file_name)
 
 .. image:: images/co_citation_network_graph.png
