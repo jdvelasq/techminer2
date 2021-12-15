37fd4fa02f96bf55b57a23d30de4533250019c4a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-15 16:42:30 -0500

    Update most_relevant_authors.py

--- techminer2/most_relevant_authors.py
+++ techminer2/most_relevant_authors.py
@@ -4,7 +4,7 @@ Most Relevant Authors
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> file_name = "/workspaces/techminer-api/sphinx/images/most_relevant_authors.png"
+>>> file_name = "/workspaces/techminer2/sphinx/images/most_relevant_authors.png"
 >>> most_relevant_authors(directory=directory).savefig(file_name)
 
 .. image:: images/most_relevant_authors.png
