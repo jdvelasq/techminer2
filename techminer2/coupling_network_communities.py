eae23b3ade2a69951b7acffe54dbbfb0407a049b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-17 07:18:11 -0500

    Update coupling_network_communities.py

--- techminer2/coupling_network_communities.py
+++ techminer2/coupling_network_communities.py
@@ -52,28 +52,28 @@ def coupling_network_communities(
 
     # -------------------------------------------------------------------------
     # Rename axis of the matrix
-    documents = load_all_documents(directory)
-    records2ids = dict(zip(documents.record_no, documents.document_id))
-    records2global_citations = dict(
-        zip(documents.record_no, documents.global_citations)
-    )
-    records2local_citations = dict(zip(documents.record_no, documents.local_citations))
+    # documents = load_all_documents(directory)
+    # records2ids = dict(zip(documents.record_no, documents.document_id))
+    # records2global_citations = dict(
+    #     zip(documents.record_no, documents.global_citations)
+    # )
+    # records2local_citations = dict(zip(documents.record_no, documents.local_citations))
 
-    new_indexes = matrix.columns.get_level_values(0)
-    new_indexes = [
-        (
-            records2ids[index],
-            records2global_citations[index],
-            records2local_citations[index],
-        )
-        for index in new_indexes
-    ]
-    new_indexes = pd.MultiIndex.from_tuples(
-        new_indexes, names=["document", "global_citations", "local_citations"]
-    )
+    # new_indexes = matrix.columns.get_level_values(0)
+    # new_indexes = [
+    #     (
+    #         records2ids[index],
+    #         records2global_citations[index],
+    #         records2local_citations[index],
+    #     )
+    #     for index in new_indexes
+    # ]
+    # new_indexes = pd.MultiIndex.from_tuples(
+    #     new_indexes, names=["document", "global_citations", "local_citations"]
+    # )
 
-    matrix.columns = new_indexes.copy()
-    matrix.index = new_indexes.copy()
+    # matrix.columns = new_indexes.copy()
+    # matrix.index = new_indexes.copy()
 
     network_ = network(
         matrix,
