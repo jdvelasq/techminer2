0d040609eaf8abe95e613ba8727f6d82bc6de144
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-16 21:50:45 -0500

    Update __init__.py

--- techminer2/__init__.py
+++ techminer2/__init__.py
@@ -110,9 +110,9 @@ from .network_degree_plot import network_degree_plot
 from .network_indicators import network_indicators
 from .network_map import network_map
 from .network_plot import network_plot
-from .occurrence_bubble_chart import occurrence_bubble_chart
 from .occurrence_matrix import occurrence_matrix
 from .occurrence_matrix_associations import occurrence_matrix_associations
+from .occurrence_matrix_bubble_chart import occurrence_matrix_bubble_chart
 from .occurrence_matrix_heatmap import occurrence_matrix_heatmap
 from .occurrence_matrix_html import occurrence_matrix_html
 from .pie_chart import pie_chart
