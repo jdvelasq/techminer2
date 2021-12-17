43d1589252f201f078ff56e48aead396f1065d20
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-17 07:35:08 -0500

    Update co_citation_network_indicators.py

--- techminer2/co_citation_network_indicators.py
+++ techminer2/co_citation_network_indicators.py
@@ -5,13 +5,13 @@ Co-citation Network / Indicators
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
 >>> co_citation_network_indicators(directory=directory).head()
-           num_documents  global_citations  ...  closeness  pagerank
-node                                        ...                     
-1989-0000          25830                14  ...   0.700000  0.019213
-2000-0000           9760                 7  ...   0.620253  0.013109
-2003-0002          16860                 9  ...   0.628205  0.015167
-2009-0013            876                 9  ...   0.604938  0.011282
-2013-0009            553                 6  ...   0.550562  0.005362
+                                                 num_documents  ...  pagerank
+node                                                            ...          
+Allen HJ et al, 2019, GEORGE WASHINGTON LAW REV             21  ...  0.008687
+Alt R et al, 2018, ELECTRON MARK                            62  ...  0.017369
+Anagnostopoulos I et al, 2018, J ECON BUS                   76  ...  0.024708
+Anshari M et al, 2019, ENERGY PROCEDIA                      35  ...  0.014987
+Arner DW et al, 2020, EUR BUS ORG LAW REV                   26  ...  0.019136
 <BLANKLINE>
 [5 rows x 6 columns]
 
