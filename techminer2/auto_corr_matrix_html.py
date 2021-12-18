78b8924213fe3b59e379fdb546e127ee12e92bf8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-17 23:22:27 -0500

    Create auto_corr_matrix_html.py

---
+++ techminer2/auto_corr_matrix_html.py
@@ -0,0 +1,45 @@
+"""
+Auto-correlation Matrix / HTML
+===============================================================================
+
+
+
+>>> from techminer2 import *
+>>> directory = "/workspaces/techminer2/data/"
+>>> auto_corr_matrix_html(
+...     column='authors', 
+...     min_occ=6, 
+...     directory=directory,
+... ) # doctest: +ELLIPSIS
+<pandas.io.formats.style.Styler ...
+
+
+
+"""
+
+
+from .auto_corr_matrix import auto_corr_matrix
+
+
+def auto_corr_matrix_html(
+    column,
+    method="pearson",
+    min_occ=1,
+    max_occ=None,
+    scheme=None,
+    sep="; ",
+    directory="./",
+    cmap="Greys",
+):
+
+    matrix = auto_corr_matrix(
+        column=column,
+        method=method,
+        min_occ=min_occ,
+        max_occ=max_occ,
+        scheme=scheme,
+        sep=sep,
+        directory=directory,
+    )
+
+    return matrix.style.background_gradient(cmap=cmap, axis=None)
