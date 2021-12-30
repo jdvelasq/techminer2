e5f7b5ab8ba28e683a59fedd3a76f07b3e739a3f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-30 18:55:31 -0500

    Update most_relevant_sources.py

--- techminer2/most_relevant_sources.py
+++ techminer2/most_relevant_sources.py
@@ -24,7 +24,7 @@ FRONTIER ARTIF INTELL                      5                23                4
 
 
 """
-from .column_cleveland_dot_chart import column_cleveland_dot_chart
+from .topic_view_cleveland_chart import topic_view_cleveland_chart
 
 
 def most_relevant_sources(
@@ -34,7 +34,7 @@ def most_relevant_sources(
     directory="./",
     plot=True,
 ):
-    return column_cleveland_dot_chart(
+    return topic_view_cleveland_chart(
         column="iso_source_name",
         top_n=top_n,
         color=color,
