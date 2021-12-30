73546b3f13679b3de1980c2ac2d97663387f0a44
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-30 18:55:06 -0500

    Create topic_view_dotted_gantt_chart.py

---
+++ techminer2/topic_view_dotted_gantt_chart.py
@@ -0,0 +1,78 @@
+"""
+Topic View / Dotted Gantt Chart
+===============================================================================
+
+>>> from techminer2 import *
+>>> directory = "/workspaces/techminer2/data/"
+>>> file_name = "/workspaces/techminer2/sphinx/images/topic_view_dotted_gantt_chart.png"
+>>> topic_view_dotted_gantt_chart(
+...     column='author_keywords',
+...     top_n=20, 
+...     directory=directory,
+... ).savefig(file_name)
+
+.. image:: images/topic_view_dotted_gantt_chart.png
+    :width: 700px
+    :align: center
+
+>>> topic_view_dotted_gantt_chart(
+...     column='author_keywords',
+...     top_n=20, 
+...     directory=directory,
+...     plot=False,
+... ).head()
+pub_year                2016  2017  2018  2019  2020  2021
+author_keywords                                           
+fintech                    4     6    16    16    38    59
+financial technologies     1     0     6     4     5    12
+financial inclusion        0     2     0     2     3    10
+block-chain                0     2     3     4     2     6
+innovating                 2     1     3     3     3     1
+
+
+"""
+
+
+from .annual_occurrence_matrix import annual_occurrence_matrix
+from .dotted_gantt_chart import dotted_gantt_chart
+from .topic_view import topic_view
+
+
+def topic_view_dotted_gantt_chart(
+    column,
+    metric="num_documents",
+    top_n=None,
+    min_occ=1,
+    max_occ=None,
+    sort_values=None,
+    sort_index=None,
+    directory="./",
+    #
+    figsize=(6, 6),
+    plot=True,
+):
+    indicators = topic_view(
+        column=column,
+        metric=metric,
+        top_n=top_n,
+        min_occ=min_occ,
+        max_occ=max_occ,
+        sort_values=sort_values,
+        sort_index=sort_index,
+        directory=directory,
+    )
+
+    topics = indicators.index
+
+    production = annual_occurrence_matrix(
+        column,
+        min_occ=1,
+        directory=directory,
+    )
+
+    production = production.loc[topics]
+
+    if plot is False:
+        return production
+
+    return dotted_gantt_chart(production, figsize=figsize)
