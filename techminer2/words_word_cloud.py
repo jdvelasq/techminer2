0159d9789b742cee321db9c3e1de034649850924
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-30 18:55:39 -0500

    Update words_word_cloud.py

--- techminer2/words_word_cloud.py
+++ techminer2/words_word_cloud.py
@@ -12,19 +12,10 @@ Words / Word Cloud
     :align: center
 
 
->>> words_word_cloud('author_keywords', 50, directory=directory, plot=False).head()
-                        num_documents  global_citations  local_citations
-author_keywords                                                         
-fintech                           139              1285              218
-financial technologies             28               225               41
-financial inclusion                17               339               61
-block-chain                        17               149               22
-bank                               13               193               23
-
 """
 
 import os
 
-from .column_word_cloud import column_word_cloud
+from .topic_view_word_cloud import topic_view_word_cloud
 
-words_word_cloud = column_word_cloud
+words_word_cloud = topic_view_word_cloud
