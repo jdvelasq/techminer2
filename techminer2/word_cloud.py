7d5dff749d62c1c3e8a0d12061bc704416a010a2
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-12-31 11:46:09 -0500

    Create word_cloud.py

---
+++ techminer2/word_cloud.py
@@ -0,0 +1,57 @@
+"""
+Word Cloud
+===============================================================================
+
+>>> from techminer2 import *
+>>> directory = "/workspaces/techminer2/data/"
+>>> file_name = "/workspaces/techminer2/sphinx/images/word_cloud.png"
+>>> word_cloud(
+...     column='author_keywords', 
+...     top_n=50, 
+...     directory=directory,
+... ).savefig(file_name)
+
+.. image:: images/word_cloud.png
+    :width: 700px
+    :align: center
+
+
+"""
+
+from ._word_cloud import _word_cloud
+from .topic_view import topic_view
+
+
+def word_cloud(
+    column,
+    metric="num_documents",
+    top_n=None,
+    min_occ=1,
+    max_occ=None,
+    sort_values=None,
+    sort_index=None,
+    directory="./",
+    #
+    figsize=(10, 5),
+    cmap="Greys",
+):
+
+    indicators = topic_view(
+        column=column,
+        metric=metric,
+        top_n=top_n,
+        min_occ=min_occ,
+        max_occ=max_occ,
+        sort_values=sort_values,
+        sort_index=sort_index,
+        directory=directory,
+    )
+
+    indicators = indicators[metric]
+
+    return _word_cloud(
+        series=indicators,
+        darkness=indicators,
+        cmap=cmap,
+        figsize=figsize,
+    )
