1b715a71fa5b5024366f74c3c3e32636607c3b6a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-01-03 09:20:58 -0500

    Update tree_map.py

--- techminer2/tree_map.py
+++ techminer2/tree_map.py
@@ -2,63 +2,57 @@
 Tree Map
 ===============================================================================
 
-
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
 >>> file_name = "/workspaces/techminer2/sphinx/images/tree_map.png"
->>> from techminer2.indicators_api.annual_indicators import annual_indicators
->>> series = annual_indicators(directory).num_documents
->>> darkness = annual_indicators(directory).global_citations
->>> tree_map(series=series, darkness=darkness).savefig(file_name)
+>>> tree_map(
+...     'author_keywords',
+...     top_n=15, 
+...     directory=directory,
+... ).savefig(file_name)
 
 .. image:: images/tree_map.png
     :width: 700px
     :align: center
 
+
 """
 
-import textwrap
 
-import matplotlib.pyplot as plt
-import squarify
-
-TEXTLEN = 40
+from ._tree_map import _tree_map
+from .topic_view import topic_view
 
 
 def tree_map(
-    series,
-    darkness=None,
-    cmap="Greys",
-    figsize=(6, 6),
+    column,
+    metric="num_documents",
+    top_n=None,
+    min_occ=1,
+    max_occ=None,
+    sort_values=None,
+    sort_index=None,
+    directory="./",
+    #
+    figsize=(8, 6),
+    cmap="Blues",
 ):
-    """Creates a classification plot.."""
 
-    fig = plt.Figure(figsize=figsize)
-    ax = fig.subplots()
-    cmap = plt.cm.get_cmap(cmap)
-
-    labels = series.index.astype(str)
-    labels = [textwrap.shorten(text=text, width=TEXTLEN) for text in labels]
-    labels = [textwrap.wrap(text=text, width=15) for text in labels]
-    labels = ["\n".join(text) for text in labels]
-
-    colors = [
-        cmap(0.4 + 0.6 * (d - darkness.min()) / (darkness.max() - darkness.min()))
-        for d in darkness
-    ]
-
-    squarify.plot(
-        sizes=series,
-        label=labels,
-        color=colors,
-        alpha=0.9,
-        ax=ax,
-        pad=True,
-        bar_kwargs={"edgecolor": "k", "linewidth": 0.5},
-        text_kwargs={"color": "w", "fontsize": 9},
+    indicators = topic_view(
+        column=column,
+        metric=metric,
+        top_n=top_n,
+        min_occ=min_occ,
+        max_occ=max_occ,
+        sort_values=sort_values,
+        sort_index=sort_index,
+        directory=directory,
     )
-    ax.axis("off")
 
-    fig.set_tight_layout(True)
+    indicators = indicators[metric]
 
-    return fig
+    return _tree_map(
+        series=indicators,
+        darkness=indicators,
+        cmap=cmap,
+        figsize=figsize,
+    )
