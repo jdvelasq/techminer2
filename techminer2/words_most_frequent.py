83495535ff36481cb873b651287b2742df73d40b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-01-03 09:20:55 -0500

    Update words_most_frequent.py

--- techminer2/words_most_frequent.py
+++ techminer2/words_most_frequent.py
@@ -24,22 +24,6 @@ innovating                         13               249               46
 
 
 """
-from .topic_view_cleveland_chart import topic_view_cleveland_chart
+from .cleveland_chart import cleveland_chart
 
-
-def words_most_frequent(
-    column,
-    top_n=20,
-    color="k",
-    figsize=(6, 6),
-    directory="./",
-    plot=True,
-):
-    return topic_view_cleveland_chart(
-        column=column,
-        top_n=top_n,
-        color=color,
-        figsize=figsize,
-        directory=directory,
-        plot=plot,
-    )
+words_most_frequent = cleveland_chart
