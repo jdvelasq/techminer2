78aae1e808c1837b699bc8f42e65c5e3ee816810
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-01-03 09:19:47 -0500

    Create _tree_map.py

---
+++ techminer2/_tree_map.py
@@ -0,0 +1,63 @@
+"""
+Tree Map
+===============================================================================
+
+
+>>> directory = "/workspaces/techminer2/data/"
+>>> file_name = "/workspaces/techminer2/sphinx/images/_tree_map.png"
+>>> from techminer2.annual_indicators import annual_indicators
+>>> series = annual_indicators(directory).num_documents
+>>> darkness = annual_indicators(directory).global_citations
+>>> _tree_map(series=series, darkness=darkness).savefig(file_name)
+
+.. image:: images/_tree_map.png
+    :width: 700px
+    :align: center
+
+"""
+
+import textwrap
+
+import matplotlib.pyplot as plt
+import squarify
+
+TEXTLEN = 40
+
+
+def _tree_map(
+    series,
+    darkness=None,
+    cmap="Greys",
+    figsize=(6, 6),
+):
+    """Creates a classification plot.."""
+
+    fig = plt.Figure(figsize=figsize)
+    ax = fig.subplots()
+    cmap = plt.cm.get_cmap(cmap)
+
+    labels = series.index.astype(str)
+    labels = [textwrap.shorten(text=text, width=TEXTLEN) for text in labels]
+    labels = [textwrap.wrap(text=text, width=15) for text in labels]
+    labels = ["\n".join(text) for text in labels]
+
+    colors = [
+        cmap(0.4 + 0.6 * (d - darkness.min()) / (darkness.max() - darkness.min()))
+        for d in darkness
+    ]
+
+    squarify.plot(
+        sizes=series,
+        label=labels,
+        color=colors,
+        alpha=0.9,
+        ax=ax,
+        pad=True,
+        bar_kwargs={"edgecolor": "k", "linewidth": 0.5},
+        text_kwargs={"color": "w", "fontsize": 9},
+    )
+    ax.axis("off")
+
+    fig.set_tight_layout(True)
+
+    return fig
