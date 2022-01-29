db2f950160eaf8d7b545a312063fcc22e2c07ade
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-01-29 14:33:00 -0500

    Update create_keywords_thesaurus.py

--- techminer2/create_keywords_thesaurus.py
+++ techminer2/create_keywords_thesaurus.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from . import logging
 from .porter_stemmer import porter_stemmer
-from .thesaurus import Thesaurus, load_file_as_dict, text_clustering
+from .thesaurus import Thesaurus, read_textfile, text_clustering
 
 
 def create_keywords_thesaurus(directory, use_nlp_phrases=False):
@@ -15,14 +15,11 @@ def create_keywords_thesaurus(directory, use_nlp_phrases=False):
     logging.info("Creating keywords thesaurus ...")
 
     # --------------------------------------------------------------------------
-    # Loads documents.csv
     filename = os.path.join(directory, "documents.csv")
     if not os.path.isfile(filename):
         raise FileNotFoundError(f"The file '{filename}' does not exist.")
     data = pd.read_csv(filename, sep=",", encoding="utf-8")
     # --------------------------------------------------------------------------
-
-    thesaurus_file = os.path.join(directory, "keywords.txt")
 
     words_list = []
 
@@ -56,41 +53,27 @@ def create_keywords_thesaurus(directory, use_nlp_phrases=False):
         for word in words_list
     ]
 
+    # ----< new algorithm >----------------------------------------------------
+    # {palabra: clave_del_grupo}
+    new_th = text_clustering(pd.Series(words_list)).to_dict()
+
+    thesaurus_file = os.path.join(directory, "keywords.txt")
     if os.path.isfile(thesaurus_file):
+        old_th = read_textfile(thesaurus_file).to_dict()
+        new_th = {**new_th, **old_th}
 
-        #  Loads existent thesaurus
-        dict_ = load_file_as_dict(thesaurus_file)
+    keys = list(set(new_th.values()))
 
-        #  Selects the new words to cluster
-        clustered_words = [word for word in dict_.values()]
-        words_list = [word for word in words_list if word not in clustered_words]
+    th = {key: [] for key in keys}
 
-        if len(words_list) > 0:
+    for key, value in new_th.items():
+        th[value].append(key)
 
-            th = text_clustering(pd.Series(words_list))
-
-            # old_values = list([value for values in dict_.values() for value in values])
-
-            # for key, values in th._thesaurus.items():
-            #     if key in old_values:
-            #         print(key, values)
-            #         old_key = {k: v for k, v in dict_.items() if key in v}
-            #         old_key = list(old_key.keys())[0]
-            #         dict_[old_key].extend(values)
-            #         dict_[old_key] = list(set(dict_[old_key]))
-
-            th = Thesaurus(
-                x={**th._thesaurus, **dict_},
-                ignore_case=True,
-                full_match=False,
-                use_re=False,
-            )
-            th.to_textfile(thesaurus_file)
-
-    else:
-        #
-        # Creates a new thesaurus
-        #
-        text_clustering(pd.Series(words_list)).to_textfile(thesaurus_file)
+    Thesaurus(
+        x=th,
+        ignore_case=True,
+        full_match=False,
+        use_re=False,
+    ).to_textfile(thesaurus_file)
 
     logging.info(f"Thesaurus file '{thesaurus_file}' created.")
