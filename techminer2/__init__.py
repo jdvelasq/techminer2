108023efa41dd88c275309eb5db21166e0513df0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-02-15 17:35:38 -0500

    Update __init__.py

--- techminer2/__init__.py
+++ techminer2/__init__.py
@@ -79,6 +79,7 @@ from .factorial_analysis_mds_map import factorial_analysis_mds_map
 from .factorial_analysis_mds_silhouette_scores import (
     factorial_analysis_mds_silhouette_scores,
 )
+from .find_abbreviations import find_abbreviations
 from .find_keyword import find_keyword
 from .find_string import find_string
 from .gantt_chart import gantt_chart
