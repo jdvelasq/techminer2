cf4e51e92c355cb83d10e5df03f24ba87eb52baf
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-02-17 15:31:27 -0500

    Update keywords_summarization.py

--- techminer2/keywords_summarization.py
+++ techminer2/keywords_summarization.py
@@ -55,6 +55,143 @@ def keywords_summarization(
 
         return expanded_keywords
 
+    def select_documents(documents, expanded_keywords):
+        documents = documents.copy()
+        documents["_points_"] = 0
+
+        for keyword in expanded_keywords:
+            documents.loc[
+                documents.document_title.str.contains(keyword), "_points_"
+            ] += 1
+
+        documents = documents[documents._points_ > 0]
+        documents = documents.sort_values(
+            by=["_points_", "global_citations"], ascending=False
+        )
+        return documents
+
+    def write_report(documents, sufix):
+
+        # ----< select columns to report >-----------------------------------------------
+        documents = documents.copy()
+        column_list = []
+        reported_columns = [
+            "document_title",
+            "authors",
+            "global_citations",
+            "source_title",
+            "pub_year",
+            "abstract",
+        ]
+        for column in reported_columns:
+            if column in documents.columns:
+                column_list.append(column)
+        documents = documents[column_list]
+
+        # ----< report >-----------------------------------------------------------------
+        with open(
+            join(directory, f"abstract_summarization{sufix}.txt"), "w"
+        ) as out_file:
+
+            for index, row in documents.iterrows():
+
+                for column in reported_columns:
+
+                    if column not in row.index:
+                        continue
+
+                    if column == "document_title":
+                        print("      document_title :", end="", file=out_file)
+                        print(
+                            textwrap.fill(
+                                row[column],
+                                width=115,
+                                initial_indent=" " * 23,
+                                subsequent_indent=" " * 23,
+                                fix_sentence_endings=True,
+                            )[22:],
+                            file=out_file,
+                        )
+                        continue
+
+                    if column == "abstract":
+                        print("            abstract :", end="", file=out_file)
+                        print(
+                            textwrap.fill(
+                                row[column],
+                                width=115,
+                                initial_indent=" " * 23,
+                                subsequent_indent=" " * 23,
+                                fix_sentence_endings=True,
+                            )[22:],
+                            file=out_file,
+                        )
+                        continue
+
+                    if column in [
+                        "raw_author_keywords",
+                        "author_keywords",
+                        "raw_index_keywords",
+                        "index_keywords",
+                    ]:
+                        keywords = row[column]
+                        if pd.isna(keywords):
+                            continue
+                        keywords = keywords.split("; ")
+                        print(" {:>19} : {}".format(column, keywords[0]), file=out_file)
+                        for keyword in keywords[1:]:
+                            print(" " * 23 + keyword, file=out_file)
+                        continue
+
+                    print(" {:>19} : {}".format(column, row[column]), file=out_file)
+
+                if index != documents.index[-1]:
+                    print(
+                        "-" * 120,
+                        file=out_file,
+                    )
+
+    # ----< data must be a list >--------------------------------------------------------
+    if isinstance(keywords, str):
+        keywords = [keywords]
+    keywords = [word for word in keywords if word != ""]
+
+    documents = load_filtered_documents(directory)
+    expanded_keywords = expand_keywords(keywords)
+    documents = select_documents(documents, expanded_keywords)
+    documents = documents.head(n_phrases)
+    write_report(documents, sufix)
+    print("done!")
+
+
+def _keywords_summarization(
+    column,
+    keywords=None,
+    n_phrases=10,
+    sufix="",
+    directory="./",
+):
+    def expand_keywords(keywords):
+        #
+        # Expands the original keywords list to the equivalent keywords in the thesaurus.
+        #
+        thesaurus_file = join(directory, "keywords.txt")
+        if isfile(thesaurus_file):
+            th = load_file_as_dict(thesaurus_file)
+        else:
+            raise FileNotFoundError(
+                "The file {} does not exist.".format(thesaurus_file)
+            )
+
+        # extract keys for thesaurus
+        reversed_th = {value: key for key, values in th.items() for value in values}
+        th_keys = []
+        for keyword in keywords:
+            th_keys.append(reversed_th[keyword])
+        expanded_keywords = [text for key in th_keys for text in th[key]]
+
+        return expanded_keywords
+
     def select_documents(documents, column, expanded_keywords):
         documents = documents.copy()
         documents.index = documents.record_no
