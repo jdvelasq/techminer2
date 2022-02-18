b6bed09857a443fbc76e84bcc58e4015ae241856
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-02-17 19:15:06 -0500

    Update co_occurrence_network_summarization.py

--- techminer2/co_occurrence_network_summarization.py
+++ techminer2/co_occurrence_network_summarization.py
@@ -11,75 +11,10 @@ Co-occurrence Network / Summarization
 ...     n_phrases=3,
 ...     directory=directory,
 ... )
-The research on data science and ai in FINTECH involves many latest progress made in smart
-FINTECH for bankingtech, tradetech, lendtech, insurtech, wealthtech, paytech, risktech,
-cryptocurrencies, and blockchain, and the dsai techniques including complex system
-methods, quantitative methods, intelligent interactions, recognition and responses, data
-analytics, deep learning, federated learning, privacy-preserving processing, augmentation,
-optimization, and system intelligence enhancement... From the theoretical point of view,
-our research indicates, that besides key growth driving factors, outlined in existing
-literature, such as strategy, prerequisites for rapid growth, business model choice,
-international business networks, entrepreneur's characteristics, product development or
-theoretical frameworks for development, especially within the international market, the
-quality of digital logistics performance of FINTECH companies seem to matter... The most
-important factors that influence the level of satisfaction when using FINTECH services
-were considered: comfort and ease of use, legal regulations, ease of account opening,
-mobile payments features, crowdfunding options, international money transfers features,
-reduced costs associated with transactions, PEER-TO-PEER LENDING, insurances options,
-online brokerage, cryptocoins options and exchange options.
-<BLANKLINE>
-The research on data science and ai in fintech involves many latest progress made in smart
-fintech for BANKingtech, tradetech, lendtech, insurtech, wealthtech, paytech, risktech,
-cryptocurrencies, and blockchain, and the dsai techniques including complex system
-methods, quantitative methods, intelligent interactions, recognition and responses, data
-analytics, deep learning, federated learning, privacy-preserving processing, augmentation,
-optimization, and system intelligence enhancement... From the theoretical point of view,
-our research indicates, that besides key growth driving factors, outlined in existing
-literature, such as strategy, prerequisites for rapid growth, business model choice,
-international business networks, entrepreneur's characteristics, product development or
-theoretical frameworks for development, especially within the international market, the
-quality of digital logistics performance of fintech companies seem to matter... The most
-important factors that influence the level of satisfaction when using fintech services
-were considered: comfort and ease of use, legal REGULATIONs, ease of account opening,
-mobile payments features, crowdfunding options, international money transfers features,
-reduced costs associated with transactions, peer-to-peer lending, insurances options,
-online brokerage, cryptocoins options and exchange options.
-<BLANKLINE>
-The research on data science and ai in fintech involves many latest progress made in smart
-fintech for bankingtech, tradetech, lendtech, insurtech, wealthtech, paytech, risktech,
-CRYPTOCURRENCIES, and BLOCKCHAIN, and the dsai techniques including complex system
-methods, quantitative methods, intelligent interactions, recognition and responses, data
-analytics, deep learning, federated learning, privacy-preserving processing, augmentation,
-optimization, and system intelligence enhancement... From the theoretical point of view,
-our research indicates, that besides key growth driving factors, outlined in existing
-literature, such as strategy, prerequisites for rapid growth, business model choice,
-international business networks, entrepreneur's characteristics, product development or
-theoretical frameworks for development, especially within the international market, the
-quality of digital logistics performance of fintech companies seem to matter... The most
-important factors that influence the level of satisfaction when using fintech services
-were considered: comfort and ease of use, legal regulations, ease of account opening,
-mobile payments features, crowdfunding options, international money transfers features,
-reduced costs associated with transactions, peer-to-peer lending, insurances options,
-online brokerage, cryptocoins options and exchange options.
-<BLANKLINE>
-The research on data science and ai in fintech involves many latest progress made in smart
-fintech for bankingtech, tradetech, lendtech, insurtech, wealthtech, paytech, RISKtech,
-cryptocurrencies, and blockchain, and the dsai techniques including complex system
-methods, quantitative methods, intelligent interactions, recognition and responses, data
-analytics, deep learning, federated learning, privacy-preserving processing, augmentation,
-optimization, and system intelligence enhancement... From the theoretical point of view,
-our research indicates, that besides key growth driving factors, outlined in existing
-literature, such as strategy, prerequisites for rapid growth, business model choice,
-international business networks, entrepreneur's characteristics, product development or
-theoretical frameworks for development, especially within the international market, the
-quality of digital logistics performance of fintech companies seem to matter... The most
-important factors that influence the level of satisfaction when using fintech services
-were considered: comfort and ease of use, legal regulations, ease of account opening,
-mobile payments features, crowdfunding options, international money transfers features,
-reduced costs associated with transactions, PEER-TO-PEER lending, insurances options,
-online brokerage, cryptocoins options and exchange options.
-<BLANKLINE>
-
+- INFO - Generating /workspaces/techminer2/data/keywords_summarization_CL_00.txt
+- INFO - Generating /workspaces/techminer2/data/keywords_summarization_CL_01.txt
+- INFO - Generating /workspaces/techminer2/data/keywords_summarization_CL_02.txt
+- INFO - Generating /workspaces/techminer2/data/keywords_summarization_CL_03.txt
 
 
 """
@@ -129,5 +64,3 @@ def co_occurrence_network_summarization(
             sufix="_" + row[0],
             directory=directory,
         )
-
-        print()
