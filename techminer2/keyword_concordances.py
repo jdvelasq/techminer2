d49e9a62f89d59bc345a35b793a66b720098ee40
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-02-21 22:30:41 -0500

    Update keyword_concordances.py

--- techminer2/keyword_concordances.py
+++ techminer2/keyword_concordances.py
@@ -7,7 +7,18 @@ Abstract concordances exploration tool.
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> keyword_concordances('fintech', top_n=10, directory=directory)
+>>> keyword_concordances('structural equation model', top_n=10, directory=directory)
+- INFO - Abstract concordances report generated.
+                                We analyzed the data with a  structural equation model  (sem) to test the hypotheses, including the relationshi ...
+...  model, five hypotheses were developed and tested using  structural equation model  techniques (sem-pls).
+                              The data analysis method used  structural equation model  with warppls 6.0 software.
+                                      For the analysis, the  structural equation model  (sem) was used.
+      To analyze the causal relationship between variables,  structural equation model  (sem) was implemented.
+         The research design is a quantitative method using  structural equation model  (sem).
+                                         Analysis data with  structural equation model  (sem) using smart pls v2.0.
+                                                             structural equation model  (sem) was used to analyze and verify the study variables.
+... roach using a questionnaire to collect the data and use  structural equation model  to analyse it.
+                                                      Using  structural equation model  to analyze the study's conceptual model, our results co ...
 
 
 """
@@ -18,13 +29,14 @@ from os.path import isfile, join
 import pandas as pd
 
 from . import logging
+from .load_filtered_documents import load_filtered_documents
 from .thesaurus import load_file_as_dict
 
 
-def keyword_concordances(keywords, top_n=50, directory="./"):
+def keyword_concordances(keyword, top_n=50, directory="./"):
 
     # ---< Sort abstracts by importance >------------------------------------------------
-    documents = pd.read_csv(join(directory, "documents.csv"))
+    documents = load_filtered_documents(directory)
     record_no2citation = dict(
         zip(documents["record_no"], documents["global_citations"])
     )
@@ -45,28 +57,26 @@ def keyword_concordances(keywords, top_n=50, directory="./"):
     reversed_th = {value: key for key, values in th.items() for value in values}
     th_keys = []
 
-    if isinstance(keywords, str):
-        keywords = [keywords]
-
-    for keyword in keywords:
-        th_keys.append(reversed_th[keyword])
+    th_keys.append(reversed_th[keyword])
     expanded_keywords = [text for key in th_keys for text in th[key]]
 
     # ---< Selects the abstracts >-------------------------------------------------------
-    regex = [r"\b" + keyword + r"\b" for keyword in expanded_keywords]
-    regex
+    regex = [r"\b" + word + r"\b" for word in expanded_keywords]
+    regex = "|".join(regex)
     abstracts = abstracts[abstracts.text.str.contains(regex, regex=True)]
     abstracts = abstracts[["record_no", "text"]]
     abstracts["text"] = abstracts["text"].str.capitalize()
-    abstracts["text"] = abstracts["text"].str.replace(
-        r"\b" + text + r"\b", text.upper(), regex=True
-    )
-    abstracts["text"] = abstracts["text"].str.replace(
-        r"\b" + text.capitalize() + r"\b", text.upper(), regex=True
-    )
+    for word in expanded_keywords:
+        abstracts["text"] = abstracts["text"].str.replace(
+            r"\b" + word + r"\b", word.upper(), regex=True
+        )
+        abstracts["text"] = abstracts["text"].str.replace(
+            r"\b" + word.capitalize() + r"\b", word.upper(), regex=True
+        )
 
     # ---< Writes the report >-----------------------------------------------------------
-    with open(os.path.join(directory, "abstract_concordantes.txt"), "w") as out_file:
+    with open(join(directory, "abstract_concordantes.txt"), "w") as out_file:
+
         for _, row in abstracts.iterrows():
 
             paragraph = textwrap.fill(
@@ -83,8 +93,10 @@ def keyword_concordances(keywords, top_n=50, directory="./"):
     logging.info("Abstract concordances report generated.")
 
     # ---< Display results >-------------------------------------------------------------
-    text = text.upper()
-    regex = r"\b" + text + r"\b"
+    expanded_keywords = [word.upper() for word in expanded_keywords]
+
+    regex = [r"\b" + word + r"\b" for word in expanded_keywords]
+    regex = "(" + "|".join(regex) + ")"
     contexts = abstracts["text"].str.extract(
         r"(?P<left_context>[\s \S]*)" + regex + r"(?P<right_context>[\s \S]*)"
     )
@@ -97,11 +109,12 @@ def keyword_concordances(keywords, top_n=50, directory="./"):
     contexts["right_context"] = contexts["right_context"].map(
         lambda x: x[:56] + " ..." if len(x) > 60 else x
     )
+
     for _, row in contexts.head(top_n).iterrows():
         print(
             "{:>60s} {:s} {:s}".format(
                 row["left_context"],
-                text,
+                keyword,
                 row["right_context"],
             )
         )
