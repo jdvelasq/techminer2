db903c7de868184738f2e08999d04a45ec83ebb8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-14 21:53:00 -0500

    Update abstract_concordances.py

--- techminer2/abstract_concordances.py
+++ techminer2/abstract_concordances.py
@@ -1,14 +1,17 @@
 """
 Abstract Concordances
-===============================================================================
+=========================================================================================
 
 Abstract concordances exploration tool.
 
 
 >>> from techminer2 import *
 >>> directory = "/workspaces/techminer2/data/"
->>> abstract_concordances('fintech', top_n=10, directory=directory)
-- INFO - Abstract concordances report generated.
+>>> abstract_concordances(
+...     'fintech',
+...     top_n=10,
+...     directory=directory,
+... )
                              The industry overall, and many  FINTECH  start-ups are looking for new pathways to successful bu ...
 ...  able to figure out how to effectively hook up with the  FINTECH  revolution is at stake.
                                            We present a new  FINTECH  innovation mapping approach that enables the assessment ...
@@ -22,58 +25,32 @@ Abstract concordances exploration tool.
 
 """
 
-import os
-import textwrap
-
-import pandas as pd
-
-from . import logging
+from .load_abstracts import load_abstracts
+from .load_filtered_documents import load_filtered_documents
 
 
-def abstract_concordances(text, top_n=50, directory="./"):
+def abstract_concordances(
+    text,
+    top_n=50,
+    directory="./",
+):
+    """Checks the occurrence contexts of a given text in the abstract's phrases."""
 
-    # ---< Sort abstracts by importance >------------------------------------------------
-    documents = pd.read_csv(os.path.join(directory, "documents.csv"))
-    record_no2citation = dict(
-        zip(documents["record_no"], documents["global_citations"])
-    )
-    abstracts = pd.read_csv(os.path.join(directory, "abstracts.csv"))
-    abstracts["citations"] = abstracts["record_no"].map(record_no2citation)
-    abstracts = abstracts.sort_values(
-        ["citations", "record_no", "line_no"], ascending=[False, True, True]
-    )
+    abstracts = load_abstracts(directory)
+    abstracts = _adds_citations_to_abstracts(abstracts, directory)
+    abstracts = _select_abstracts(abstracts, text)
+    abstracts = abstracts.head(top_n)
+    contexts = _extract_contexts(abstracts, text)
+    _print_concordances(contexts, text)
 
-    # ---< Selects the abstracts >-------------------------------------------------------
-    regex = r"\b" + text + r"\b"
-    abstracts = abstracts[abstracts.text.str.contains(regex, regex=True)]
-    abstracts = abstracts[["record_no", "text"]]
-    abstracts["text"] = abstracts["text"].str.capitalize()
-    abstracts["text"] = abstracts["text"].str.replace(
-        r"\b" + text + r"\b", text.upper(), regex=True
-    )
-    abstracts["text"] = abstracts["text"].str.replace(
-        r"\b" + text.capitalize() + r"\b", text.upper(), regex=True
-    )
 
-    # ---< Writes the report >-----------------------------------------------------------
-    with open(os.path.join(directory, "abstract_concordantes.txt"), "w") as out_file:
-        for _, row in abstracts.iterrows():
+def _print_concordances(contexts, text):
+    """Prints the report."""
+    for _, row in contexts.iterrows():
+        print(f"{row['left_context']:>60} {text.upper()} {row['right_context']}")
 
-            paragraph = textwrap.fill(
-                row["text"],
-                width=90,
-            )
-            document_id = documents[
-                documents.record_no == row.record_no
-            ].document_id.tolist()[0]
 
-            print("*** " + document_id, file=out_file)
-            print(paragraph, file=out_file)
-            print("", file=out_file)
-
-    logging.info("Abstract concordances report generated.")
-
-    # ---< Display results >-------------------------------------------------------------
+def _extract_contexts(abstracts, text):
     text = text.upper()
     regex = r"\b" + text + r"\b"
     contexts = abstracts["text"].str.extract(
@@ -88,11 +65,35 @@ def abstract_concordances(text, top_n=50, directory="./"):
     contexts["right_context"] = contexts["right_context"].map(
         lambda x: x[:56] + " ..." if len(x) > 60 else x
     )
-    for _, row in contexts.head(top_n).iterrows():
-        print(
-            "{:>60s} {:s} {:s}".format(
-                row["left_context"],
-                text,
-                row["right_context"],
-            )
-        )
+    return contexts
+
+
+def _select_abstracts(abstracts, text):
+    """Selects the abstracts."""
+
+    regex = r"\b" + text + r"\b"
+    abstracts = abstracts[abstracts.text.str.contains(regex, regex=True)]
+    abstracts = abstracts[["record_no", "text"]]
+    abstracts["text"] = abstracts["text"].str.capitalize()
+    abstracts["text"] = abstracts["text"].str.replace(
+        r"\b" + text + r"\b", text.upper(), regex=True
+    )
+    abstracts["text"] = abstracts["text"].str.replace(
+        r"\b" + text.capitalize() + r"\b", text.upper(), regex=True
+    )
+
+    return abstracts
+
+
+def _adds_citations_to_abstracts(abstracts, directory):
+    """Adds citations to abstracts."""
+
+    documents = load_filtered_documents(directory)
+    record_no2citation = dict(
+        zip(documents["record_no"], documents["global_citations"])
+    )
+    abstracts["citations"] = abstracts["record_no"].map(record_no2citation)
+    abstracts = abstracts.sort_values(
+        ["citations", "record_no", "line_no"], ascending=[False, True, True]
+    )
+    return abstracts
