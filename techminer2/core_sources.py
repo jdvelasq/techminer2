230dfd9fc09ed1d8491e5ab3078f837da68ec62b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-16 14:59:44 -0500

    Update core_sources.py

--- techminer2/core_sources.py
+++ techminer2/core_sources.py
@@ -24,7 +24,7 @@ Core Sources
 import numpy as np
 import pandas as pd
 
-from .load_filtered_documents import load_filtered_documents
+from ._read_records import read_filtered_records
 from .explode import explode
 
 
