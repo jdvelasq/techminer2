dd1a5b4164467ba2001bde0089e2a32df133de11
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-20 11:55:49 -0500

    Update annual_indicators.py

--- techminer2/annual_indicators.py
+++ techminer2/annual_indicators.py
@@ -5,16 +5,33 @@ Annual Indicators
 >>> from techminer2 import *
 >>> directory = "data/"
 >>> annual_indicators(directory)
-          num_documents  ...  cum_local_citations
-pub_year                 ...                     
-2016                  5  ...                   29
-2017                 10  ...                   77
-2018                 34  ...                  204
-2019                 38  ...                  315
-2020                 62  ...                  377
-2021                 99  ...                  397
+          num_documents  ...  mean_local_citations_per_year
+pub_year                 ...                               
+2016                  5  ...                           0.97
+2017                 10  ...                           0.96
+2018                 34  ...                           0.93
+2019                 38  ...                           0.97
+2020                 62  ...                           0.50
+2021                 99  ...                           0.20
 <BLANKLINE>
-[6 rows x 8 columns]
+[6 rows x 11 columns]
+
+>>> from pprint import pprint
+>>> pprint(annual_indicators(directory).columns.to_list())
+['num_documents',
+ 'local_citations',
+ 'global_citations',
+ 'mean_global_citations',
+ 'mean_local_citations',
+ 'cum_num_documents',
+ 'cum_global_citations',
+ 'cum_local_citations',
+ 'citable_years',
+ 'mean_global_citations_per_year',
+ 'mean_local_citations_per_year']
+
+
+
 
 """
 
@@ -44,5 +61,21 @@ def annual_indicators(directory="./"):
     records = records.assign(cum_num_documents=records.num_documents.cumsum())
     records = records.assign(cum_global_citations=records.global_citations.cumsum())
     records = records.assign(cum_local_citations=records.local_citations.cumsum())
+    current_year = records.index.max()
+    records = records.assign(citable_years=current_year - records.index + 1)
+    records = records.assign(
+        mean_global_citations_per_year=records.mean_global_citations
+        / records.citable_years
+    )
+    records.mean_global_citations_per_year = (
+        records.mean_global_citations_per_year.round(2)
+    )
+    records = records.assign(
+        mean_local_citations_per_year=records.mean_local_citations
+        / records.citable_years
+    )
+    records.mean_local_citations_per_year = records.mean_local_citations_per_year.round(
+        2
+    )
 
     return records
