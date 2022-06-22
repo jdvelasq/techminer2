67e8e3e88045b3f707f67a6589c27ef668a864e0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-22 12:04:41 -0500

    Create documents_by_funding_sponsor.py

---
+++ techminer2/scopus/documents_by_funding_sponsor.py
@@ -0,0 +1,21 @@
+"""
+Documents by funding sponsor (pendient)
+===============================================================================
+
+>>> from techminer2.scopus import *
+>>> directory = "data/"
+>>> file_name = "sphinx/scopus/images/documents_by_funding_sponsor.png"
+
+>>> documents_by_funding_sponsor(
+...     directory
+... ).write_image(file_name)
+
+.. image:: images/documents_by_funding_sponsor.png
+    :width: 700px
+    :align: center
+
+"""
+
+
+def documents_by_funding_sponsor(directory):
+    pass
