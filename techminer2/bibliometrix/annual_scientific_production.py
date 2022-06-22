76016527265f0455ad229367b9a8fec8532a40ac
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-22 12:48:00 -0500

    Create annual_scientific_production.py

---
+++ techminer2/bibliometrix/annual_scientific_production.py
@@ -0,0 +1,41 @@
+"""
+Annual Scientific Production
+===============================================================================
+
+See https://jdvelasq.github.io/techminer2/annual_indicators.html
+
+
+>>> from techminer2.bibliometrix import *
+>>> directory = "data/"
+>>> file_name = "sphinx/bibliometrix/images/annual_scientific_production.png"
+>>> annual_scientific_production(directory).write_image(file_name)
+
+.. image:: images/annual_scientific_production.png
+    :width: 700px
+    :align: center
+
+"""
+import plotly.express as px
+
+from ..annual_indicators import annual_indicators
+
+
+def annual_scientific_production(directory):
+    indicators = annual_indicators(directory)
+    fig = px.line(
+        x=indicators.index,
+        y=indicators.num_documents,
+        title="Annual Scientific Production",
+        markers=True,
+        text=indicators.num_documents,
+        labels={"x": "Year", "y": "Number of publications"},
+    )
+    fig.update_traces(marker=dict(size=12))
+    fig.update_traces(textposition="bottom right")
+    fig.update_traces(line=dict(color="black"))
+    fig.update_xaxes(tickangle=270)
+    fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
+    fig.update_xaxes(linecolor="gray", gridcolor="lightgray")
+    fig.update_yaxes(linecolor="gray", gridcolor="lightgray")
+    fig.update_yaxes(showticklabels=False)
+    return fig
