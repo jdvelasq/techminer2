6e9cbf6b0dfe9a2469340701417646dbbba25a77
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-22 12:04:32 -0500

    Create documents_per_year_by_source.py

---
+++ techminer2/scopus/documents_per_year_by_source.py
@@ -0,0 +1,21 @@
+"""
+Documents per year by source (pendient)
+===============================================================================
+
+>>> from techminer2.scopus import *
+>>> directory = "data/"
+>>> file_name = "sphinx/scopus/images/documents_per_year_by_source.png"
+
+>>> documents_by_author(
+...     directory
+... ).write_image(file_name)
+
+.. image:: images/documents_per_year_by_source.png
+    :width: 700px
+    :align: center
+
+"""
+
+
+def documents_per_year_by_source(directory):
+    pass
