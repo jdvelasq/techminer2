51ff76e922fc2c8b151e466e487cd96c76e74182
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-22 12:04:36 -0500

    Create documents_by_type.py

---
+++ techminer2/scopus/documents_by_type.py
@@ -0,0 +1,21 @@
+"""
+Documents by type (pendient)
+===============================================================================
+
+>>> from techminer2.scopus import *
+>>> directory = "data/"
+>>> file_name = "sphinx/scopus/images/documents_by_type.png"
+
+>>> documents_by_type(
+...     directory
+... ).write_image(file_name)
+
+.. image:: images/documents_by_type.png
+    :width: 700px
+    :align: center
+
+"""
+
+
+def documents_by_type(directory):
+    pass
