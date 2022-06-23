c212f45ad96a601a85ffd8429a388557d269e69d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-22 21:47:16 -0500

    Update most_relevant_sources.py

--- techminer2/most_relevant_sources.py
+++ techminer2/most_relevant_sources.py
@@ -1,8 +1,9 @@
 """
-Most Relevant Sources
+Most relevant sources (ok!)
 ===============================================================================
 
-See https://jdvelasq.github.io/techminer2/column_indicators.html
+See :doc:`column indicators <column_indicators>` to obtain a `pandas.Dataframe` 
+with the data.
 
 >>> from techminer2 import *
 >>> directory = "data/"
