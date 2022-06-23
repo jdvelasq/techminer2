c82fa0ddb260bfeca030887f883e5af0938c0232
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-23 13:52:28 -0500

    Update average_citations_per_year.py

--- techminer2/average_citations_per_year.py
+++ techminer2/average_citations_per_year.py
@@ -1,5 +1,5 @@
 """
-Average Citations per Year (ok!)
+Average Citations per Year
 ===============================================================================
 
 See :doc:`annual indicators <annual_indicators>` to obtain a `pandas.Dataframe` 
@@ -7,36 +7,48 @@ with the data.
 
 >>> from techminer2 import *
 >>> directory = "data/"
->>> file_name = "sphinx/images/average_citations_per_year.png"
->>> average_citations_per_year(directory=directory).write_image(file_name)
+>>> file_name = "sphinx/_static/average_citations_per_year.html"
 
-.. image:: images/average_citations_per_year.png
-    :width: 700px
-    :align: center
+>>> average_citations_per_year(directory=directory).write_html(file_name)
+
+.. raw:: html
+
+    <iframe src="_static/average_citations_per_year.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-import plotly.express as px
-
-from .annual_indicators import annual_indicators
+from .annual_indicators_plot import annual_indicators_plot
 
 
 def average_citations_per_year(directory="./"):
-
-    indicators = annual_indicators(directory)
-    fig = px.line(
-        x=indicators.index,
-        y=indicators.mean_global_citations,
+    return annual_indicators_plot(
+        column="mean_global_citations",
         title="Average citations per year",
-        markers=True,
-        text=indicators.mean_global_citations.round(2),
-        labels={"x": "Year", "y": "Citations"},
+        directory=directory,
     )
-    fig.update_traces(marker=dict(size=12))
-    fig.update_traces(textposition="top right")
-    fig.update_traces(line=dict(color="black"))
-    fig.update_xaxes(tickangle=270)
-    fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
-    fig.update_xaxes(linecolor="gray", gridcolor="lightgray")
-    fig.update_yaxes(linecolor="gray", gridcolor="lightgray")
-    fig.update_yaxes(showticklabels=False)
-    return fig
+
+
+# import plotly.express as px
+
+# from .annual_indicators import annual_indicators
+
+
+# def average_citations_per_year(directory="./"):
+
+#     indicators = annual_indicators(directory)
+#     fig = px.line(
+#         x=indicators.index,
+#         y=indicators.mean_global_citations,
+#         title="Average citations per year",
+#         markers=True,
+#         text=indicators.mean_global_citations.round(2),
+#         labels={"x": "Year", "y": "Citations"},
+#     )
+#     fig.update_traces(marker=dict(size=12))
+#     fig.update_traces(textposition="top right")
+#     fig.update_traces(line=dict(color="black"))
+#     fig.update_xaxes(tickangle=270)
+#     fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
+#     fig.update_xaxes(linecolor="gray", gridcolor="lightgray")
+#     fig.update_yaxes(linecolor="gray", gridcolor="lightgray")
+#     fig.update_yaxes(showticklabels=False)
+#     return fig
