a12dbb22275219ee6cb8e6928d8789af155d79d1
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-23 16:54:38 -0500

    plotly -> document by country

--- techminer2/documents_by_country.py
+++ techminer2/documents_by_country.py
@@ -2,23 +2,25 @@
 Documents by country
 ===============================================================================
 
->>> from techminer2.scopus import *
+>>> from techminer2 import *
 >>> directory = "data/"
->>> file_name = "sphinx/images/documents_by_country.png"
+>>> file_name = "sphinx/_static/documents_by_country.html"
+
 
 >>> documents_by_country(
 ...     directory
-... ).write_image(file_name)
+... ).write_html(file_name)
 
-.. image:: images/documents_by_country.png
-    :width: 700px
-    :align: center
+.. raw:: html
+
+    <iframe src="_static/documents_by_country.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
 from .bar_chart import bar_chart
 
 
 def documents_by_country(directory):
+
     return bar_chart(
         column="countries",
         min_occ=None,
