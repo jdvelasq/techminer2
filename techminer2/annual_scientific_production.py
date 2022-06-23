39290198734b5a877254fab6c539e1deb9e2038d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-23 13:47:44 -0500

    Update annual_scientific_production.py

--- techminer2/annual_scientific_production.py
+++ techminer2/annual_scientific_production.py
@@ -25,28 +25,3 @@ def annual_scientific_production(directory="./"):
         title="Annual Scientific Production",
         directory=directory,
     )
-
-
-# import plotly.express as px
-
-# from .annual_indicators import annual_indicators
-
-# def annual_scientific_production(directory="./"):
-#     indicators = annual_indicators(directory)
-#     fig = px.line(
-#         x=indicators.index,
-#         y=indicators.num_documents,
-#         title="Annual Scientific Production",
-#         markers=True,
-#         text=indicators.num_documents,
-#         labels={"x": "Year", "y": "Number of publications"},
-#     )
-#     fig.update_traces(marker=dict(size=12))
-#     fig.update_traces(textposition="bottom right")
-#     fig.update_traces(line=dict(color="black"))
-#     fig.update_xaxes(tickangle=270)
-#     fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
-#     fig.update_xaxes(linecolor="gray", gridcolor="lightgray")
-#     fig.update_yaxes(linecolor="gray", gridcolor="lightgray")
-#     fig.update_yaxes(showticklabels=False)
-#     return fig
