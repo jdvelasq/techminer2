a2ddb67ceb16fa565cf6feb5429e28332f3a6430
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-24 08:32:45 -0500

    Update most_local_cited_sources.py

--- techminer2/most_local_cited_sources.py
+++ techminer2/most_local_cited_sources.py
@@ -18,40 +18,30 @@ with the data. In this case, use:
 
 >>> from techminer2 import *
 >>> directory = "data/"
->>> file_name = "sphinx/images/most_local_cited_sources.png"
+>>> file_name = "sphinx/_static/most_local_cited_sources.html"
+
 >>> most_local_cited_sources(
 ...     top_n=20,
 ...     directory=directory,
-... ).write_image(file_name)
+... ).write_html(file_name)
 
-.. image:: images/most_local_cited_sources.png
-    :width: 700px
-    :align: center
+.. raw:: html
+
+    <iframe src="_static/most_local_cited_sources.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-from .cleveland_plot import cleveland_plot
-from .column_indicators import column_indicators
+from .cleveland_chart import cleveland_chart
 
 
-def most_local_cited_sources(
-    top_n=10,
-    directory="./",
-):
+def most_local_cited_sources(directory="./", top_n=20):
 
-    indicator = column_indicators(
+    return cleveland_chart(
         column="iso_source_name",
+        top_n=top_n,
+        min_occ=None,
+        max_occ=None,
         directory=directory,
+        metric="num_documents",
+        title="Most local cited sources",
         file_name="references.csv",
     )
-    indicator = indicator.sort_values(by="local_citations", ascending=False)
-    indicator = indicator.head(top_n)
-    indicator = indicator.local_citations
-
-    fig = cleveland_plot(
-        series=indicator,
-        x_label=None,
-        y_label="Source Title",
-        title="Most local cited sources in references",
-    )
-
-    return fig
