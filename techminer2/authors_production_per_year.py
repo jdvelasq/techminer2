0d74f2ada191129b84df50f6dd0cc97090bc29e6
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-23 22:57:26 -0500

    Update authors_production_per_year.py

--- techminer2/authors_production_per_year.py
+++ techminer2/authors_production_per_year.py
@@ -1,30 +1,29 @@
 """
-Authors' production per year (TODO)
+Authors' production per year
 ===============================================================================
-
-Creates a table with:
-
-* Author
-
-* Year
-
-* Num documents
-
-* Global citations in the year
-
-* Global citations in the year / citable period.
-
 
 >>> from techminer2 import *
 >>> directory = "data/"
 
 >>> authors_production_per_year(
-...    min_occ=2, 
 ...    directory=directory,
-... )
+... ).head()
+                    num_documents  ...  local_citations_per_year
+authors   pub_year                 ...                          
+Dolata M  2016                  1  ...                     1.167
+Hong S    2016                  1  ...                     0.167
+Hung J-L  2016                  1  ...                     0.667
+Kim K     2016                  1  ...                     0.167
+Kotarba M 2016                  1  ...                     0.500
+<BLANKLINE>
+[5 rows x 6 columns]
 
 """
+from .column_indicators_by_year import column_indicators_by_year
 
 
-def authors_production_per_year(top_n=10, directory="./"):
-    pass
+def authors_production_per_year(directory="./"):
+    return column_indicators_by_year(
+        column="authors",
+        directory=directory,
+    )
