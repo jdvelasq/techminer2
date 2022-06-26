7f669f15d4afabb4f68878a08f9105469abf2250
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-26 10:35:59 -0500

    Update bar_plot.py

--- techminer2/bar_plot.py
+++ techminer2/bar_plot.py
@@ -12,14 +12,13 @@ Bar plot
 
 >>> bar_plot(
 ...     dataframe,
-...     column="num_documents",
+...     metric="num_documents",
 ...     title="Annual Scientific Production",
 ... ).write_html(file_name)
 
 .. raw:: html
 
     <iframe src="_static/bar_plot.html" height="600px" width="100%" frameBorder="0"></iframe>
-
 
 """
 from .make_bar_plot import make_bar_plot
@@ -28,42 +27,22 @@ from .prepare_plot import prepare_plot
 
 def bar_plot(
     dataframe,
-    column,
+    metric,
     title=None,
 ):
     """
     Make a  bar plot from a dataframe.
 
     :param dataframe: Dataframe
-    :param column: Column to plot
+    :param metric: Column to plot
     :param title: Title of the plot
     :return: Plotly figure
     """
 
-    x_label, y_label, dataframe = prepare_plot(dataframe, column)
-
-    # x_label = column.replace("_", " ").title()
-    # y_label = dataframe.index.name.replace("_", " ").title()
-
-    # dataframe = dataframe.reset_index()
-    # names_dict = {col: col.replace("_", " ").title() for col in dataframe.columns}
-    # dataframe.rename(columns=names_dict, inplace=True)
-    # dataframe[y_label] = dataframe[y_label].str.title()
-
-    # if dataframe.index.dtype != "int64":
-    #     dataframe.index = [
-    #         textwrap.shorten(
-    #             text=text,
-    #             width=TEXTLEN,
-    #             placeholder="...",
-    #             break_long_words=False,
-    #         )
-    #         for text in dataframe.index.to_list()
-    #     ]
-
+    metric, column, dataframe = prepare_plot(dataframe, metric)
     return make_bar_plot(
         dataframe=dataframe,
-        x_label=x_label,
-        y_label=y_label,
+        x_label=metric,
+        y_label=column,
         title=title,
     )
