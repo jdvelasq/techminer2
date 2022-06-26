4a54b6aebd6b867e9e8df13158b3b77b0b8d49ac
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-26 08:21:52 -0500

    Update bar_plot.py

--- techminer2/bar_plot.py
+++ techminer2/bar_plot.py
@@ -4,22 +4,22 @@ Bar plot
 
 >>> from techminer2 import *
 >>> directory = "data/"
->>> file_name = "sphinx/images/bar_plot.png"
->>> series = column_indicators(
-...     column="countries", 
+>>> file_name = "sphinx/_static/bar_plot.html"
+>>> dataframe = column_indicators(
+...     column="countries",
 ...     directory=directory,
-... ).num_documents.head(20)
+... ).head(20)
 
 >>> bar_plot(
-...     series,
-...     x_label=None,
-...     y_label=None,
-...     title=None,
-... ).write_image(file_name)
+...     dataframe,
+...     column="num_documents",
+...     title="Annual Scientific Production",
+... ).write_html(file_name)
 
-.. image:: images/bar_plot.png
-    :width: 700px
-    :align: center
+.. raw:: html
+
+    <iframe src="_static/bar_plot.html" height="600px" width="100%" frameBorder="0"></iframe>
+
 
 """
 import textwrap
@@ -30,47 +30,61 @@ TEXTLEN = 40
 
 
 def bar_plot(
-    series,
-    x_label=None,
-    y_label=None,
+    dataframe,
+    column,
     title=None,
 ):
-    if x_label is None:
-        x_label = series.name.replace("_", " ").title()
+    """
+    Make a  bar plot from a dataframe.
 
-    if y_label is None:
-        y_label = series.index.name.replace("_", " ").title()
+    :param dataframe: Dataframe
+    :param column: Column to plot
+    :param title: Title of the plot
+    :return: Plotly figure
+    """
 
-    if series.index.dtype != "int64":
-        series.index = [
+    x_label = column.replace("_", " ").title()
+    y_label = dataframe.index.name.replace("_", " ").title()
+
+    dataframe = dataframe.reset_index()
+    names_dict = {col: col.replace("_", " ").title() for col in dataframe.columns}
+    dataframe.rename(columns=names_dict, inplace=True)
+    dataframe[y_label] = dataframe[y_label].str.title()
+
+    if dataframe.index.dtype != "int64":
+        dataframe.index = [
             textwrap.shorten(
                 text=text,
                 width=TEXTLEN,
                 placeholder="...",
                 break_long_words=False,
             )
-            for text in series.index.to_list()
+            for text in dataframe.index.to_list()
         ]
 
     fig = px.bar(
-        x=series.values,
-        y=series.index,
-        text=series.astype(str),
+        dataframe,
+        x=x_label,
+        y=y_label,
+        hover_data=dataframe.columns.to_list(),
         title=title,
-        labels={"x": x_label, "y": y_label},
         orientation="h",
     )
-    fig.update_traces(textposition="outside")
     fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
-    fig.update_traces(marker_color="lightgray", marker_line={"color": "gray"})
+    fig.update_traces(
+        marker_color="rgb(96,99,106)", marker_line={"color": "darkslategray"}
+    )
     fig.update_yaxes(
         linecolor="gray",
         linewidth=2,
         autorange="reversed",
-        # gridcolor="lightgray",
-        # griddash="dot",
+        gridcolor="lightgray",
+        griddash="dot",
     )
-    # fig.update_xaxes(tickangle=270)
-    fig.update_xaxes(showticklabels=False)
-
+    fig.update_xaxes(
+        linecolor="gray",
+        linewidth=2,
+        gridcolor="lightgray",
+        griddash="dot",
+    )
     return fig
