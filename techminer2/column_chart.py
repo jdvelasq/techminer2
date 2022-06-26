dd967c3bbed7d966dfce5992f8f3a5a0f3aba8b0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-26 10:35:56 -0500

    Update column_chart.py

--- techminer2/column_chart.py
+++ techminer2/column_chart.py
@@ -1,15 +1,16 @@
 """
-Column Chart
+Column chart
 ===============================================================================
 
->>> from techminer2.vantagepoint import *
+>>> from techminer2 import *
 >>> directory = "data/"
 >>> file_name = "sphinx/_static/column_chart.html"
 
 >>> column_chart(
-...     'author_keywords',
-...     top_n=20,
+...     column='author_keywords',
+...     top_n=15,
 ...     directory=directory,
+...     metric="num_documents",
 ... ).write_html(file_name)
 
 .. raw:: html
@@ -17,9 +18,8 @@ Column Chart
     <iframe src="_static/column_chart.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-import plotly.express as px
-
 from .column_indicators_by_metric import column_indicators_by_metric
+from .column_plot import column_plot
 
 
 def column_chart(
@@ -31,6 +31,7 @@ def column_chart(
     metric="num_documents",
     title=None,
 ):
+    """Plots a column chart from a column of a dataframe."""
 
     indicators = column_indicators_by_metric(
         column,
@@ -40,34 +41,9 @@ def column_chart(
         directory=directory,
         metric=metric,
     )
-    indicators = indicators.reset_index()
-    column_names = {
-        column: column.replace("_", " ").title() for column in indicators.columns
-    }
-    indicators = indicators.rename(columns=column_names)
 
-    fig = px.bar(
-        indicators,
-        x=column.replace("_", " ").title(),
-        y=metric.replace("_", " ").title(),
-        hover_data=["Num Documents", "Global Citations", "Local Citations"],
+    return column_plot(
+        dataframe=indicators,
+        metric=metric,
         title=title,
-        orientation="v",
     )
-    fig.update_traces(textposition="outside")
-    fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
-    fig.update_traces(marker_color="lightgray", marker_line={"color": "gray"})
-    fig.update_yaxes(
-        linecolor="gray",
-        linewidth=2,
-        gridcolor="lightgray",
-        griddash="dot",
-    )
-    fig.update_xaxes(
-        linecolor="gray",
-        linewidth=2,
-        gridcolor="lightgray",
-        griddash="dot",
-    )
-
-    return fig
