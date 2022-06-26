ee623b2a2e6a3d6cf3615de484f2f6eac13487b7
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-26 10:35:38 -0500

    Update prepare_plot.py

--- techminer2/prepare_plot.py
+++ techminer2/prepare_plot.py
@@ -6,17 +6,17 @@ TEXTLEN = 40
 
 def prepare_plot(
     dataframe,
-    column,
+    metric,
 ):
     """Formats a dataframe for plotting."""
 
-    x_label = column.replace("_", " ").title()
-    y_label = dataframe.index.name.replace("_", " ").title()
+    metric = metric.replace("_", " ").title()
+    column = dataframe.index.name.replace("_", " ").title()
 
     dataframe = dataframe.reset_index()
     names_dict = {col: col.replace("_", " ").title() for col in dataframe.columns}
     dataframe.rename(columns=names_dict, inplace=True)
-    dataframe[y_label] = dataframe[y_label].str.title()
+    dataframe[column] = dataframe[column].str.title()
 
     if dataframe.index.dtype != "int64":
         dataframe.index = [
@@ -29,4 +29,4 @@ def prepare_plot(
             for text in dataframe.index.to_list()
         ]
 
-    return x_label, y_label, dataframe
+    return metric, column, dataframe
