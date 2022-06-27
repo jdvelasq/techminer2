e1a64c145178a035fd3f54b6bf91dec150b30ba9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-27 10:06:01 -0500

    authors production over time

--- techminer2/authors_production_over_time.py
+++ techminer2/authors_production_over_time.py
@@ -16,7 +16,7 @@ Authors' production over time
     <iframe src="_static/authors_production_over_time.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-from .column_production_by_year import column_production_by_year
+from .production_over_time import production_over_time
 
 
 def authors_production_over_time(
@@ -24,7 +24,7 @@ def authors_production_over_time(
     directory="./",
 ):
 
-    return column_production_by_year(
+    return production_over_time(
         column="authors",
         top_n=top_n,
         directory=directory,
