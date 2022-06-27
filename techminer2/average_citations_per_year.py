17ef5a07e1cdc3c2c01f38cfeedd0f4a5a5ec3b9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-27 18:26:31 -0500

    Update average_citations_per_year.py

--- techminer2/average_citations_per_year.py
+++ techminer2/average_citations_per_year.py
@@ -25,30 +25,3 @@ def average_citations_per_year(directory="./"):
         title="Average citations per year",
         directory=directory,
     )
-
-
-# import plotly.express as px
-
-# from .annual_indicators import annual_indicators
-
-
-# def average_citations_per_year(directory="./"):
-
-#     indicators = annual_indicators(directory)
-#     fig = px.line(
-#         x=indicators.index,
-#         y=indicators.mean_global_citations,
-#         title="Average citations per year",
-#         markers=True,
-#         text=indicators.mean_global_citations.round(2),
-#         labels={"x": "Year", "y": "Citations"},
-#     )
-#     fig.update_traces(marker=dict(size=12))
-#     fig.update_traces(textposition="top right")
-#     fig.update_traces(line=dict(color="black"))
-#     fig.update_xaxes(tickangle=270)
-#     fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
-#     fig.update_xaxes(linecolor="gray", gridcolor="lightgray")
-#     fig.update_yaxes(linecolor="gray", gridcolor="lightgray")
-#     fig.update_yaxes(showticklabels=False)
-#     return fig
