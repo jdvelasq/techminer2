b1268eb0d8cc169fb0e436506f71aec8f7389d94
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-27 19:19:17 -0500

    Update production_over_time.py

--- techminer2/production_over_time.py
+++ techminer2/production_over_time.py
@@ -13,6 +13,54 @@ def production_over_time(
 ):
     """Primitive to plot production over time."""
 
+    indicators_by_year = _compute_production_over_time(
+        column=column,
+        top_n=top_n,
+        directory=directory,
+    )
+
+    fig = scatter_px(
+        dataframe=indicators_by_year,
+        x_label="Year",
+        y_label=column.replace("_", " ").title(),
+        size="Num Documents",
+        title=title,
+    )
+    fig.update_traces(
+        marker=dict(
+            line=dict(
+                color="black",
+                width=2,
+            ),
+        ),
+        marker_color="darkslategray",
+    )
+    fig.update_yaxes(
+        linecolor="white",
+        linewidth=1,
+        autorange="reversed",
+        gridcolor="gray",
+        griddash="dot",
+    )
+    fig.update_xaxes(
+        linecolor="white",
+        linewidth=1,
+        gridcolor="gray",
+        griddash="dot",
+        tickangle=270,
+    )
+    fig.update_layout(
+        xaxis_type="category",
+    )
+    return fig
+
+
+def _compute_production_over_time(
+    column,
+    top_n=10,
+    directory="./",
+):
+
     indicators_by_year = column_indicators_by_year(column=column, directory=directory)
     indicators_by_year = indicators_by_year.reset_index()
     selected_terms = column_indicators(column=column, directory=directory).head(top_n)
@@ -29,10 +77,4 @@ def production_over_time(
     )
     indicators_by_year = indicators_by_year.rename(columns={"Pub Year": "Year"})
 
-    return scatter_px(
-        dataframe=indicators_by_year,
-        x_label="Year",
-        y_label=column.replace("_", " ").title(),
-        size="Num Documents",
-        title=title,
-    )
+    return indicators_by_year
