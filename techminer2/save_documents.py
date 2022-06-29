6008eae4eb0dfba9eb9dfb55a419587e80ffb27c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-29 16:16:00 -0500

    Update save_documents.py

--- techminer2/save_documents.py
+++ techminer2/save_documents.py
@@ -1,16 +1,14 @@
 import os
-
-from . import logging
+import sys
 
 
 def save_documents(documents, directory="./"):
 
-    filename = os.path.join(directory, "processed", "documents.csv")
+    filename = os.path.join(directory, "processed", "_documents.csv")
     documents.to_csv(
         filename,
         sep=",",
         encoding="utf-8",
         index=False,
     )
-
-    logging.info(f"Documents saved/merged to '{filename}'")
+    sys.stdout.write(f"--INFO-- Documents saved/merged to `{filename}`\n")
