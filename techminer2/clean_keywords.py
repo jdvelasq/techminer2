bed3df0cff8219d67fb6e10dca3707ac17e75446
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 00:03:49 -0500

    Update clean_keywords.py

--- techminer2/clean_keywords.py
+++ techminer2/clean_keywords.py
@@ -6,6 +6,7 @@ Cleans the keywords columns using the `keywords.txt`file.
 
 >>> from techminer2 import *
 >>> directory = "data/"
+
 >>> clean_keywords(directory)
 - INFO - Applying thesaurus to 'raw_author_keywords' column ...
 - INFO - Applying thesaurus to 'raw_index_keywords' column...
@@ -19,74 +20,64 @@ Cleans the keywords columns using the `keywords.txt`file.
 # pylint: disable=no-member
 # pylint: disable=invalid-name
 
-from os.path import isfile, join
+import glob
+import os
+import os.path
+import sys
 
-from . import logging
-from ._read_records import read_all_records
-from .map_ import map_
-from .save_documents import save_documents
+import pandas as pd
+
 from .thesaurus import read_textfile
 
 
 def clean_keywords(directory="./"):
-    """
-    Clean all keywords columns in the records using a thesaurus (keywrords.txt).
+    """Clean all keywords columns in the records using a thesaurus (keywrords.txt)."""
 
-    """
+    sys.stdout.write("--INFO-- Cleaning keywords in database files\n")
 
-    # --------------------------------------------------------------------------
-    # Loads documents.csv
-    documents = read_all_records(directory)
-
-    thesaurus_file = join(directory, "processed", "keywords.txt")
-    if isfile(thesaurus_file):
-        th = read_textfile(thesaurus_file)
-        th = th.compile_as_dict()
+    thesaurus_file = os.path.join(directory, "processed", "keywords.txt")
+    if os.path.isfile(thesaurus_file):
+        thesaurus = read_textfile(thesaurus_file)
+        thesaurus = thesaurus.compile_as_dict()
     else:
-        raise FileNotFoundError("The file {} does not exist.".format(thesaurus_file))
-
+        raise FileNotFoundError(f"The file {thesaurus_file} does not exist.")
     #
-    # Author keywords cleaning
-    #
-    if "raw_author_keywords" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_author_keywords' column ...")
-        documents["author_keywords"] = map_(
-            documents, "raw_author_keywords", th.apply_as_dict
-        )
 
-    #
-    # Index keywords cleaning
-    #
-    if "raw_index_keywords" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_index_keywords' column...")
-        documents["index_keywords"] = map_(
-            documents, "raw_index_keywords", th.apply_as_dict
-        )
+    files = list(glob.glob(os.path.join(directory, "processed/_*.csv")))
+    for file in files:
+        data = pd.read_csv(file, encoding="utf-8")
+        #
+        if "raw_author_keywords" in data.columns:
+            data = data.assign(author_keywords=data.raw_author_keywords.str.split(";"))
+            data = data.assign(
+                author_keywords=data.author_keywords.map(
+                    lambda x: [thesaurus.apply_as_dict(y.strip()) for y in x]
+                    if isinstance(x, list)
+                    else x
+                )
+            )
+            data = data.assign(
+                author_keywords=data.author_keywords.map(
+                    lambda x: set(x) if isinstance(x, list) else x
+                )
+            )
+            data = data.assign(author_keywords=data.author_keywords.str.join("; "))
 
-    if "keywords" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_keywords' column...")
-        documents["keywords"] = map_(documents, "raw_keywords", th.apply_as_dict)
+        if "raw_index_keywords" in data.columns:
+            data = data.assign(index_keywords=data.raw_index_keywords.str.split(";"))
+            data = data.assign(
+                index_keywords=data.index_keywords.map(
+                    lambda x: [thesaurus.apply_as_dict(y.strip()) for y in x]
+                    if isinstance(x, list)
+                    else x
+                )
+            )
+            data = data.assign(
+                index_keywords=data.index_keywords.map(
+                    lambda x: set(x) if isinstance(x, list) else x
+                )
+            )
+            data = data.assign(index_keywords=data.index_keywords.str.join("; "))
 
-    if "raw_nlp_document_title" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_nlp_document_title' column...")
-        documents["nlp_document_title"] = map_(
-            documents, "raw_nlp_document_title", th.apply_as_dict
-        )
-
-    if "raw_nlp_abstract" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_nlp_abstract' column...")
-        documents["nlp_abstract"] = map_(
-            documents, "raw_nlp_abstract", th.apply_as_dict
-        )
-
-    if "raw_nlp_phrases" in documents.columns:
-        logging.info("Applying thesaurus to 'raw_nlp_phrases' column...")
-        documents["nlp_phrases"] = map_(documents, "raw_nlp_phrases", th.apply_as_dict)
-
-    #
-    # Saves!
-    #
-    # datastore.to_csv(datastorefile, index=False)
-    # --------------------------------------------------------------------------
-    save_documents(documents, directory)
-    logging.info("The thesaurus was applied to all keywords.")
+        #
+        data.to_csv(file, sep=",", encoding="utf-8", index=False)
