5c69eadf8715633175391d5d6868c012a2cdc9ae
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 08:57:41 -0500

    Update _read_records.py

--- techminer2/_read_records.py
+++ techminer2/_read_records.py
@@ -5,9 +5,23 @@ import os
 import pandas as pd
 import yaml
 
+# read the records of the documents.csv file and apply an optional filter
+# to the records.
+#
+# Parameters:
+# - directory: the directory of the project
+# - database: the database to read (either "documents" or "references")
+# - use_filter: if True, apply the filter to the records
+#
+# Returns:
+# - a pandas DataFrame with the records of the database
+#
+# Raises:
+# - FileNotFoundError: if the file does not exist
 
-def read_all_records(directory, database="documents"):
-    "Reads all records of the specified file."
+
+def read_records(directory="./", database="documents", use_filter=True):
+    """load filter records"""
 
     file_name = {
         "documents": "_documents.csv",
@@ -16,31 +30,36 @@ def read_all_records(directory, database="documents"):
     }[database]
 
     file_path = os.path.join(directory, "processed", file_name)
-    if not os.path.isfile(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"file '{file_path}' not found")
+
     records = pd.read_csv(file_path, sep=",", encoding="utf-8")
+    if use_filter:
+        records = _filter_records(directory, records)
     return records
 
 
-def read_filtered_records(directory):
-    """Loads documents from project directory."""
-    documents = read_all_records(directory)
+# Filter the records of the database.
+#
+# Parameters:
+# - records: the records of the database
+#
+# Returns:
+# - a pandas DataFrame with the filtered records of the database
+def _filter_records(directory, records):
+    """docs"""
 
-    # Filter documents
-    yaml_filename = os.path.join(directory, "processed", "filter.yaml")
-    with open(yaml_filename, "r", encoding="utf-8") as yaml_file:
-        filter_ = yaml.load(yaml_file, Loader=yaml.FullLoader)
-
+    filter_ = _load_filter(directory)
     year_min, year_max = filter_["first_year"], filter_["last_year"]
-    documents = documents.query(f"year >= {year_min}")
-    documents = documents.query(f"year <= {year_max}")
+    records = records.query(f"year >= {year_min}")
+    records = records.query(f"year <= {year_max}")
 
     min_citations, max_citations = filter_["min_citations"], filter_["max_citations"]
-    documents = documents.query(f"global_citations >= {min_citations}")
-    documents = documents.query(f"global_citations <= {max_citations}")
+    records = records.query(f"global_citations >= {min_citations}")
+    records = records.query(f"global_citations <= {max_citations}")
 
     bradford = filter_["bradford"]
-    documents = documents.query(f"bradford <= {bradford}")
+    records = records.query(f"bradford <= {bradford}")
 
     for key, value in filter_.items():
         if key in [
@@ -53,6 +72,21 @@ def read_filtered_records(directory):
             continue
 
         if value is False:
-            documents = documents.query(f"document_type != '{key}'")
+            records = records.query(f"document_type != '{key}'")
 
-    return documents
+    return records
+
+
+def _load_filter(directory):
+    yaml_filename = os.path.join(directory, "processed", "filter.yaml")
+    with open(yaml_filename, "r", encoding="utf-8") as yaml_file:
+        filter_ = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    return filter_
+
+
+def read_filtered_records(**kwars):
+    print("deprecated")
+
+
+def read_all_records(**kwars):
+    print("deprecated")
