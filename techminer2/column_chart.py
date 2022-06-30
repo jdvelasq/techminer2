ad1ee0ce9957159757274d3747b2b4db808107dd
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 16:02:07 -0500

    Update column_chart.py

--- techminer2/column_chart.py
+++ techminer2/column_chart.py
@@ -18,7 +18,7 @@ Column chart
     <iframe src="_static/column_chart.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-from .column_indicators_by_metric import column_indicators_by_metric
+from .column_indicators import column_indicators
 from .column_plot import column_plot
 
 
@@ -30,19 +30,26 @@ def column_chart(
     directory="./",
     metric="num_documents",
     title=None,
-    file_name="documents.csv",
+    database="documents",
 ):
     """Plots a column chart from a column of a dataframe."""
 
-    indicators = column_indicators_by_metric(
-        column,
-        min_occ=min_occ,
-        max_occ=max_occ,
-        top_n=top_n,
+    indicators = column_indicators(
+        column=column,
         directory=directory,
-        metric=metric,
-        file_name=file_name,
+        database=database,
+        use_filter=True if database == "documents" else False,
+        sep=";",
     )
+
+    indicators = indicators.sort_values(metric, ascending=False)
+
+    if min_occ is not None:
+        indicators = indicators[indicators.num_documents >= min_occ]
+    if max_occ is not None:
+        indicators = indicators[indicators.num_documents <= max_occ]
+    if top_n is not None:
+        indicators = indicators.head(top_n)
 
     return column_plot(
         dataframe=indicators,
