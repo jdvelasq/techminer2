9ee92fb0e0ad29fddbd243765c92022f0cd44055
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 16:29:55 -0500

    Update average_citations_per_year.py

--- techminer2/average_citations_per_year.py
+++ techminer2/average_citations_per_year.py
@@ -9,7 +9,10 @@ with the data.
 >>> directory = "data/"
 >>> file_name = "sphinx/_static/average_citations_per_year.html"
 
->>> average_citations_per_year(directory=directory).write_html(file_name)
+>>> average_citations_per_year(
+...     directory=directory,
+...     database="documents",
+... ).write_html(file_name)
 
 .. raw:: html
 
@@ -19,9 +22,24 @@ with the data.
 from .annual_indicators_plot import annual_indicators_plot
 
 
-def average_citations_per_year(directory="./"):
+def average_citations_per_year(
+    directory="./",
+    database="documents",
+):
+    """Plots the number of average citations per year of publication."""
+
+    if database == "documents":
+        title = "Average citations per Year of Publication"
+    elif database == "references":
+        title = "Average Citations per Year in References"
+    elif database == "cited_by":
+        title = "Average citations per year of citing documents"
+    else:
+        raise ValueError("Invalid database")
+
     return annual_indicators_plot(
         column="mean_global_citations",
-        title="Average citations per year",
+        title=title,
         directory=directory,
+        database=database,
     )
