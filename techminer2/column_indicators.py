69b31daa953cd3ccec234ecf159d01b89a21b8e4
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 11:04:28 -0500

    Update column_indicators.py

--- techminer2/column_indicators.py
+++ techminer2/column_indicators.py
@@ -4,28 +4,30 @@ Column Indicators
 
 >>> from techminer2 import *
 >>> directory = "data/"
->>> column_indicators('authors',directory=directory).head()
-            num_documents  ...  avg_document_global_citations
-authors                    ...                               
-Wojcik D                5  ...                              3
-Rabbani MR              3  ...                             13
-Hornuf L                3  ...                             36
-Parker C                2  ...                            114
-Yuksel S                2  ...                              7
+>>> column_indicators('authors',directory=directory).head() # doctest: +NORMALIZE_WHITESPACE
+             num_documents  ...  local_citations_per_document
+authors                     ...
+Arner DW                 7  ...                             2
+Buckley RP               6  ...                             2
+Zetzsche DA              4  ...                             2
+Barberis JN              4  ...                             3
+Ryan P                   3  ...                             1
 <BLANKLINE>
-[5 rows x 4 columns]
+[5 rows x 5 columns]
+
 
 >>> from pprint import pprint
 >>> pprint(column_indicators('authors',directory=directory).columns.to_list())
 ['num_documents',
- 'global_citations',
  'local_citations',
- 'avg_document_global_citations']
+ 'global_citations',
+ 'global_citations_per_document',
+ 'local_citations_per_document']
 
 
 """
 
-from ._read_records import read_all_records, read_filtered_records
+from ._read_records import read_records
 
 
 def column_indicators(
@@ -33,24 +35,21 @@ def column_indicators(
     sep=";",
     directory="./",
     database="documents",
+    use_filter=False,
 ):
-    """
-    Column Indicators
-    """
+    """column indicators"""
 
-    if database == "documents":
-        records = read_filtered_records(directory=directory)
-    elif database == "references":
-        records = read_all_records(directory=directory, database=database)
-    else:
-        raise ValueError(
-            "database must be one of 'documents', 'references', or 'cited_by'"
-        )
+    records = read_records(
+        directory=directory, database=database, use_filter=use_filter
+    )
 
     records = records.assign(num_documents=1)
-    records = records[
-        [column, "num_documents", "global_citations", "local_citations"]
-    ].copy()
+    columns = [column, "num_documents"]
+    if "local_citations" in records.columns:
+        columns.append("local_citations")
+    if "global_citations" in records.columns:
+        columns.append("global_citations")
+    records = records[columns]
 
     if sep is not None:
         records[column] = records[column].str.split(sep)
@@ -63,13 +62,18 @@ def column_indicators(
         .sort_values(by="num_documents", ascending=False)
     )
 
-    indicators = indicators.assign(
-        avg_document_global_citations=indicators.global_citations
-        / indicators.num_documents
-    )
-    indicators.avg_document_global_citations = (
-        indicators.avg_document_global_citations.round(2)
-    )
+    if "global_citations" in records.columns:
+        indicators = indicators.assign(
+            global_citations_per_document=(
+                indicators.global_citations / indicators.num_documents
+            ).round(2)
+        )
+    if "local_citations" in records.columns:
+        indicators = indicators.assign(
+            local_citations_per_document=(
+                indicators.local_citations / indicators.num_documents
+            ).round(2)
+        )
 
     indicators = indicators.astype(int)
 
