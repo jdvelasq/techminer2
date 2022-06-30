33c518de3a70e53f0b814374ea48d3817209e676
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 16:19:39 -0500

    Update annual_scientific_production.py

--- techminer2/annual_scientific_production.py
+++ techminer2/annual_scientific_production.py
@@ -9,7 +9,10 @@ with the data.
 >>> directory = "data/"
 >>> file_name = "sphinx/_static/annual_scientific_production.html"
 
->>> annual_scientific_production(directory).write_html(file_name)
+>>> annual_scientific_production(
+...     directory,
+...     database="documents",
+... ).write_html(file_name)
 
 .. raw:: html
 
@@ -19,9 +22,21 @@ with the data.
 from .annual_indicators_plot import annual_indicators_plot
 
 
-def annual_scientific_production(directory="./"):
+def annual_scientific_production(directory="./", database="documents"):
+    """Computes annual scientific production (number of documents per year)."""
+
+    if database == "documents":
+        title = "Annual Scientific Production"
+    elif database == "references":
+        title = "Num Documents per Year in References"
+    elif database == "cited_by":
+        title = "Citting Documents per Year"
+    else:
+        raise ValueError("Invalid database")
+
     return annual_indicators_plot(
         column="num_documents",
-        title="Annual Scientific Production",
+        title=title,
         directory=directory,
+        database=database,
     )
