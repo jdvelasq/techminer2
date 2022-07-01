8fd34fbfa930fcd94ea834c2facfd58a244bd43e
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-06-30 23:08:05 -0500

    most global cited authors

--- techminer2/most_global_cited_authors.py
+++ techminer2/most_global_cited_authors.py
@@ -6,19 +6,26 @@ Most global cited authors
 >>> directory = "data/"
 >>> file_name = "sphinx/_static/most_global_cited_authors.html"
 
-
 >>> most_global_cited_authors(
 ...     directory,
 ...     top_n=20,
-...     plot="bar",
+...     min_occ=None,
+...     max_occ=None,
+...     plot="cleveland",
+...     database="documents",
 ... ).write_html(file_name)
 
 .. raw:: html
 
-    <iframe src="_static/global_citations_by_author.html" height="600px" width="100%" frameBorder="0"></iframe>
+    <iframe src="_static/most_global_cited_authors.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
-from .plot_metric_by_item import plot_metric_by_item
+from .bar_chart import bar_chart
+from .circle_chart import circle_chart
+from .cleveland_chart import cleveland_chart
+from .column_chart import column_chart
+from .line_chart import line_chart
+from .word_cloud import word_cloud
 
 
 def most_global_cited_authors(
@@ -26,19 +33,38 @@ def most_global_cited_authors(
     top_n=20,
     min_occ=None,
     max_occ=None,
-    title="Most global cited authors",
     plot="bar",
+    database="documents",
 ):
     """Plots the number of global citations by author using the specified plot."""
 
-    return plot_metric_by_item(
+    if database == "documents":
+        title = "Most Global Cited Authors"
+    elif database == "references":
+        title = "Most Global Cited Authors in References"
+    elif database == "cited_by":
+        title = "Most Global Cited Authors in citing documents"
+    else:
+        raise ValueError(
+            "Invalid database name. Database must be one of: 'documents', 'references', 'cited_by'"
+        )
+
+    plot_function = {
+        "bar": bar_chart,
+        "column": column_chart,
+        "line": line_chart,
+        "circle": circle_chart,
+        "cleveland": cleveland_chart,
+        "wordcloud": word_cloud,
+    }[plot]
+
+    return plot_function(
         column="authors",
-        metric="global_citations",
-        directory=directory,
-        top_n=top_n,
         min_occ=min_occ,
         max_occ=max_occ,
+        top_n=top_n,
+        directory=directory,
+        metric="global_citations",
         title=title,
-        plot=plot,
-        file_name="documents.csv",
+        database=database,
     )
