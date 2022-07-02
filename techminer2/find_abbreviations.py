a31312d794dc81010c70618b758692a9645134df
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-01 22:45:19 -0500

    Update find_abbreviations.py

--- techminer2/find_abbreviations.py
+++ techminer2/find_abbreviations.py
@@ -6,39 +6,138 @@ Finds string abbreviations in keywords.
 
 >>> from techminer2 import *
 >>> directory = "data/"
->>> find_abbreviations(directory=directory)
-ai
-    ai
+
+>>> find_abbreviations(
+...     "author_keywords.txt",
+...     directory=directory,
+... )
 artificial intelligence
     artificial intelligence
     artificial intelligence (ai)
-competition
-    competition
-    competition (economics)
-economics
-    economics
+artificial intelligence (ai) governance
+    artificial intelligence (ai) governance
+ai
+    ai
+    ai strategy
+aml
+    aml
+anti-money laundering
+    anti money laundering
+    anti money laundering (aml)
+    anti-money laundering
+    anti-money laundering (aml)
+bank regulation
+    bank regulation
+    bank regulation (basel iii)
+    bank regulations
+    banking regulation
+basel iii
+    basel iii
+compliance
+    compliance
+    compliance (grc)
+grc
+    grc
+contracting
+    contracting
+    contracts
+    contracts (htlc)
+hashed timelock contracts (htlc)
+    hashed timelock contracts (htlc)
+dao
+    dao
+decentralized autonomous organization
+    decentralized autonomous organization
+    decentralized autonomous organization (dao)
+dapp
+    dapp
+    dapps
+distributed applications
+    distributed applications
+    distributed applications (dapps)
+distributed ledger technologies
+    distributed ledger technologies
+    distributed ledger technology
+    distributed ledger technology (dlt)
+dlt
+    dlt
 financial technologies
     financial technologies
+    financial technologies (fintech)
     financial technology
     financial technology (fintech)
 fintech
     fintech
+    fintechs
+gdpr
+    gdpr
+general data protection regulation
+    general data protection regulation
+    general data protection regulation (gdpr)
+ict
+    ict
+information and communication technologies
+    information and communication technologies
+    information and communication technology (ict)
+information technologies
+    information technologies
+    information technology
+    information technology (it)
+it
+    it
+ico
+    ico
+    icos
+initial coin offering
+    initial coin offering
+    initial coin offering (ico)
+    initial coin offerings
 internet of things
     internet of things
     internet of things (iot)
 iot
     iot
-p2p lending
-    p2p lending
+know your customer (kyc) compliance
+    know your customer (kyc) compliance
+youre your customer (kyc)
+    youre your customer (kyc)
+natural language processing
+    natural language processing
+    natural language processing (nlp)
+nlp
+    nlp
+partial least squares structural equation modeling (pls-sem)
+    partial least squares structural equation modeling (pls-sem)
+pls-sem
+    pls-sem
+p2p
+    p2p
+peer-to-peer (p2p) lending risk evaluation
+    peer-to-peer (p2p) lending risk evaluation
 peer-to-peer lending
     peer to peer lending
-    peer-to peer lending (p2p lending)
+    peer-to-peer (p2p) lending
     peer-to-peer lending
-sem
-    sem
-structural equation model (sem)
-    structural equation model (sem)
-    structural equations model
+proof of work (pow)
+    proof of work (pow)
+proof-of-work (pow)
+    proof-of-work (pow)
+regtech
+    regtech
+    regtech.
+regulatory technologies (regtech)
+    regulatory technologies (regtech)
+    regulatory technology
+    regulatory technology (regtech)
+tam
+    tam
+technology acceptance model
+    technology acceptance model
+    technology acceptance model (tam)
+tism
+    tism
+total interpretive structural modeling (tism)
+    total interpretive structural modeling (tism)
 unified theory of acceptance and use technology (utaut)
     unified theory of acceptance and use technology (utaut)
 utaut
@@ -53,12 +152,10 @@ from .thesaurus import load_file_as_dict
 
 
 def find_abbreviations(
+    thesaurus_file,
     directory="./",
 ):
-    """
-    Find abbreviations and reorder the thesaurus to reflect the search.
-
-    """
+    """Find abbreviations and reorder the thesaurus to reflect the search."""
 
     def extract_abbreviation(x):
         if "(" in x:
@@ -67,11 +164,11 @@ def find_abbreviations(
         return None
 
     # ----< Load and reverse the thesaurus >------------------------------------------------------
-    thesaurus_file = join(directory, "processed", "keywords.txt")
-    if isfile(thesaurus_file):
-        th = load_file_as_dict(thesaurus_file)
+    th_file = join(directory, "processed", thesaurus_file)
+    if isfile(th_file):
+        th = load_file_as_dict(th_file)
     else:
-        raise FileNotFoundError("The file {} does not exist.".format(thesaurus_file))
+        raise FileNotFoundError("The file {} does not exist.".format(th_file))
     reversed_th = {value: key for key, values in th.items() for value in values}
 
     # ----< search for abbreviations >-------------------------------------------------------------
@@ -108,7 +205,7 @@ def find_abbreviations(
         th.pop(key)
 
     # ----< save the thesaurus >--------------------------------------------------------------------
-    with open(thesaurus_file, "w", encoding="utf-8") as file:
+    with open(th_file, "w", encoding="utf-8") as file:
 
         for key in findings.keys():
             print(key)
