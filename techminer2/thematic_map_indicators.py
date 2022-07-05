667bd006ef092a97717116c5498318f5d37d3dc9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-05 10:15:11 -0500

    Update thematic_map_indicators.py

--- techminer2/thematic_map_indicators.py
+++ techminer2/thematic_map_indicators.py
@@ -7,15 +7,7 @@ Thematic Map / Indicators
 >>> thematic_map_indicators(
 ...     'author_keywords', min_occ=4, directory=directory
 ... ).head()
-                        num_documents  global_citations  ...  closeness  pagerank
-node                                                     ...                     
-bank                                5                12  ...   0.636364  0.106303
-blockchain                          6                 9  ...   0.636364  0.126858
-covid-19                            6                 8  ...   0.583333  0.109351
-financial inclusion                10                14  ...   0.636364  0.094269
-financial technologies             12                32  ...   0.700000  0.131281
-<BLANKLINE>
-[5 rows x 6 columns]
+
 
 """
 
