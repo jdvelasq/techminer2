3e02fa3becb579508317e851b0145f023befb31b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-05 08:28:23 -0500

    Update __init__.py

--- techminer2/__init__.py
+++ techminer2/__init__.py
@@ -161,6 +161,7 @@ from .source_impact import source_impact
 from .sources_production_over_time import sources_production_over_time
 from .stemming import stemming_and, stemming_or
 from .summary_view import summary_view
+from .terms_by_year import terms_by_year
 from .tf_idf_matrix import tf_idf_matrix
 from .tf_idf_matrix_svd_map import tf_idf_matrix_svd_map
 from .tf_matrix import tf_matrix
