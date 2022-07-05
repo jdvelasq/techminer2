0e44d622cd130bf25090bd135097713269043a24
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-05 10:15:07 -0500

    Update network_indicators.py

--- techminer2/network_indicators.py
+++ techminer2/network_indicators.py
@@ -14,15 +14,7 @@ Network Indicators
 >>> network_ = network(coc_matrix)
 >>> from techminer2.network_api.network_indicators import network_indicators
 >>> network_indicators(network_).head()
-                  num_documents  global_citations  ...  closeness  pagerank
-node                                               ...                     
-bank                         12               185  ...   0.695652  0.050294
-block-chain                  17               149  ...   0.695652  0.064299
-covid-19                      8                36  ...   0.533333  0.020194
-crowdfunding                  8               116  ...   0.615385  0.030571
-cryptocurrencies              8                36  ...   0.571429  0.039274
-<BLANKLINE>
-[5 rows x 6 columns]
+
 
 """
 
