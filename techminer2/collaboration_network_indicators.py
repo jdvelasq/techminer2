945bb3fc2b77086955e33243c1774046a22247b6
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-05 10:15:03 -0500

    Update collaboration_network_indicators.py

--- techminer2/collaboration_network_indicators.py
+++ techminer2/collaboration_network_indicators.py
@@ -5,15 +5,7 @@ Collaboration Network / Indicators
 >>> from techminer2 import *
 >>> directory = "data/"
 >>> collaboration_network_indicators('authors', min_occ=2, directory=directory).head()
-            num_documents  global_citations  ...  closeness  pagerank
-node                                         ...                     
-Arqawi S                2                 5  ...   0.027027  0.034904
-Bernards N              2                35  ...   0.000000  0.005236
-Budi I                  2                 8  ...   0.054054  0.034904
-Daragmeh A              2                 3  ...   0.027027  0.034904
-Dincer H                2                15  ...   0.054054  0.038757
-<BLANKLINE>
-[5 rows x 6 columns]
+
 
 """
 from .co_occurrence_network_indicators import co_occurrence_network_indicators
