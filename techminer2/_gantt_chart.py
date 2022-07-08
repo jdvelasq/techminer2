93ab8d0c0f49a5cfe6bcafdbe51abf85fffce683
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-07 22:01:29 -0500

    Update _gantt_chart.py

--- techminer2/_gantt_chart.py
+++ techminer2/_gantt_chart.py
@@ -3,7 +3,7 @@ Gantt Chart
 ===============================================================================
 
 
->>> directory = "data/"
+>>> directory = "data/regtech/"
 >>> file_name = "sphinx/images/_gantt_chart.png"
 >>> from techminer2.annual_occurrence_matrix import annual_occurrence_matrix
 >>> data = annual_occurrence_matrix(column='author_keywords',  min_occ=6, directory=directory)
