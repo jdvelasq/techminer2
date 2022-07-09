b0a5fd4ab32bad6fcfff69a32e234d67962b43cc
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-09 12:06:23 -0500

    average citations per year

--- techminer2/average_citations_per_year.py
+++ techminer2/average_citations_per_year.py
@@ -9,37 +9,51 @@ with the data.
 >>> directory = "data/regtech/"
 >>> file_name = "sphinx/_static/average_citations_per_year.html"
 
->>> average_citations_per_year(
-...     directory=directory,
-...     database="documents",
-... ).write_html(file_name)
+>>> average_citations_per_year(directory).plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="_static/average_citations_per_year.html" height="600px" width="100%" frameBorder="0"></iframe>
 
+
+>>> average_citations_per_year(directory).table_
+   year  cum_OCC  ...  cum_local_citations  mean_local_citations_per_year
+0  2016        2  ...                    5                           -0.0
+1  2017        7  ...                   12                           -0.0
+2  2018       23  ...                   44                           -0.0
+3  2019       37  ...                   57                           -0.0
+4  2020       62  ...                   73                           -0.0
+5  2021       84  ...                   76                           -0.0
+6  2022       94  ...                   76                           -0.0
+<BLANKLINE>
+[7 rows x 12 columns]
+
 """
-from .annual_indicators_plot import annual_indicators_plot
+from .annual_indicators import annual_indicators
+from .time_plot import time_plot
+
+
+class _Result:
+    """Class for general reporting."""
+
+    def __init__(self, plot, table):
+        self.plot_ = plot
+        self.table_ = table
 
 
 def average_citations_per_year(
     directory="./",
-    database="documents",
 ):
-    """Plots the number of average citations per year of publication."""
+    """Average citations per year."""
 
-    if database == "documents":
-        title = "Average citations per Year of Publication"
-    elif database == "references":
-        title = "Average Citations per Year in References"
-    elif database == "cited_by":
-        title = "Average citations per year of citing documents"
-    else:
-        raise ValueError("Invalid database")
+    indicators = annual_indicators(directory)
+    plot = time_plot(
+        indicators,
+        metric="mean_global_citations",
+        title="Average Citations per Year",
+    )
 
-    return annual_indicators_plot(
-        column="mean_global_citations",
-        title=title,
-        directory=directory,
-        database=database,
+    return _Result(
+        plot=plot,
+        table=indicators,
     )
