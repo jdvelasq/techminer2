e0723522fac1fb49d5a2d79c21fb9e9824350322
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-12 13:43:25 -0500

    rpys

--- techminer2/rpys.py
+++ techminer2/rpys.py
@@ -1,129 +1,140 @@
 """
-RPYS (Reference Publication Year Spectroscopy) (TODO)
+RPYS (Reference Publication Year Spectroscopy)
 ===============================================================================
 
 
 >>> from techminer2 import *
 >>> directory = "data/regtech/"
->>> file_name = "sphinx/images/rpys.png"
->>> rpys(directory=directory).savefig(file_name)
+>>> file_name = "sphinx/_static/rpys.html"
 
-.. image:: images/rpys.png
-    :width: 700px
-    :align: center
+
+>>> rpys(directory=directory).plot_.write_html(file_name)
+
+.. raw:: html
+
+    <iframe src="_static/rpys.html" height="600px" width="100%" frameBorder="0"></iframe>
+
+
+>>> rpys(directory=directory).table_.head()
+      Num References  Median
+1937               1    -1.0
+1938               0     0.0
+1939               0     0.0
+1940               0     0.0
+1941               0     0.0
 
 
 """
-from os.path import join
+import os.path
 
-import matplotlib.pyplot as plt
-import matplotlib.ticker as tick
 import pandas as pd
-from matplotlib.ticker import MaxNLocator
-
-from techminer2._read_records import read_all_records
+import plotly.graph_objects as go
 
 
-def _yaxis_format(y_value, y_position):
-    y_formated = "{:1.0f}".format(y_value)
-    return y_formated
+class _Result:
+    def __init__(self):
+        self.plot_ = None
+        self.table_ = None
 
 
 def rpys(
+    directory="./",
     starting_year=None,
     ending_year=None,
-    figsize=(8, 6),
-    directory="./",
-    plot=True,
 ):
-
     references = pd.read_csv(
-        join(directory, "processed", "references.csv"), sep=",", encoding="utf-8"
+        os.path.join(directory, "processed", "_references.csv"),
+        sep=",",
+        encoding="utf-8",
     )
-    references = references["pub_year"]
-    references = references.dropna()
-    references = references.value_counts()
-    max_year = references.index.max()
-    min_year = references.index.min()
-
-    documents = read_all_records(directory)
-    documents = documents["pub_year"]
-    documents = documents.dropna()
-    max_year = max(max_year, documents.max())
-    min_year = min(min_year, documents.min())
-
-    years = list(range(min_year, max_year + 1))
-    num_references = pd.Series(0, index=years)
-    for year in years:
-        if year in references.index:
-            num_references[year] = references[year]
-
-    median = pd.Series(0, index=years)
-    for year in years[4:]:
-        median[year] = num_references.loc[year - 4 : year].median()
-
-    indicator = num_references - median
-
+    indicator = _compute_rpys(references)
     if starting_year is not None:
         indicator = indicator.loc[starting_year:]
-        num_references = num_references.loc[starting_year:]
-
     if ending_year is not None:
         indicator = indicator.loc[:ending_year]
-        num_references = num_references.loc[:ending_year]
 
-    if plot is False:
-        return indicator
+    fig = _plot_rpys(indicator)
 
-    fig = plt.Figure(figsize=figsize)
-    ax = fig.subplots()
+    result = _Result()
+    result.table_ = indicator
+    result.plot_ = fig
+    return result
 
-    ax.plot(
-        num_references.index.astype(str),
-        num_references.values,
-        ".-",
-        color="grey",
-        alpha=0.5,
+
+def _plot_rpys(indicator):
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=indicator.index,
+            y=indicator["Num References"],
+            fill="tozeroy",
+            name="Num References",
+            opacity=0.3,
+            marker_color="lightgrey",  # darkslategray
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=indicator.index,
+            y=indicator["Median"],
+            fill="tozeroy",
+            name="Median",
+            opacity=0.8,
+            marker_color="darkslategray",
+        )
+    )
+    fig.update_layout(
+        paper_bgcolor="white",
+        plot_bgcolor="white",
+        title="Reference Spectroscopy",
     )
 
-    ax.plot(
-        indicator.index.astype(str),
-        indicator.values,
-        ".-",
-        color="k",
-        alpha=1.0,
+    fig.update_traces(
+        marker=dict(
+            size=6,
+            line=dict(color="darkslategray", width=2),
+        ),
+    )
+    fig.update_xaxes(
+        linecolor="gray",
+        linewidth=2,
+        gridcolor="lightgray",
+        griddash="dot",
+    )
+    fig.update_yaxes(
+        linecolor="gray",
+        linewidth=2,
+        gridcolor="lightgray",
+        griddash="dot",
+        title="Cited References",
     )
 
-    ax.set_title(
-        "Reference Publication Year Spectroscopy",
-        fontsize=12,
-        color="dimgray",
-        loc="left",
-    )
-    ax.set_ylabel("Cited References", color="dimgray")
-    ax.set_xlabel("Year", color="dimgray")
-    ax.set_xticklabels(
-        num_references.index.astype(str),
-        rotation=90,
-        horizontalalignment="center",
-        fontsize=7,
-        color="dimgray",
-    )
-    ax.set_yticklabels(
-        ax.get_yticks(),
-        fontsize=7,
-        color="dimgray",
-    )
-
-    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-
-    ax.yaxis.set_major_formatter(
-        tick.FuncFormatter(_yaxis_format),
-    )
-
-    ax.spines["left"].set_color("dimgray")
-    ax.spines["bottom"].set_color("dimgray")
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.grid(alpha=0.5)
     return fig
+
+
+def _compute_rpys(references):
+
+    references = references[["year"]]
+    references = references.dropna()
+    references_by_year = references["year"].value_counts()
+
+    year_min = references_by_year.index.min()
+    year_max = references_by_year.index.max()
+    years = list(range(year_min, year_max + 1))
+
+    indicator = pd.DataFrame(
+        {
+            "Num References": 0,
+        },
+        index=years,
+    )
+
+    indicator.loc[references_by_year.index, "Num References"] = references_by_year
+    indicator = indicator.sort_index(axis=0, ascending=True)
+    indicator["Median"] = (
+        indicator["Num References"].rolling(window=5).median().fillna(0)
+    )
+
+    indicator["Median"] = indicator["Median"] - indicator["Num References"]
+    return indicator
