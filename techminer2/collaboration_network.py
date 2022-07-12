6a9cbcc73b0c8c03b18cb99ad585f9a048e0b296
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-12 17:20:36 -0500

    Update collaboration_network.py

--- techminer2/collaboration_network.py
+++ techminer2/collaboration_network.py
@@ -2,6 +2,20 @@
 Collaboration Network
 ===============================================================================
 
+.. note:: 
+    A collaboration network is a generic co-occurrence network where the analized column
+    is restricted to the following columns in the dataset:
+
+    * Authors.
+
+    * Institutions. 
+
+    * Countries.
+
+    As a consequence, many implemented plots and analysis are valid for analyzing a 
+    co-occurrence network, including heat maps and other plot types.
+
+
 >>> from techminer2 import *
 >>> directory = "data/regtech/"
 
