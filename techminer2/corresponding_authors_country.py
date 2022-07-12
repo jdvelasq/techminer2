56f456eed48858104079e6bf94259860570a98b5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-12 11:46:44 -0500

    corresponding authors country

--- techminer2/corresponding_authors_country.py
+++ techminer2/corresponding_authors_country.py
@@ -1,5 +1,5 @@
 """
-Corresponding author's country (TODO)
+Corresponding Author's Country
 ===============================================================================
 
 
@@ -10,21 +10,21 @@ Corresponding author's country (TODO)
 >>> corresponding_authors_country(
 ...     top_n=20,
 ...     directory=directory,
-... ).write_html(file_name)
+... ).plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="_static/corresponding_authors_country.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 
->>> corresponding_authors_country(directory=directory, plot=False).head()
+>>> corresponding_authors_country(directory=directory, top_n=20).table_.head()
                 single_publication  multiple_publication  mcp_ratio
 countries                                                          
-china                           20                    23   1.150000
-united kingdom                  19                    22   1.157895
-indonesia                       21                     1   0.047619
-united states                    7                    15   2.142857
-australia                        4                    14   3.500000
+United Kingdom                  10                     6   0.600000
+Australia                        4                     9   2.250000
+Germany                          3                     8   2.666667
+United States                    6                     4   0.666667
+Hong Kong                        2                     6   3.000000
 
 
 """
@@ -33,18 +33,39 @@ import plotly.express as px
 from .collaboration_indicators import collaboration_indicators
 
 
-def corresponding_authors_country(top_n=20, directory="./", plot=True):
+class _Results:
+    def __init__(self):
+        self.table_ = None
+        self.plot_ = None
+
+
+def corresponding_authors_country(
+    top_n=20,
+    directory="./",
+):
+    """Corresponding Author's Country"""
+
+    indicators = _make_table(directory)
+    fig = _make_plot(indicators, top_n)
+
+    results = _Results()
+    results.table_ = indicators
+    results.plot_ = fig
+    return results
+
+
+def _make_table(directory):
 
     indicators = collaboration_indicators("countries", directory=directory)
-    indicators = indicators.sort_values(by="num_documents", ascending=False)
+    indicators = indicators.sort_values(by="OCC", ascending=False)
     indicators = indicators[["single_publication", "multiple_publication"]]
+    indicators = indicators.assign(
+        mcp_ratio=indicators["multiple_publication"] / indicators["single_publication"]
+    )
+    return indicators
 
-    if plot is False:
-        indicators = indicators.assign(
-            mcp_ratio=indicators["multiple_publication"]
-            / indicators["single_publication"]
-        )
-        return indicators
+
+def _make_plot(indicators, top_n):
 
     indicators = indicators.head(top_n)
     indicators = indicators.reset_index()
@@ -65,7 +86,7 @@ def corresponding_authors_country(top_n=20, directory="./", plot=True):
         x="Num Documents",
         y="countries",
         color="publication",
-        title="Corresponding author's country",
+        title="Corresponding Author's Country",
         hover_data=["Num Documents"],
         orientation="h",
         color_discrete_map={
@@ -73,14 +94,11 @@ def corresponding_authors_country(top_n=20, directory="./", plot=True):
             "Multiple Publication": "#556f81",
         },
     )
-    # "slategray"
     fig.update_layout(paper_bgcolor="white", plot_bgcolor="white")
     fig.update_yaxes(
         linecolor="gray",
         linewidth=2,
         autorange="reversed",
-        # gridcolor="gray",
-        # griddash="dot",
     )
     fig.update_xaxes(
         linecolor="gray",
