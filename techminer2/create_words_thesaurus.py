8b054a6ffbb978b5eabbf9f4d50fca3651e57707
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-13 15:35:23 -0500

    Update create_words_thesaurus.py

--- techminer2/create_words_thesaurus.py
+++ techminer2/create_words_thesaurus.py
@@ -16,32 +16,23 @@ from .thesaurus import Thesaurus, read_textfile, text_clustering
 
 
 def create_words_thesaurus(directory="./"):
-    """Createa a keywords thesaurus from raw author/index keywords and title/abstact words."""
+    """Creates a words thesaurus from raw author/index keywords and title/abstact words."""
 
     sys.stdout.write(
         "--INFO-- Creating `words.txt` from author/index keywords, and abstract/title words\n"
     )
 
-    words_list = []
     files = list(glob.glob(os.path.join(directory, "processed/_*.csv")))
     for file in files:
         data = pd.read_csv(file, encoding="utf-8")
-        if "raw_author_keywords" in data.columns:
-            words_list += data.raw_author_keywords.tolist()
-        if "raw_index_keywords" in data.columns:
-            words_list += data.raw_index_keywords.tolist()
-        if "raw_title_words" in data.columns:
-            words_list += data.raw_title_words.tolist()
-        if "raw_abstract_words" in data.columns:
-            words_list += data.raw_abstract_words.tolist()
+        words_list = data.raw_words.copy()
 
-    words_list = pd.Series(words_list)
     words_list = words_list.dropna()
     words_list = words_list.str.lower()
     words_list = words_list.str.split(";")
     words_list = words_list.explode()
     words_list = words_list.str.strip()
-    words_list = words_list[words_list.str.len() > 1]
+    words_list = words_list.drop_duplicates()
 
     words_list = words_list.str.replace('"', "")
     words_list = words_list.str.replace(chr(8212), "")
