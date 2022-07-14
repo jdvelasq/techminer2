77d11d95772c50bce1258b1282965b12f52ff41a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-14 18:57:15 -0500

    co occ word association

--- techminer2/co_occ_word_association.py
+++ techminer2/co_occ_word_association.py
@@ -15,7 +15,7 @@ Word Association
 
 .. raw:: html
 
-    <iframe src="_static/radial_diagram.html" height="600px" width="100%" frameBorder="0"></iframe>
+    <iframe src="_static/co_occ_word_association.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 """
 from .bar_plot import bar_plot
@@ -24,7 +24,6 @@ from .co_occ_matrix_list import co_occ_matrix_list
 from .column_plot import column_plot
 from .line_plot import line_plot
 from .pie_plot import pie_plot
-from .wordcloud import wordcloud
 
 
 def co_occ_word_association(
@@ -66,17 +65,24 @@ def co_occ_word_association(
     if top_n is not None:
         matrix_list = matrix_list.head(top_n)
 
+    matrix_list = matrix_list.set_index("column")
+
     plot_function = {
         "bar": bar_plot,
         "column": column_plot,
         "line": line_plot,
         "pie": pie_plot,
         "cleveland": cleveland_plot,
-        "wordcloud": wordcloud,
     }[plot]
 
-    return plot_function(
+    fig = plot_function(
         dataframe=matrix_list,
         metric="OCC",
         title=None,
     )
+    fig.update_layout(
+        yaxis_title=None,
+        margin=dict(l=1, r=1, t=1, b=1),
+    )
+
+    return fig
