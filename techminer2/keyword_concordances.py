34be22a419f6c242a90da16150f2a6396b75dbb7
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-14 22:53:11 -0500

    Update keyword_concordances.py

--- techminer2/keyword_concordances.py
+++ techminer2/keyword_concordances.py
@@ -7,19 +7,23 @@ Abstract concordances exploration tool.
 
 >>> from techminer2 import *
 >>> directory = "data/regtech/"
->>> keyword_concordances('structural equation model', top_n=10, directory=directory)
-- INFO - Abstract concordances report generated.
-                                We analyzed the data with a  structural equation model  (sem) to test the hypotheses, including the relationshi ...
-...  model, five hypotheses were developed and tested using  structural equation model  techniques (sem-pls).
-                              The data analysis method used  structural equation model  with warppls 6.0 software.
-                                      For the analysis, the  structural equation model  (sem) was used.
-      To analyze the causal relationship between variables,  structural equation model  (sem) was implemented.
-         The research design is a quantitative method using  structural equation model  (sem).
-                                         Analysis data with  structural equation model  (sem) using smart pls v2.0.
-                                                             structural equation model  (sem) was used to analyze and verify the study variables.
-... roach using a questionnaire to collect the data and use  structural equation model  to analyse it.
-                                                      Using  structural equation model  to analyze the study's conceptual model, our results co ...
 
+>>> keyword_concordances(
+...     'regtech', 
+...     top_n=10, 
+...     directory=directory,
+... )
+- INFO - Abstract concordances report generated.
+<<<  systems requires increasing the use of and reliance on  regtech .
+                                                             regtech  developments are leading towards a paradigm shift neces >>>
+                                                             regtech  to date has focused on the digitization of manual repor >>>
+                                  However, the potential of  regtech  is far greater  it has the potential to enable a nearly >>>
+<<< d, sets the foundation for a practical understanding of  regtech , and proposes sequenced reforms that could benefit regu >>>
+           Although also not a panacea, the development of " regtech " solutions will help clear away volumes of work that un >>>
+                                                             regtech  will not eliminate policy considerations, nor will it r >>>
+                Nevertheless, a sophisticated deployment of  regtech  should help focus regulatory discretion and public-poli >>>
+                                            Europes road to  regtech  has rested upon four apparently unrelated pillars: (1)  >>>
+<<< hat together they are underpinning the development of a  regtech  ecosystem in europe and will continue to do so.
 
 """
 
@@ -29,7 +33,7 @@ from os.path import isfile, join
 import pandas as pd
 
 from . import logging
-from ._read_records import read_filtered_records
+from ._read_records import read_records
 from .thesaurus import load_file_as_dict
 
 
@@ -40,18 +44,16 @@ def keyword_concordances(
 ):
 
     # ---< Sort abstracts by importance >------------------------------------------------
-    documents = read_filtered_records(directory)
-    record_no2citation = dict(
-        zip(documents["record_no"], documents["global_citations"])
-    )
-    abstracts = pd.read_csv(join(directory, "processed", "abstracts.csv"))
-    abstracts["citations"] = abstracts["record_no"].map(record_no2citation)
+    documents = read_records(directory)
+    article2citation = dict(zip(documents["article"], documents["global_citations"]))
+    abstracts = pd.read_csv(join(directory, "processed", "_abstracts.csv"))
+    abstracts["citations"] = abstracts["article"].map(article2citation)
     abstracts = abstracts.sort_values(
-        ["citations", "record_no", "line_no"], ascending=[False, True, True]
+        ["citations", "article", "line_no"], ascending=[False, True, True]
     )
 
     # ----< loads keywords >-------------------------------------------------------------
-    thesaurus_file = join(directory, "processed", "keywords.txt")
+    thesaurus_file = join(directory, "processed", "words.txt")
     if isfile(thesaurus_file):
         th = load_file_as_dict(thesaurus_file)
     else:
@@ -67,14 +69,14 @@ def keyword_concordances(
     # ---< Selects the abstracts >-------------------------------------------------------
     regex = [r"\b" + word + r"\b" for word in expanded_keywords]
     regex = "|".join(regex)
-    abstracts = abstracts[abstracts.text.str.contains(regex, regex=True)]
-    abstracts = abstracts[["record_no", "text"]]
-    abstracts["text"] = abstracts["text"].str.capitalize()
+    abstracts = abstracts[abstracts.phrase.str.contains(regex, regex=True)]
+    abstracts = abstracts[["article", "phrase"]]
+    abstracts["phrase"] = abstracts["phrase"].str.capitalize()
     for word in expanded_keywords:
-        abstracts["text"] = abstracts["text"].str.replace(
+        abstracts["phrase"] = abstracts["phrase"].str.replace(
             r"\b" + word + r"\b", word.upper(), regex=True
         )
-        abstracts["text"] = abstracts["text"].str.replace(
+        abstracts["phrase"] = abstracts["phrase"].str.replace(
             r"\b" + word.capitalize() + r"\b", word.upper(), regex=True
         )
 
@@ -84,12 +86,12 @@ def keyword_concordances(
         for _, row in abstracts.iterrows():
 
             paragraph = textwrap.fill(
-                row["text"],
+                row["phrase"],
                 width=90,
             )
-            document_id = documents[
-                documents.record_no == row.record_no
-            ].document_id.tolist()[0]
+            document_id = documents[documents.article == row.article].article.tolist()[
+                0
+            ]
 
             print("*** " + document_id, file=out_file)
             print(paragraph, file=out_file)
@@ -102,17 +104,17 @@ def keyword_concordances(
 
     regex = [r"\b" + word + r"\b" for word in expanded_keywords]
     regex = "(" + "|".join(regex) + ")"
-    contexts = abstracts["text"].str.extract(
+    contexts = abstracts["phrase"].str.extract(
         r"(?P<left_context>[\s \S]*)" + regex + r"(?P<right_context>[\s \S]*)"
     )
 
     contexts["left_context"] = contexts["left_context"].fillna("")
     contexts["right_context"] = contexts["right_context"].fillna("")
     contexts["left_context"] = contexts["left_context"].map(
-        lambda x: "... " + x[-56:] if len(x) > 60 else x
+        lambda x: "<<< " + x[-56:] if len(x) > 60 else x
     )
     contexts["right_context"] = contexts["right_context"].map(
-        lambda x: x[:56] + " ..." if len(x) > 60 else x
+        lambda x: x[:56] + " >>>" if len(x) > 60 else x
     )
 
     for _, row in contexts.head(top_n).iterrows():
