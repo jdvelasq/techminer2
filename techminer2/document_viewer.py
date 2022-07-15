91936221dd85d221a78ab1bcfddbb752d56576ca
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-14 21:57:26 -0500

    Update document_viewer.py

--- techminer2/document_viewer.py
+++ techminer2/document_viewer.py
@@ -17,6 +17,43 @@ Document Viewer
     global_citations : 110
         source_title : Journal of Economics and Business
                 year : 2018
+            abstract : the purpose of this paper is to develop an insight and review the effect of fintech
+                       development against the broader environment in financial technology.  we further aim to
+                       offer various perspectives in order to aid the understanding of the disruptive potential of
+                       fintech, and its implications for the wider financial ecosystem.  by drawing upon very
+                       recent and highly topical research on this area this study examines the implications for
+                       financial institutions, and regulation especially when technology poses a challenge to the
+                       global banking and regulatory system.  it is driven by a wide-ranging overview of the
+                       development, the current state, and possible future of fintech.  this paper attempts to
+                       connect practitioner-led and academic research.  while it draws on academic research, the
+                       perspective it takes is also practice-oriented.  it relies on the current academic
+                       literature as well as insights from industry sources, action research and other publicly
+                       available commentaries.  it also draws on professional practitioners roundtable discussions,
+                       and think-tanks in which the author has been an active participant.  we attempt to interpret
+                       banking, and regulatory issues from a behavioural perspective.  the last crisis exposed
+                       significant failures in regulation and supervision.  it has made the financial market law
+                       and compliance a key topic on the current agenda.  disruptive technological change also
+                       seems to be important in investigating regulatory compliance followed by change.  we
+                       contribute to the current literature review on financial and digital innovation by new
+                       entrants where this has also practical implications.  we also provide for an updated review
+                       of the current regulatory issues addressing the contextual root causes of disruption within
+                       the financial services domain.  the aim here is to assist market participants to improve
+                       effectiveness and collaboration.  the difficulties arising from extensive regulation may
+                       suggest a more liberal and principled approach to financial regulation.  disruptive
+                       innovation has the potential for welfare outcomes for consumers, regulatory, and supervisory
+                       gains as well as reputational gains for the financial services industry.  it becomes even
+                       more important as the financial services industry evolves.  for example, the preparedness of
+                       the regulators to instil culture change and harmonise technological advancements with
+                       regulation could likely achieve many desired outcomes.  such results range from achieving an
+                       orderly market growth, further aiding systemic stability and restoring trust and confidence
+                       in the financial system.  our action-led research results have implications for both
+                       research and practice.  these should be of interest to regulatory standard setters,
+                       investors, international organisations and other academics who are researching regulatory
+                       and competition issues, and their manifestation within the financial and social contexts.
+                       as a perspective on a social construct, this study appeals to regulators and law makers,
+                       entrepreneurs, and investors who participate in technology applied within the innovative
+                       financial services domain.  it is also of interest to bankers who might consider fintech and
+                       strategic partnerships as a prospective, future strategic direction.1  2018 elsevier inc.
  raw_author_keywords : business models
                        financial services
                        fintech
