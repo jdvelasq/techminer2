911b89a0c74073763edfc24b54046843b04722f9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-25 15:03:30 -0500

    scientopy__word_cloud

--- techminer2/scientopy__time_line.py
+++ techminer2/scientopy__time_line.py
@@ -10,7 +10,7 @@ ScientoPy Time Line Plot.
 >>> file_name = "sphinx/_static/scientopy__time_line-1.html"
 >>> from techminer2 import scientopy__time_line
 >>> time_line = scientopy__time_line(
-...     column="author_keywords",
+...     criterion="author_keywords",
 ...     topics_length=5,
 ...     directory=directory,
 ... )
@@ -21,39 +21,6 @@ ScientoPy Time Line Plot.
     <iframe src="../_static/scientopy__time_line-1.html" height="800px" width="100%" frameBorder="0"></iframe>
 
 
-**'skip_first' argument.**
-
->>> time_line.table_.head()
-   Year Author Keywords  OCC
-0  2016         regtech    1
-1  2017         regtech    3
-2  2018         regtech   14
-3  2019         regtech   13
-4  2020         regtech   18
-
-
->>> file_name = "sphinx/_static/scientopy__time_line-2.html"
->>> from techminer2 import scientopy__time_line
->>> time_line = scientopy__time_line(
-...     column="author_keywords",
-...     topics_length=5,
-...     skip_first=2,
-...     directory=directory,
-... )
->>> time_line.plot_.write_html(file_name)
-
-.. raw:: html
-
-    <iframe src="../_static/scientopy__time_line-2.html" height="800px" width="100%" frameBorder="0"></iframe>
-
->>> time_line.table_.head()
-   Year Author Keywords  OCC
-0  2016      blockchain    0
-1  2017      blockchain    2
-2  2018      blockchain    2
-3  2019      blockchain    5
-4  2020      blockchain    4
-
 
 
 **Time Filter.**
@@ -61,7 +28,7 @@ ScientoPy Time Line Plot.
 >>> file_name = "sphinx/_static/scientopy__time_line-3.html"
 >>> from techminer2 import scientopy__time_line
 >>> time_line = scientopy__time_line(
-...     column="author_keywords",
+...     criterion="author_keywords",
 ...     topics_length=5,
 ...     start_year=2018,
 ...     end_year=2021,
@@ -89,7 +56,7 @@ ScientoPy Time Line Plot.
 >>> file_name = "sphinx/_static/scientopy__time_line-4.html"
 >>> from techminer2 import scientopy__time_line
 >>> time_line = scientopy__time_line(
-...     column="author_keywords",
+...     criterion="author_keywords",
 ...     custom_topics=["fintech", "blockchain", "financial regulation", "machine learning"],
 ...     directory=directory,
 ... )
@@ -120,32 +87,32 @@ class _Results:
 
 
 def scientopy__time_line(
-    column,
+    criterion,
     topics_length=5,
     directory="./",
     start_year=None,
     end_year=None,
-    skip_first=0,
     custom_topics=None,
     title=None,
+    **filters,
 ):
     """ScientoPy Bar Trend."""
 
     results = _Results()
     column_, results.table_ = _make_table(
-        column=column,
+        criterion=criterion,
         directory=directory,
         topics_length=topics_length,
         start_year=start_year,
         end_year=end_year,
-        skip_first=skip_first,
         custom_topics=custom_topics,
+        **filters,
     )
     results.plot_ = _make_plot(column_, results.table_, title)
     return results
 
 
-def _make_plot(column, indicators, title):
+def _make_plot(criterion, indicators, title):
 
     fig = px.line(
         indicators,
@@ -153,8 +120,8 @@ def _make_plot(column, indicators, title):
         y="OCC",
         title=title,
         markers=True,
-        hover_data=[column, "Year", "OCC"],
-        color=column,
+        hover_data=[criterion, "Year", "OCC"],
+        color=criterion,
     )
     fig.update_traces(
         marker=dict(size=10, line=dict(color="darkslategray", width=2)),
@@ -182,22 +149,27 @@ def _make_plot(column, indicators, title):
 
 
 def _make_table(
-    column,
+    criterion,
     directory,
     topics_length,
     start_year,
     end_year,
-    skip_first,
     custom_topics,
+    **filters,
 ):
 
     indicators = indicators_by_topic_per_year(
-        directory=directory, criterion=column, use_filter=False
+        directory=directory,
+        criterion=criterion,
+        start_year=start_year,
+        end_year=end_year,
+        **filters,
     )
+
     indicators = indicators[["OCC"]]
     indicators = indicators.reset_index()
-    indicators = indicators.sort_values([column, "year"], ascending=True)
-    indicators = indicators.pivot(index="year", columns=column, values="OCC")
+    indicators = indicators.sort_values([criterion, "year"], ascending=True)
+    indicators = indicators.pivot(index="year", columns=criterion, values="OCC")
     indicators = indicators.fillna(0)
 
     # complete missing years
@@ -211,13 +183,6 @@ def _make_table(
     indicators = indicators.append(pdf)
     indicators = indicators.sort_index()
 
-    ## Apply year filters. The number of occurrences is affected
-    ## by the number of years in the time line.
-    if start_year is not None:
-        indicators = indicators[indicators.index >= start_year]
-    if end_year is not None:
-        indicators = indicators[indicators.index <= end_year]
-
     ## Count and sort topics
     occ = indicators.sum(axis=0)
     occ = occ.sort_values(ascending=False)
@@ -227,8 +192,6 @@ def _make_table(
         custom_topics = [topic for topic in custom_topics if topic in occ.index]
     else:
         custom_topics = occ.index.copy()
-        if skip_first > 0:
-            custom_topics = custom_topics[skip_first:]
         custom_topics = custom_topics[:topics_length]
 
     ## Create table
@@ -243,13 +206,13 @@ def _make_table(
     indicators = indicators.melt(
         id_vars="year",
         value_vars=indicators.columns,
-        var_name=column,
+        var_name=criterion,
         value_name="OCC",
     )
-    indicators[column] = indicators[column].apply(_shorten)
+    indicators[criterion] = indicators[criterion].apply(_shorten)
 
-    column_ = column.replace("_", " ").title()
-    indicators = indicators.rename(columns={"year": "Year", column: column_})
+    column_ = criterion.replace("_", " ").title()
+    indicators = indicators.rename(columns={"year": "Year", criterion: column_})
 
     return column_, indicators
 
--- techminer2/scientopy__word_cloud.py
+++ techminer2/scientopy__word_cloud.py
@@ -11,7 +11,7 @@ Word Cloud
 >>> from techminer2 import scientopy__word_cloud
 >>> file_name = "sphinx/images/scientopy__word_cloud-1.png"
 >>> scientopy__word_cloud(
-...     column='author_keywords',
+...     criterion='author_keywords',
 ...     title="Author Keywords",
 ...     topics_length=50,
 ...     directory=directory,
@@ -21,20 +21,6 @@ Word Cloud
     :width: 900px
     :align: center
 
-**'skip_first' argument.**
-
->>> from techminer2 import scientopy__word_cloud
->>> file_name = "sphinx/images/scientopy__word_cloud-2.png"
->>> scientopy__word_cloud(
-...     column='author_keywords',
-...     title="Author Keywords",
-...     skip_first=5,
-...     directory=directory,
-... ).savefig(file_name)
-
-.. image:: ../images/scientopy__word_cloud-2.png
-    :width: 900px
-    :align: center
 
 
 **Time Filter.**
@@ -42,7 +28,7 @@ Word Cloud
 >>> from techminer2 import scientopy__word_cloud
 >>> file_name = "sphinx/images/scientopy__word_cloud-3.png"
 >>> scientopy__word_cloud(
-...     column='author_keywords',
+...     criterion='author_keywords',
 ...     title="Author Keywords",
 ...     start_year=2018,
 ...     end_year=2021,
@@ -59,7 +45,7 @@ Word Cloud
 >>> from techminer2 import scientopy__word_cloud
 >>> file_name = "sphinx/images/scientopy__word_cloud-4.png"
 >>> scientopy__word_cloud(
-...     column='author_keywords',
+...     criterion='author_keywords',
 ...     custom_topics=[
 ...         "fintech",
 ...         "blockchain",
@@ -77,38 +63,38 @@ Word Cloud
 
 
 """
+from ._indicators.indicators_by_topic import indicators_by_topic
 from ._plots.word_cloud_for_indicators import word_cloud_for_indicators
-
-# from .scientopy__bar import _compute_indicators, _filter_indicators
+from .scientopy__bar import _filter_indicators
 
 
 def scientopy__word_cloud(
-    column,
+    criterion,
     start_year=None,
     end_year=None,
     topics_length=50,
-    skip_first=0,
     custom_topics=None,
     title=None,
     directory="./",
     database="documents",
     #
     figsize=(12, 12),
+    **filters,
 ):
     """Plots a word cloud from a dataframe."""
 
-    indicators = _compute_indicators(
+    indicators = indicators_by_topic(
+        criterion=criterion,
         directory=directory,
-        column=column,
         database=database,
         start_year=start_year,
         end_year=end_year,
+        **filters,
     )
 
     indicators = _filter_indicators(
         indicators=indicators,
         topics_length=topics_length,
-        skip_first=skip_first,
         custom_topics=custom_topics,
     )
 
