2765f6bc370acfb7f4cc58eb1c82e0fabec29930
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-27 18:42:42 -0500

    Update vantagepoint__list_view.py

--- techminer2/vantagepoint__list_view.py
+++ techminer2/vantagepoint__list_view.py
@@ -1,5 +1,5 @@
 """
-List View (TODO)
+List View
 ===============================================================================
 
 
@@ -8,24 +8,18 @@ List View (TODO)
 
 >>> from techminer2 import vantagepoint__list_view
 >>> vantagepoint__list_view(
-...    column='author_keywords',
-...    min_occ=3,
+...    criterion='author_keywords',
 ...    directory=directory,
-... ).head(10)
-                                   OCC  ...  local_citations_per_document
-author_keywords                         ...                              
-regtech                             70  ...                             0
-fintech                             42  ...                             1
-blockchain                          18  ...                             0
-artificial intelligence             13  ...                             0
-regulatory technologies (regtech)   12  ...                             0
-compliance                          12  ...                             0
-financial technologies               9  ...                             0
-financial regulation                 8  ...                             1
-regulation                           6  ...                             3
-machine learning                     6  ...                             0
+... ).head()
+                         OCC  ...  local_citations_per_document
+author_keywords               ...                              
+regtech                   69  ...                             0
+fintech                   42  ...                             1
+blockchain                18  ...                             0
+artificial intelligence   13  ...                             0
+regulatory technology     12  ...                             0
 <BLANKLINE>
-[10 rows x 5 columns]
+[5 rows x 5 columns]
 
 
 >>> from pprint import pprint
@@ -41,31 +35,20 @@ from ._indicators.indicators_by_topic import indicators_by_topic
 
 
 def vantagepoint__list_view(
-    column,
-    metric="OCC",
-    top_n=None,
-    min_occ=None,
-    max_occ=None,
+    criterion,
     directory="./",
     database="documents",
+    start_year=None,
+    end_year=None,
+    **filters,
 ):
     """Creates a list of terms with indicators."""
 
-    indicators = indicators_by_topic(
-        criterion=column,
+    return indicators_by_topic(
+        criterion=criterion,
         directory=directory,
         database=database,
-        use_filter=(database == "documents"),
-        sep=";",
+        start_year=start_year,
+        end_year=end_year,
+        **filters,
     )
-
-    indicators = indicators.sort_values(metric, ascending=False)
-
-    if min_occ is not None:
-        indicators = indicators[indicators.OCC >= min_occ]
-    if max_occ is not None:
-        indicators = indicators[indicators.OCC <= max_occ]
-    if top_n is not None:
-        indicators = indicators.head(top_n)
-
-    return indicators
