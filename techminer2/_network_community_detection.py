299d513a85635b3c7b2b0f3d09b7da68ad2d305f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-27 18:32:19 -0500

    Update _network_community_detection.py

--- techminer2/_network_community_detection.py
+++ techminer2/_network_community_detection.py
@@ -2,20 +2,25 @@
 Network Community Detection
 ===============================================================================
 
-Builds a network from a matrix
 
->>> from techminer2 import *
+
+
 >>> directory = "data/regtech/"
->>> matrix_list = co_occ_matrix_list(
-...    column='author_keywords',
-...    min_occ=3,
+
+>>> from techminer2 import vantagepoint__co_occ_matrix_list
+>>> from techminer2._matrix_list_2_network_graph import matrix_list_2_network_graph
+>>> from techminer2._get_network_graph_indicators import get_network_graph_indicators
+>>> from techminer2._network_community_detection import network_community_detection
+
+>>> matrix_list = vantagepoint__co_occ_matrix_list(
+...    criterion='author_keywords',
+...    topics_length=3,
 ...    directory=directory,
 ... )
 
->>> from techminer2.co_occ_network import co_occ_network
->>> graph = co_occ_network(matrix_list)
->>> from techminer2.network_community_detection import network_community_detection
->>> network_community_detection(graph, method='louvain') # doctest: +ELLIPSIS
+>>> graph = matrix_list_2_network_graph(matrix_list) 
+>>> graph = network_community_detection(graph, method='louvain') # doctest: +ELLIPSIS
+>>> graph
 <networkx.classes.graph.Graph ...
 
 """
