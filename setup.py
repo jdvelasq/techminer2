0f1d75af82e3a395387c782214a3d0a4710d129a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-28 09:27:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from setuptools.command.build_py import build_py
 class BuildPyCommand(build_py):
     def run(self):
         #
-        import nltk
+        # import nltk
 
         #
         # nltk.download("stopwords")
-        # nltk.download("wordnet")
+        # nltk.download("punkt")
+        # nltk.download("brown")
         # nltk.download("averaged_perceptron_tagger")
-        nltk.download("punkt")
+        # nltk.download("punkt")
         #
         build_py.run(self)
 
@@ -33,20 +34,25 @@ setup(
         # "graphviz==0.10.1",
         # "ipykernel==4.10.1",
         # "ipywidgets==7.6.5",
-        "matplotlib==3.5.1",
-        # "networkx==2.6.3",
+        # "matplotlib==3.5.2",
+        # "networkx==2.8.4",
         # "nltk==3.2.5",
         # "numpy==1.19.5",
         # "pandas==1.1.5",
-        "PyYAML==5.4.1",
+        # "PyYAML==5.4.1",
         # "scikit-learn==1.0.2",
         # "seaborn==0.11.2",
         # "textblob==0.15.3",
         # "wordcloud==1.5.0",
         # not in colab:
-        "cdlib==0.2.5",
+        # "sumy",
+        # "itables==1.0.0",
+        # "fuzzywuzzy==0.18.0",
+        # "dash==2.5.1",
+        "cdlib==0.2.6",
         "squarify==0.4.3",
         "python-Levenshtein==0.12.2",
+        "wordcloud==1.8.1",
     ],
     packages=[
         "techminer2",
