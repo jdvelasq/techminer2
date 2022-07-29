ed44fb9c228097fd238536358018b02ff4a198a0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-29 13:16:53 -0500

    Update vantagepoint__cross_corr_matrix.py

--- techminer2/vantagepoint__cross_corr_matrix.py
+++ techminer2/vantagepoint__cross_corr_matrix.py
@@ -8,60 +8,56 @@ Cross-correlation Matrix (TODO)
 
 >>> from techminer2 import vantagepoint__cross_corr_matrix
 >>> vantagepoint__cross_corr_matrix(
-...     'authors', 
-...     by='countries',
-...     top_n=10,
+...     criterion_for_columns = 'authors', 
+...     criterion_for_rows='countries',
+...     topics_length=10,
 ...     directory=directory,
 ... )
-                   Arner DW 7:220  ...  Zetzsche DA 4:092
-Arner DW 7:220           1.000000  ...           0.943564
-Barberis JN 4:146        0.930115  ...           0.758266
-Brennan R 3:008         -0.246183  ...          -0.250873
-Buckley RP 6:217         0.995464  ...           0.958911
-Crane M 2:008           -0.246183  ...          -0.250873
-Das SR 2:028            -0.246183  ...          -0.250873
-Hamdan A 2:011          -0.436436  ...          -0.444750
-Ryan P 3:008            -0.246183  ...          -0.250873
-Turki M 2:011           -0.436436  ...          -0.444750
-Zetzsche DA 4:092        0.943564  ...           1.000000
+column             Arner DW 7:220  ...  Das SR 2:028
+column                             ...              
+Arner DW 7:220           1.000000  ...           0.0
+Buckley RP 6:217         0.994244  ...           0.0
+Barberis JN 4:146        0.921304  ...           0.0
+Zetzsche DA 4:092        0.927105  ...           0.0
+Brennan R 3:008          0.000000  ...           0.0
+Ryan P 3:008             0.000000  ...           0.0
+Crane M 2:008            0.000000  ...           0.0
+Das SR 2:028             0.000000  ...           1.0
 <BLANKLINE>
-[10 rows x 10 columns]
+[8 rows x 8 columns]
 
 """
-from .vantagepoint__co_occ_matrix_list import (  # _select_top_n_items,
-    _add_counters_to_items,
-    _create_matrix_list,
-    _remove_stopwords,
-)
+from .vantagepoint__auto_corr_matrix import _compute_corr_matrix
+from .vantagepoint__occ_matrix import vantagepoint__occ_matrix
 
 
 def vantagepoint__cross_corr_matrix(
-    column,
-    by,
+    criterion_for_columns=None,
+    criterion_for_rows=None,
     method="pearson",
-    top_n=50,
+    topics_length=None,
+    topic_min_occ=None,
+    topic_min_citations=None,
     directory="./",
     database="documents",
+    start_year=None,
+    end_year=None,
+    **filters,
 ):
 
-    matrix_list = _create_matrix_list(
-        criterion=column,
-        row=by,
+    data_matrix = vantagepoint__occ_matrix(
+        criterion_for_columns=criterion_for_columns,
+        criterion_for_rows=criterion_for_rows,
+        topics_length=topics_length,
+        topic_min_occ=topic_min_occ,
+        topic_min_citations=topic_min_citations,
         directory=directory,
         database=database,
+        start_year=start_year,
+        end_year=end_year,
+        **filters,
     )
 
-    matrix_list = _remove_stopwords(directory, matrix_list)
-    matrix_list = _add_counters_to_items(
-        column, "column", directory, database, matrix_list
-    )
-    matrix_list = _add_counters_to_items(by, "row", directory, database, matrix_list)
-    matrix_list = _select_top_n_items(top_n, matrix_list, "column")
+    corr_matrix = _compute_corr_matrix(method, data_matrix)
 
-    matrix = matrix_list.pivot(index="row", columns="column", values="OCC")
-    matrix = matrix.fillna(0)
-    matrix = matrix.corr(method=method)
-    matrix.columns = matrix.columns.to_list()
-    matrix.index = matrix.index.to_list()
-
-    return matrix
+    return corr_matrix
