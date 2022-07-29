100a95ca3a8a35fe5627d20cd5f01b1214aa784d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-29 11:59:05 -0500

    Update vantagepoint__tf_idf_matrix.py

--- techminer2/vantagepoint__tf_idf_matrix.py
+++ techminer2/vantagepoint__tf_idf_matrix.py
@@ -1,5 +1,5 @@
 """
-TF-IDF Matrix (TODO)
+TF-IDF Matrix
 ===============================================================================
 
 
@@ -8,17 +8,17 @@ TF-IDF Matrix (TODO)
 
 >>> from techminer2 import vantagepoint__tf_idf_matrix
 >>> vantagepoint__tf_idf_matrix(
-...     'authors', 
-...     min_occ=2, 
+...     criterion='authors',
+...     topic_min_occ=2,
 ...     directory=directory,
 ... ).head()
-                                                    Arner DW 7:220  ...  Lin W 2:007
-article                                                             ...             
-Arner DW, 2017, HANDB OF BLOCKCHAIN, DIGIT FINA...        0.522776  ...          0.0
-Arner DW, 2017, NORTHWEST J INTL LAW BUS, V37, ...        0.522776  ...          0.0
-Arner DW, 2019, EUR BUS ORG LAW REV, V20, P55             0.439335  ...          0.0
-Arner DW, 2020, EUR BUS ORG LAW REV, V21, P7              0.522776  ...          0.0
-Barberis JN, 2016, NEW ECON WINDOWS, P69                  0.629705  ...          0.0
+authors                                             Arner DW 7:220  ...  Mayer N 2:002
+article                                                             ...               
+Arner DW, 2017, HANDB OF BLOCKCHAIN, DIGIT FINA...        0.522776  ...            0.0
+Arner DW, 2017, NORTHWEST J INTL LAW BUS, V37, ...        0.522776  ...            0.0
+Arner DW, 2019, EUR BUS ORG LAW REV, V20, P55             0.439335  ...            0.0
+Arner DW, 2020, EUR BUS ORG LAW REV, V21, P7              0.522776  ...            0.0
+Barberis JN, 2016, NEW ECON WINDOWS, P69                  0.629705  ...            0.0
 <BLANKLINE>
 [5 rows x 15 columns]
 
@@ -30,15 +30,21 @@ from .vantagepoint__tf_matrix import vantagepoint__tf_matrix
 
 
 def vantagepoint__tf_idf_matrix(
-    column,
-    min_occ=None,
-    max_occ=None,
+    criterion,
+    topics_length=None,
+    topic_min_occ=None,
+    topic_min_citations=None,
+    custom_topics=None,
     scheme=None,
+    directory="./",
+    database="documents",
+    start_year=None,
+    end_year=None,
     norm="l2",
     use_idf=True,
     smooth_idf=True,
     sublinear_tf=False,
-    directory="./",
+    **filters,
 ):
     """
     Compute TF-IDF matrix.
@@ -47,16 +53,25 @@ def vantagepoint__tf_idf_matrix(
     ----------
     """
     tfmatrix = vantagepoint__tf_matrix(
-        directory=directory,
-        criterion=column,
-        topic_min_occ=min_occ,
-        topic_min_citations=max_occ,
+        criterion=criterion,
+        topics_length=topics_length,
+        topic_min_occ=topic_min_occ,
+        topic_min_citations=topic_min_citations,
+        custom_topics=custom_topics,
         scheme=scheme,
+        directory=directory,
+        database=database,
+        start_year=start_year,
+        end_year=end_year,
+        **filters,
     )
 
     values = (
         TfidfTransformer(
-            norm=norm, use_idf=use_idf, smooth_idf=smooth_idf, sublinear_tf=sublinear_tf
+            norm=norm,
+            use_idf=use_idf,
+            smooth_idf=smooth_idf,
+            sublinear_tf=sublinear_tf,
         )
         .fit_transform(tfmatrix)
         .toarray()
