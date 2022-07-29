1d23d3650609908453a1109dce2320a0cb66526f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-29 13:25:38 -0500

    Update vantagepoint__apply_thesaurus.py

--- techminer2/vantagepoint__apply_thesaurus.py
+++ techminer2/vantagepoint__apply_thesaurus.py
@@ -6,21 +6,21 @@ Apply a thesaurus to a column
 >>> from techminer2 import *
 >>> directory = "data/regtech/"
 
->>> create_thesaurus(
-...     column="author_keywords",
+>>> from techminer2 import vantagepoint__create_thesaurus
+>>> vantagepoint__create_thesaurus(
+...     criterion="author_keywords",
 ...     output_file="test_keywords.txt",
 ...     directory=directory,
 ... )
 --INFO-- Creating a thesaurus file from `author_keywords` column in all databases
---INFO-- The thesaurus file data/processed/test_keywords.txt was created
+--INFO-- The thesaurus file `test_keywords.txt` was created
 
 
 >>> from techminer2 import vantagepoint__apply_thesaurus
 >>> vantagepoint__apply_thesaurus(
 ...     thesaurus_file="test_keywords.txt",
-...     input_column="author_keywords",
-...     output_column="author_keywords_thesaurus",
-...     strict=False,
+...     input_criterion="author_keywords",
+...     output_criterion="author_keywords_thesaurus",
 ...     directory=directory,
 ... )
 --INFO-- The thesaurus was applied to all databases
@@ -34,14 +34,12 @@ import sys
 import pandas as pd
 
 from ._thesaurus import read_textfile
-from .map_ import map_
 
 
 def vantagepoint__apply_thesaurus(
     thesaurus_file,
-    input_column,
-    output_column,
-    strict,
+    input_criterion,
+    output_criterion,
     directory="./",
 ):
     """Apply a thesaurus to a column of a dataframe."""
@@ -55,20 +53,20 @@ def vantagepoint__apply_thesaurus(
     for file in files:
         data = pd.read_csv(file, encoding="utf-8")
         #
-        if input_column in data.columns:
-            data[output_column] = data[input_column].str.split(";")
+        if input_criterion in data.columns:
+            data[output_criterion] = data[input_criterion].str.split(";")
 
-            data[output_column] = data[output_column].map(
-                lambda x: [thesaurus.apply_as_dict(y.strip()) for y in x]
+            data[output_criterion] = data[output_criterion].map(
+                lambda x: [th.apply_as_dict(y.strip()) for y in x]
                 if isinstance(x, list)
                 else x
             )
 
-            data[output_column] = data[output_column].map(
+            data[output_criterion] = data[output_criterion].map(
                 lambda x: sorted(set(x)) if isinstance(x, list) else x
             )
 
-            data[output_column] = data[output_column].str.join("; ")
+            data[output_criterion] = data[output_criterion].str.join("; ")
 
         #
         data.to_csv(file, sep=",", encoding="utf-8", index=False)
@@ -93,4 +91,4 @@ def vantagepoint__apply_thesaurus(
     #         data[output_column] = map_(data, input_column, apply_unstrict)
     #     data.to_csv(file, sep=",", encoding="utf-8", index=False)
 
-    # sys.stdout.write("--INFO-- The thesaurus was applied to all databases\n")
+    sys.stdout.write("--INFO-- The thesaurus was applied to all databases\n")
