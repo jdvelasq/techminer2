05177bcfac051b266c85b0f4892425f770605e4f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-07-30 10:59:10 -0500

    Update tlab__word_associations_mds_map.py

--- techminer2/tlab__word_associations_mds_map.py
+++ techminer2/tlab__word_associations_mds_map.py
@@ -1,5 +1,5 @@
 """
-Word Associations MDS Map
+MDS Map
 ===============================================================================
 
 Plots the SVD of the co-occurrence matrix normalized with the **salton** measure.
