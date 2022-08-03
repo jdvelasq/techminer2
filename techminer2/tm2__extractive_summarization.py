548398ff43734fa71aa94380f802c030a0076a3c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-08-03 08:58:01 -0500

    Update tm2__extractive_summarization.py

--- techminer2/tm2__extractive_summarization.py
+++ techminer2/tm2__extractive_summarization.py
@@ -8,34 +8,16 @@ Abstracts Extractive Summarization
 >>> from techminer2 import tm2__extractive_summarization
 >>> tm2__extractive_summarization(
 ...     criterion="author_keywords",
-...     custom_topics=["blockchain"],
-...     n_abstracts=20,    
-...     n_phrases_per_algorithm=20,
+...     custom_topics=["blockchain", "artificial intelligence"],
+...     n_abstracts=50,    
+...     n_phrases_per_algorithm=50,
 ...     directory=directory,
 ... )
-extractive_summarization.txt
-authors of this paper investigate how blockchain can be used to secure stock exchange
-transactions, with an especial focus to the technological as well as legal aspects of such
-applications. >> blockchain (bc) technology, being distributed and immutable in nature,
-has proved to the 'trust machine' eliminating the need for third-parties. >> however, to
-create valid legal effects blockchains should be anchored prudently in the surrounding
-legal context and specific regulations will be supportive to unfold their potential in a
-sustainable way and in an international context. >> in this work, we propose a blockchain-
-based regtech system which helps to track the credit of organizations. >> pace of
-transition can be seen in particular in the position of emerging technology, also
-summarized as the abcd framework: artificial intelligence ("ai"), blockchain, cloud and
-technology, which are rapidly co-evolving with finance. >> though blockchain technology
-has been leveraged to increase effectiveness of certain corporate banking products, the
-originality of the paper lies in coming out with a detailed framework for the possible use
-of blockchain (a distributed ledger based technology) for credit decisions, timely
-generation of red-flags and tightening the regulatory framework.
-<BLANKLINE>
 
 
 """
-import os
-import textwrap
 
+import pandas as pd
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.kl import KLSummarizer
@@ -44,7 +26,7 @@ from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 
 from ._read_records import read_records
-from .tm2__abstracts_report import _sort_by_custom_terms
+from .tm2__abstracts_report import _sort_by_custom_terms, _write_report
 
 
 def tm2__extractive_summarization(
@@ -52,8 +34,7 @@ def tm2__extractive_summarization(
     custom_topics,
     file_name="extractive_summarization.txt",
     n_abstracts=50,
-    n_phrases_per_algorithm=5,
-    quiet=False,
+    n_phrases_per_algorithm=50,
     directory="./",
     database="documents",
     start_year=None,
@@ -69,11 +50,91 @@ def tm2__extractive_summarization(
         end_year=end_year,
         **filters,
     )
+    records = records.dropna(subset=["abstract"])
 
     records = _select_records(criterion, custom_topics, n_abstracts, records)
-
     document = _create_document(records)
+    summary = _generate_summary(n_phrases_per_algorithm, document)
+    final_summary = _select_phrases_with_keywords(custom_topics, summary)
 
+    ###
+    final_summary = summary
+    ###
+
+    final_summary = _complete_record_info(criterion, records, final_summary)
+
+    final_summary = _sort_criterion_field(
+        records=final_summary, criterion=criterion, custom_topics=custom_topics
+    )
+
+    final_summary = final_summary.rename(columns={"phrase": "abstract"})
+
+    _write_report(
+        criterion=criterion,
+        file_name=file_name,
+        use_textwrap=True,
+        directory=directory,
+        records=final_summary,
+    )
+
+
+def _sort_criterion_field(records, criterion, custom_topics):
+    records["TERMS"] = records[criterion].str.split(";")
+    records["TERMS"] = records["TERMS"].map(lambda x: [y.strip() for y in x])
+    records["TERMS_1"] = records["TERMS"].map(
+        lambda x: [
+            "(*) " + custom_topic for custom_topic in custom_topics if custom_topic in x
+        ],
+        na_action="ignore",
+    )
+    records["TERMS_2"] = records["TERMS"].map(
+        lambda x: [y for y in x if y not in custom_topics], na_action="ignore"
+    )
+    records["TERMS"] = records["TERMS_1"] + records["TERMS_2"]
+    records[criterion] = records["TERMS"].str.join("; ")
+    return records
+
+
+def _complete_record_info(criterion, records, final_summary):
+    summarization = pd.DataFrame({"phrase": final_summary})
+    summarization["article"] = pd.NA
+    for record in records.itertuples():
+        summarization.loc[
+            summarization["phrase"].map(
+                lambda x: x[:50] in record.abstract, na_action="ignore"
+            ),
+            "article",
+        ] = record.article
+
+    summarization = summarization.dropna(subset=["article"])
+
+    records.index = records.article
+
+    summarization["global_citations"] = records.loc[
+        summarization.article, "global_citations"
+    ].tolist()
+
+    summarization["local_citations"] = records.loc[
+        summarization.article, "local_citations"
+    ].tolist()
+
+    summarization["title"] = records.loc[summarization.article, "title"].tolist()
+    summarization[criterion] = records.loc[summarization.article, criterion].tolist()
+
+    return summarization
+
+
+def _select_phrases_with_keywords(custom_topics, summary):
+    final_summary = []
+    for topic in custom_topics:
+        for sentence in summary:
+            if topic in sentence:
+                final_summary.append(sentence)
+    final_summary = list(set(final_summary))
+    return final_summary
+
+
+def _generate_summary(n_phrases_per_algorithm, document):
     summary_with_lexrank = _summarize_with_lexrank(document, n_phrases_per_algorithm)
     summary_with_lsasummarizer = _summarize_with_lsasummarizer(
         document, n_phrases_per_algorithm
@@ -89,29 +150,12 @@ def tm2__extractive_summarization(
         + summary_with_luhn
         + summary_with_klsummarizer
     )
+
+    # summary = [phrase[:-1] for phrase in summary if phrase[-1] == "."]
+    # summary = [text for phrase in summary for text in phrase.split(".")]
+
     summary = list(set(summary))
-
-    final_summary = []
-    for topic in custom_topics:
-        for sentence in summary:
-            if topic in sentence:
-                final_summary.append(sentence)
-
-    final_summary = sorted(set(final_summary))
-
-    final_summary = " >> ".join(final_summary)
-    final_summary = textwrap.fill(final_summary, width=90)
-
-    with open(
-        os.path.join(directory, "reports", file_name), "w", encoding="utf-8"
-    ) as out_file:
-
-        print(final_summary, file=out_file)
-
-    if quiet is False:
-        print(file_name)
-        print(final_summary)
-        print()
+    return summary
 
 
 def _summarize_with_klsummarizer(document, n_phrases_per_algorithm):
@@ -158,19 +202,3 @@ def _select_records(criterion, custom_topics, n_abstracts, records):
     records = _sort_by_custom_terms(criterion, custom_topics, records)
     records = records.head(n_abstracts)
     return records
-
-
-# def _select_records(criterion, custom_topics, n_abstracts, records):
-
-#     selected_records = records[["article", criterion]]
-#     selected_records[criterion] = selected_records[criterion].str.split(";")
-#     selected_records = selected_records.explode(criterion)
-#     selected_records[criterion] = selected_records[criterion].str.strip()
-#     selected_records = selected_records[selected_records[criterion].isin(custom_topics)]
-#     records = records[records["article"].isin(selected_records["article"])]
-
-#     records = records.sort_values(
-#         by=["global_citations", "local_citations"], ascending=False
-#     )
-#     records = records.head(n_abstracts)
-#     return records
