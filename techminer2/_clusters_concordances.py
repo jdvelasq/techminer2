2272d1855da90ac90bf21ec5dd940e21375905c3
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-08-04 11:25:48 -0500

    Create _clusters_concordances.py

---
+++ techminer2/_clusters_concordances.py
@@ -0,0 +1,56 @@
+"""Clusters summarization"""
+import os
+import shutil
+
+from ._create_directory import create_directory
+from .tlab__co_occurrence_analysis__concordances import (
+    tlab__co_occurrence_analysis__concordances,
+)
+
+
+def clusters_concordances(
+    communities,
+    directory_for_concordances,
+    n_keywords=10,
+    n_abstracts=50,
+    directory="./",
+    start_year=None,
+    end_year=None,
+    **filters,
+):
+    """Clusters summarization."""
+
+    create_directory(
+        base_directory=directory,
+        target_directory=directory_for_concordances,
+    )
+
+    communities = communities.head(n_keywords)
+    for i_community, community_name in enumerate(communities):
+
+        path = os.path.join(
+            directory,
+            "reports",
+            directory_for_concordances,
+            "CL_{:02d}".format(i_community),
+        )
+        os.makedirs(path)
+
+        community = communities[community_name].tolist()
+        community = [x for x in community if x != ""]
+        community = [" ".join(x.split()[:-1]) for x in community]
+
+        for member in community:
+            tlab__co_occurrence_analysis__concordances(
+                search_for=member,
+                top_n=n_abstracts,
+                quiet=True,
+                directory=directory,
+                start_year=start_year,
+                end_year=end_year,
+                **filters,
+            )
+
+            src_file = os.path.join(directory, "reports/concordances.txt")
+            dst_file = os.path.join(path, member + ".txt")
+            shutil.copyfile(src_file, dst_file)
