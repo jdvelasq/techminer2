f4098c39d15b72e24186a1df737fa301c6747051
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-08 06:38:55 -0500

    refactor: load_thesaurus

--- techminer2/_load_thesaurus_as_dict_r.py
+++ techminer2/_load_thesaurus_as_dict_r.py
@@ -3,8 +3,17 @@
 from ._load_thesaurus_as_dict import load_thesaurus_as_dict
 
 
+# This function loads thesaurus from file and returns it as dictionary.
+# Each key is a word and each value is a list of synonyms.
+
+
 def load_thesaurus_as_dict_r(filename):
     """Returns reversed thesaurus as dictionary."""
-    dic = load_thesaurus_as_dict(filename)
-    dic = {value: key for key, values in dic.items() for value in values}
-    return dic
+    # load thesaurus as dictionary
+    thesaurus = load_thesaurus_as_dict(filename)
+    # reverse thesaurus
+    reversed_thesaurus = {
+        value: key for key, values in thesaurus.items() for value in values
+    }
+    # return reversed thesaurus
+    return reversed_thesaurus
