18dd823e7b9ed91b7b3bb2aa726497e5f3771a42
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-08 06:38:37 -0500

    refactor: create_directory

--- techminer2/_create_directory.py
+++ techminer2/_create_directory.py
@@ -1,22 +1,38 @@
 """Create a directory."""
-import glob
-import os.path
+import os
 
 
-def create_directory(base_directory, target_directory):
-    """Createa an empty target directory."""
+def create_directory(base_dir, target_dir):
+    """
+    Create an empty target directory.
 
-    path = os.path.join(base_directory, "reports", target_directory)
+    Args:
+        base_dir (str): The base directory.
+        target_dir (str): The target directory to create.
+    """
+    path = os.path.join(base_dir, "reports", target_dir)
     if os.path.exists(path):
-        _remove_dir(path)
+        remove_directory(path)
     os.makedirs(path)
 
 
-def _remove_dir(directory):
-    files = glob.glob(directory + "/*")
-    for file in files:
-        if os.path.isdir(file):
-            _remove_dir(file)
-        else:
-            os.remove(file)
-    os.rmdir(directory)
+def remove_directory(directory):
+    """
+    Remove a directory and all its files.
+
+    Args:
+        directory (str): The directory to remove.
+    """
+    try:
+        for file in os.scandir(directory):
+            if file.is_dir():
+                remove_directory(file.path)
+            else:
+                os.remove(file.path)
+        os.rmdir(directory)
+    except FileNotFoundError as exc:
+        print(f"Directory {directory} not found: {exc}")
+    except PermissionError as exc:
+        print(f"Permission denied when trying to remove {directory}: {exc}")
+    except OSError as exc:
+        print(f"Error while removing directory {directory}: {exc}")
