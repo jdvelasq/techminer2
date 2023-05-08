0ca81bf6a0738ec50cd82ed8effe72d8a673d59d
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-08 06:39:17 -0500

    refactor: matrix_2_matrix_list

--- techminer2/_matrix_2_matrix_list.py
+++ techminer2/_matrix_2_matrix_list.py
@@ -3,16 +3,29 @@
 
 def matrix_2_matrix_list(matrix):
     """
-    Transform a matrix into a matrix list.
+    Convert a matrix to a matrix list.
+
+    Args:
+        matrix (pd.DataFrame): The matrix to convert.
+
+    Returns:
+        pd.DataFrame: The matrix list.
     """
 
+    # Melt the matrix to a long format
     matrix_list = matrix.melt(value_name="OCC", var_name="column", ignore_index=False)
-    matrix_list = matrix_list.reset_index()
-    matrix_list = matrix_list.rename(columns={"index": "row"})
+
+    # Reset the index and rename the index column to "row"
+    matrix_list = matrix_list.reset_index().rename(columns={"index": "row"})
+
+    # Sort the matrix list by "OCC", "row", and "column" columns
     matrix_list = matrix_list.sort_values(
         by=["OCC", "row", "column"], ascending=[False, True, True]
     )
-    matrix_list = matrix_list[matrix_list.OCC.astype(float) != 0.0]
-    matrix_list = matrix_list.reset_index(drop=True)
+
+    # Remove rows with OCC value of 0
+    matrix_list = matrix_list[matrix_list.OCC.astype(float) != 0.0].reset_index(
+        drop=True
+    )
 
     return matrix_list
