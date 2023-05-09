21b44ac1a89c72f8147819f2ed61aa55fa353fe0
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-09 16:59:22 -0500

    FEA chatgpt prompt for analyzing organization impact

--- techminer2/bibliometrix/organizations/organization_impact.py
+++ techminer2/bibliometrix/organizations/organization_impact.py
@@ -20,52 +20,46 @@ Organization Impact
     <iframe src="../../../_static/bibliometrix__organization_impact.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 
->>> r.table_.head()
-             Organizations  ...  Avg Global Citations
-0  University of Hong Kong  ...                 61.67
-1      Coventry University  ...                  8.50
-2         Ahlia University  ...                  6.33
-3  University College Cork  ...                 13.67
-4            ---FinTech HK  ...                 80.50
-<BLANKLINE>
-[5 rows x 10 columns]
+>>> print(r.table_.head().to_markdown())
+|    | Organizations           |   OCC |   Global Citations |   First Pb Year |   Age |   H-Index |   G-Index |   M-Index |   Global Citations Per Year |   Avg Global Citations |
+|---:|:------------------------|------:|-------------------:|----------------:|------:|----------:|----------:|----------:|----------------------------:|-----------------------:|
+|  0 | University of Hong Kong |     3 |                185 |            2017 |     7 |         3 |         3 |      0.43 |                       26.43 |                  61.67 |
+|  1 | ---FinTech HK           |     2 |                161 |            2017 |     7 |         2 |         2 |      0.29 |                       23    |                  80.5  |
+|  2 | University College Cork |     3 |                 41 |            2018 |     6 |         2 |         2 |      0.33 |                        6.83 |                  13.67 |
+|  3 | Ahlia University        |     3 |                 19 |            2020 |     4 |         2 |         2 |      0.5  |                        4.75 |                   6.33 |
+|  4 | Coventry University     |     2 |                 17 |            2020 |     4 |         2 |         1 |      0.5  |                        4.25 |                   8.5  |
+
 
 >>> print(r.prompt_)
 <BLANKLINE>
-Act as a researcher realizing a bibliometric analysis. 
+Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on top 20 organizations with the highest H-Index. 'OCC' represents the number of documents published by the organization in the dataset. Use the information in the table to draw conclusions about the impact and productivity of the organization. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
 <BLANKLINE>
-The following table contains the top 20 Organizations 
-with more H Index in the given bibliographic dataset.
-<BLANKLINE>
-| Organizations                            |   Occ |   Global Citations |   First Pb Year |   Age |   H Index |   G Index |   M Index |   Global Citations Per Year |   Avg Global Citations |
-|:-----------------------------------------|------:|-------------------:|----------------:|------:|----------:|----------:|----------:|----------------------------:|-----------------------:|
-| University of Hong Kong                  |     3 |                185 |            2017 |     7 |         3 |         3 |      0.43 |                       26.43 |                  61.67 |
-| Coventry University                      |     2 |                 17 |            2020 |     4 |         2 |         1 |      0.5  |                        4.25 |                   8.5  |
-| Ahlia University                         |     3 |                 19 |            2020 |     4 |         2 |         2 |      0.5  |                        4.75 |                   6.33 |
-| University College Cork                  |     3 |                 41 |            2018 |     6 |         2 |         2 |      0.33 |                        6.83 |                  13.67 |
-| ---FinTech HK                            |     2 |                161 |            2017 |     7 |         2 |         2 |      0.29 |                       23    |                  80.5  |
-| Dublin City University                   |     2 |                 14 |            2020 |     4 |         2 |         1 |      0.5  |                        3.5  |                   7    |
-| University of Westminster                |     2 |                 17 |            2020 |     4 |         2 |         1 |      0.5  |                        4.25 |                   8.5  |
-| Finance and Banking                      |     1 |                 11 |            2020 |     4 |         1 |         1 |      0.25 |                        2.75 |                  11    |
-| ---AML Forensic library KPMG...          |     1 |                 10 |            2020 |     4 |         1 |         1 |      0.25 |                        2.5  |                  10    |
-| Goethe University Frankfurt              |     1 |                  1 |            2022 |     2 |         1 |         1 |      0.5  |                        0.5  |                   1    |
-| Guru Gobind Singh Indraprastha...        |     1 |                  1 |            2020 |     4 |         1 |         1 |      0.25 |                        0.25 |                   1    |
-| Politecnico di Milano                    |     2 |                  2 |            2020 |     4 |         1 |         1 |      0.25 |                        0.5  |                   1    |
-| Harvard University Weatherhead Center... |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
-| Hebei University of Technology           |     1 |                 13 |            2022 |     2 |         1 |         1 |      0.5  |                        6.5  |                  13    |
-| Heinrich Heine University                |     1 |                 24 |            2020 |     4 |         1 |         1 |      0.25 |                        6    |                  24    |
-| Jiangsu University                       |     1 |                 13 |            2022 |     2 |         1 |         1 |      0.5  |                        6.5  |                  13    |
-| FOM University of Applied Sciences       |     1 |                  5 |            2020 |     4 |         1 |         1 |      0.25 |                        1.25 |                   5    |
-| Macquarie University                     |     1 |                  2 |            2021 |     3 |         1 |         1 |      0.33 |                        0.67 |                   2    |
-| National Chengchi University             |     1 |                  1 |            2017 |     7 |         1 |         1 |      0.14 |                        0.14 |                   1    |
-| Palestine Technical University           |     1 |                  1 |            2021 |     3 |         1 |         1 |      0.33 |                        0.33 |                   1    |
-<BLANKLINE>
-Write a clear and concise paragraph describing the main findings and any 
-important trends or patterns you notice in the previous table. 
-<BLANKLINE>
-Limit your description to a paragraph with no more than 250 words.    
+|    | Organizations                            |   OCC |   Global Citations |   First Pb Year |   Age |   H-Index |   G-Index |   M-Index |   Global Citations Per Year |   Avg Global Citations |
+|---:|:-----------------------------------------|------:|-------------------:|----------------:|------:|----------:|----------:|----------:|----------------------------:|-----------------------:|
+|  0 | University of Hong Kong                  |     3 |                185 |            2017 |     7 |         3 |         3 |      0.43 |                       26.43 |                  61.67 |
+|  1 | ---FinTech HK                            |     2 |                161 |            2017 |     7 |         2 |         2 |      0.29 |                       23    |                  80.5  |
+|  2 | University College Cork                  |     3 |                 41 |            2018 |     6 |         2 |         2 |      0.33 |                        6.83 |                  13.67 |
+|  3 | Ahlia University                         |     3 |                 19 |            2020 |     4 |         2 |         2 |      0.5  |                        4.75 |                   6.33 |
+|  4 | Coventry University                      |     2 |                 17 |            2020 |     4 |         2 |         1 |      0.5  |                        4.25 |                   8.5  |
+|  5 | University of Westminster                |     2 |                 17 |            2020 |     4 |         2 |         1 |      0.5  |                        4.25 |                   8.5  |
+|  6 | Dublin City University                   |     2 |                 14 |            2020 |     4 |         2 |         1 |      0.5  |                        3.5  |                   7    |
+|  7 | ---Kingston Business School              |     1 |                153 |            2018 |     6 |         1 |         1 |      0.17 |                       25.5  |                 153    |
+|  8 | ---Centre for Law                        |     1 |                150 |            2017 |     7 |         1 |         1 |      0.14 |                       21.43 |                 150    |
+|  9 | Duke University School of Law            |     1 |                 30 |            2016 |     8 |         1 |         1 |      0.12 |                        3.75 |                  30    |
+| 10 | ---UNSW Sydney                           |     1 |                 24 |            2020 |     4 |         1 |         1 |      0.25 |                        6    |                  24    |
+| 11 | Heinrich Heine University                |     1 |                 24 |            2020 |     4 |         1 |         1 |      0.25 |                        6    |                  24    |
+| 12 | University of Luxembourg                 |     1 |                 24 |            2020 |     4 |         1 |         1 |      0.25 |                        6    |                  24    |
+| 13 | University of Zurich                     |     1 |                 24 |            2020 |     4 |         1 |         1 |      0.25 |                        6    |                  24    |
+| 14 | ---KS Strategic                          |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
+| 15 | ---Panepistemio Aigaiou                  |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
+| 16 | ---School of Engineering                 |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
+| 17 | European Central Bank                    |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
+| 18 | Harvard University Weatherhead Center... |     1 |                 21 |            2018 |     6 |         1 |         1 |      0.17 |                        3.5  |                  21    |
+| 19 | Hebei University of Technology           |     1 |                 13 |            2022 |     2 |         1 |         1 |      0.5  |                        6.5  |                  13    |
 <BLANKLINE>
 <BLANKLINE>
+<BLANKLINE>
+
 
 """
 from .._impact import _impact
@@ -84,7 +78,7 @@ def organization_impact(
 ):
     """Plots the selected impact measure by organizations."""
 
-    return _impact(
+    obj = _impact(
         criterion="organizations",
         impact_measure=impact_measure,
         topics_length=topics_length,
@@ -98,3 +92,24 @@ def organization_impact(
         end_year=end_year,
         **filters,
     )
+
+    obj.prompt_ = _create_prompt(obj.table_, impact_measure)
+
+    return obj
+
+
+def _create_prompt(table, impact_measure):
+    return f"""
+Imagine that you are a researcher analyzing a bibliographic dataset. The table \
+below provides data on top {table.shape[0]} organizations with the highest \
+{impact_measure.replace("_", "-").title()}. 'OCC' represents the number of \
+documents published by the organization in the dataset. Use the information in the \
+table to draw conclusions about the impact and productivity of the organization. \
+In your analysis, be sure to describe in a clear and concise way, any \
+findings or any patterns you observe, and identify any outliers or anomalies in \
+the data. Limit your description to one paragraph with no more than 250 words.
+
+{table.to_markdown()}
+
+
+"""
