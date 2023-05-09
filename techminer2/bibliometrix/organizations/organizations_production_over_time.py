d11940ca54d6e7751012a5e403cb34ef0b7680c1
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-09 17:30:15 -0500

    FEA chatgpt prompt for analyzing organization production over time

--- techminer2/bibliometrix/organizations/organizations_production_over_time.py
+++ techminer2/bibliometrix/organizations/organizations_production_over_time.py
@@ -18,70 +18,44 @@ Organizations' Production over Time
 
     <iframe src="../../../_static/bibliometrix__organizations_production_over_time.html" height="600px" width="100%" frameBorder="0"></iframe>
 
->>> r.documents_per_organization_.head()
-                                 organizations  ...                            doi
-0               ---Teichmann International  AG  ...  10.1016/J.TECHSOC.2022.102150
-1                           Harvard University  ...  10.1016/J.TECHSOC.2022.102150
-2                        University of Messina  ...  10.1016/J.TECHSOC.2022.102150
-3              Chinese University of Hong Kong  ...    10.1016/J.RIBAF.2022.101868
-4  Nottingham University Business School China  ...    10.1016/J.RIBAF.2022.101868
-<BLANKLINE>
-[5 rows x 7 columns]
+>>> print(r.documents_per_organization_.head().to_markdown())
+|    | organizations                               | title                                                                                        |   year | source_title                                   |   global_citations |   local_citations | doi                           |
+|---:|:--------------------------------------------|:---------------------------------------------------------------------------------------------|-------:|:-----------------------------------------------|-------------------:|------------------:|:------------------------------|
+|  0 | ---Teichmann International  AG              | RegTech  Potential benefits and challenges for businesses                                    |   2023 | Technology in Society                          |                  0 |                 0 | 10.1016/J.TECHSOC.2022.102150 |
+|  1 | Harvard University                          | RegTech  Potential benefits and challenges for businesses                                    |   2023 | Technology in Society                          |                  0 |                 0 | 10.1016/J.TECHSOC.2022.102150 |
+|  2 | University of Messina                       | RegTech  Potential benefits and challenges for businesses                                    |   2023 | Technology in Society                          |                  0 |                 0 | 10.1016/J.TECHSOC.2022.102150 |
+|  3 | Chinese University of Hong Kong             | Costs of voting and firm performance: Evidence from RegTech adoption in Chinese listed firms |   2023 | Research in International Business and Finance |                  0 |                 0 | 10.1016/J.RIBAF.2022.101868   |
+|  4 | Nottingham University Business School China | Costs of voting and firm performance: Evidence from RegTech adoption in Chinese listed firms |   2023 | Research in International Business and Finance |                  0 |                 0 | 10.1016/J.RIBAF.2022.101868   |
 
->>> r.production_per_year_.head()
-                                                         OCC  ...  local_citations_per_year
-organizations                                      year       ...                          
----3PB                                             2022    1  ...                     0.500
----AML Forensic library KPMG Luxembourg Societe... 2020    1  ...                     0.750
----BITS Pilani                                     2020    1  ...                     0.750
----Centre for Law                                  2017    1  ...                     0.000
----Deloitte LLP                                    2018    1  ...                     0.833
-<BLANKLINE>
-[5 rows x 7 columns]
+>>> print(r.production_per_year_.head().to_markdown())
+|                                                                       |   OCC |   cum_OCC |   global_citations |   local_citations |   age |   global_citations_per_year |   local_citations_per_year |
+|:----------------------------------------------------------------------|------:|----------:|-------------------:|------------------:|------:|----------------------------:|---------------------------:|
+| ('---3PB', 2022)                                                      |     1 |         1 |                  3 |                 1 |     2 |                       1.5   |                      0.5   |
+| ('---AML Forensic library KPMG Luxembourg Societe Cooperative', 2020) |     1 |         1 |                 10 |                 3 |     4 |                       2.5   |                      0.75  |
+| ('---BITS Pilani', 2020)                                              |     1 |         1 |                  2 |                 3 |     4 |                       0.5   |                      0.75  |
+| ('---Centre for Law', 2017)                                           |     1 |         1 |                150 |                 0 |     7 |                      21.429 |                      0     |
+| ('---Deloitte LLP', 2018)                                             |     1 |         1 |                  8 |                 5 |     6 |                       1.333 |                      0.833 |
+
 
 >>> print(r.prompt_)
 <BLANKLINE>
-Act as a researcher realizing a bibliometric analysis. Analyze a timeline plot
-build with the following table, which provides data corresponding to the top 10
-organizations with more documnets in a given bibliographic dataset. 
+Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on document production by year per organization for the top 10 most productive organizations in the dataset. Use the information in the table to draw conclusions about the productivity per year of the organizations. The final part of the organization name contains two numbers separated by a colon. The first \ 
+is the total number of documents of the organization, and the second is the total number of citations of the organization. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
 <BLANKLINE>
-- Column 'OCC' is the number of documents published in a given year by the 
-  current organization or institution. 
+| Organizations                          |   2017 |   2018 |   2019 |   2020 |   2021 |   2022 |
+|:---------------------------------------|-------:|-------:|-------:|-------:|-------:|-------:|
+| ---3PB 1:003                           |      0 |      0 |      0 |      0 |      0 |      1 |
+| ---FinTech HK 2:161                    |      2 |      0 |      0 |      0 |      0 |      0 |
+| ---School of Electrical Engineering... |      0 |      0 |      0 |      0 |      0 |      2 |
+| Ahlia University 3:019                 |      0 |      0 |      0 |      1 |      1 |      1 |
+| Coventry University 2:017              |      0 |      0 |      0 |      1 |      0 |      1 |
+| Dublin City University 2:014           |      0 |      0 |      0 |      1 |      1 |      0 |
+| Politecnico di Milano 2:002            |      0 |      0 |      0 |      1 |      0 |      1 |
+| University College Cork 3:041          |      0 |      1 |      1 |      0 |      0 |      1 |
+| University of Hong Kong 3:185          |      2 |      0 |      0 |      1 |      0 |      0 |
+| University of Westminster 2:017        |      0 |      0 |      0 |      1 |      0 |      1 |
 <BLANKLINE>
-- Column 'Year' is the year of publication.
 <BLANKLINE>
-- Column 'Organization' is the organization or institution.
-<BLANKLINE>
-- Numbers separated by a colon (:) are the total number of documents published
-  the total number of citations received by the current organization during the 
-  period of analysis.
-<BLANKLINE>
-| Organizations                          |   OCC |   Year |
-|:---------------------------------------|------:|-------:|
-| University of Hong Kong 3:185          |     2 |   2017 |
-| University College Cork 3:041          |     1 |   2019 |
-| University of Hong Kong 3:185          |     1 |   2020 |
-| Ahlia University 3:019                 |     1 |   2020 |
-| University College Cork 3:041          |     1 |   2018 |
-| Ahlia University 3:019                 |     1 |   2021 |
-| Ahlia University 3:019                 |     1 |   2022 |
-| University College Cork 3:041          |     1 |   2022 |
-| ---FinTech HK 2:161                    |     2 |   2017 |
-| Coventry University 2:017              |     1 |   2020 |
-| University of Westminster 2:017        |     1 |   2020 |
-| Dublin City University 2:014           |     1 |   2020 |
-| Coventry University 2:017              |     1 |   2022 |
-| University of Westminster 2:017        |     1 |   2022 |
-| Dublin City University 2:014           |     1 |   2021 |
-| Politecnico di Milano 2:002            |     1 |   2020 |
-| Politecnico di Milano 2:002            |     1 |   2022 |
-| ---School of Electrical Engineering... |     2 |   2022 |
-| ---3PB 1:003                           |     1 |   2022 |
-<BLANKLINE>
-Write a clear and concise paragraph describing the main findings and any 
-important trends or patterns you notice. 
-<BLANKLINE>
-Limit your description to a paragraph with no more than 250 words.    
 <BLANKLINE>
 
 
@@ -147,33 +121,29 @@ def organizations_production_over_time(
         **filters,
     )
 
-    prompt_table = results.table_[
-        ["organizations".replace("_", " ").title(), "OCC", "Year"]
-    ]
-    prompt_table = prompt_table.set_index("organizations".replace("_", " ").title())
-
-    results.prompt_ = f"""
-Act as a researcher realizing a bibliometric analysis. Analyze a timeline plot
-build with the following table, which provides data corresponding to the top {topics_length}
-organizations with more documnets in a given bibliographic dataset. 
-
-- Column 'OCC' is the number of documents published in a given year by the 
-  current organization or institution. 
-
-- Column 'Year' is the year of publication.
-
-- Column 'Organization' is the organization or institution.
-
-- Numbers separated by a colon (:) are the total number of documents published
-  the total number of citations received by the current organization during the 
-  period of analysis.
-
-{prompt_table.to_markdown()}
-
-Write a clear and concise paragraph describing the main findings and any 
-important trends or patterns you notice. 
-
-Limit your description to a paragraph with no more than 250 words.    
-"""
+    table = results.table_.copy()
+    table = table[["Organizations", "Year", "OCC"]]
+    table = table.pivot(index="Organizations", columns="Year", values="OCC")
+    table = table.fillna(0)
+    results.prompt_ = _create_prompt(table)
 
     return results
+
+
+def _create_prompt(table):
+    return f"""
+Imagine that you are a researcher analyzing a bibliographic dataset. The table \
+below provides data on document production by year per organization for the top {table.shape[0]} \
+most productive organizations in the dataset. Use the information in the table to \
+draw conclusions about the productivity per year of the organizations. The final \
+part of the organization name contains two numbers separated by a colon. The first \ 
+is the total number of documents of the organization, and the second is the total \
+number of citations of the organization. \
+In your analysis, be sure to describe in a clear and concise way, any findings \
+or any patterns you observe, and identify any outliers or anomalies in the \
+data. Limit your description to one paragraph with no more than 250 words.
+
+{table.to_markdown()}
+
+
+"""
