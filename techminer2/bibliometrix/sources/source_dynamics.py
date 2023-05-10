f6125480713a76d5d97fccb0edcec1735acfe691
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-09 21:14:18 -0500

    FEA chatgpt prompt for analyzing cumulative source production over time

--- techminer2/bibliometrix/sources/source_dynamics.py
+++ techminer2/bibliometrix/sources/source_dynamics.py
@@ -7,18 +7,55 @@ Source Dynamics
 >>> file_name = "sphinx/_static/bibliometrix__source_dynamics.html"
 
 >>> from techminer2 import bibliometrix
->>> bibliometrix.sources.source_dynamics(
+>>> r = bibliometrix.sources.source_dynamics(
 ...     topics_length=10, 
 ...     directory=directory,
-... ).write_html(file_name)
+... )
+>>> r.plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="../../_static/bibliometrix__source_dynamics.html" height="600px" width="100%" frameBorder="0"></iframe>
 
+>>> print(r.table_.head().to_markdown())
+|    |   year | source_abbr   |   cum_OCC |
+|---:|-------:|:--------------|----------:|
+|  0 |   2016 | J BANK REGUL  |         0 |
+|  1 |   2017 | J BANK REGUL  |         0 |
+|  2 |   2018 | J BANK REGUL  |         0 |
+|  3 |   2019 | J BANK REGUL  |         0 |
+|  4 |   2020 | J BANK REGUL  |         1 |
+
+>>> print(r.prompt_)
+<BLANKLINE>
+Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on cumulative document production by year per document source for the top 10 most productive sources in the dataset. Use the information in the table to draw conclusions about the cumulative productivity per year of the sources. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
+<BLANKLINE>
+| source_abbr                              |   2016 |   2017 |   2018 |   2019 |   2020 |   2021 |   2022 |   2023 |
+|:-----------------------------------------|-------:|-------:|-------:|-------:|-------:|-------:|-------:|-------:|
+| DUKE LAW J                               |      1 |      1 |      1 |      1 |      1 |      1 |      1 |      1 |
+| FOSTER INNOV AND COMPET WITH FINTECH,... |      0 |      0 |      0 |      0 |      2 |      2 |      2 |      2 |
+| INT CONF INF TECHNOL SYST INNOV,...      |      0 |      0 |      0 |      0 |      0 |      0 |      2 |      2 |
+| J BANK REGUL                             |      0 |      0 |      0 |      0 |      1 |      2 |      2 |      2 |
+| J ECON BUS                               |      0 |      0 |      1 |      1 |      1 |      1 |      1 |      1 |
+| J FINANC CRIME                           |      0 |      0 |      0 |      0 |      1 |      1 |      2 |      2 |
+| NORTHWEST J INTL LAW BUS                 |      0 |      1 |      1 |      1 |      1 |      1 |      1 |      1 |
+| PALGRAVE STUD DIGIT BUS ENABLING TECHNOL |      0 |      0 |      0 |      1 |      1 |      1 |      1 |      1 |
+| ROUTLEDGE HANDB OF FINANCIAL...          |      0 |      0 |      0 |      0 |      0 |      2 |      2 |      2 |
+| STUD COMPUT INTELL                       |      0 |      0 |      0 |      0 |      0 |      2 |      2 |      2 |
+<BLANKLINE>
+<BLANKLINE>
+<BLANKLINE>    
 
 """
+from dataclasses import dataclass
+
 from .._dynamics import _dynamics
+
+
+@dataclass(init=False)
+class _Results:
+    plot_ = None
+    prompt_ = None
 
 
 def source_dynamics(
@@ -35,7 +72,7 @@ def source_dynamics(
 ):
     """Makes a dynamics chat for top sources."""
 
-    return _dynamics(
+    results = _dynamics(
         criterion="source_abbr",
         topics_length=topics_length,
         topic_min_occ=topic_min_occ,
@@ -48,3 +85,28 @@ def source_dynamics(
         end_year=end_year,
         **filters,
     )
+
+    table = results.table_.copy()
+    table = table[["source_abbr", "year", "cum_OCC"]]
+    table = table.pivot(index="source_abbr", columns="year", values="cum_OCC")
+    table = table.fillna(0)
+    results.prompt_ = _create_prompt(table)
+
+    return results
+
+
+def _create_prompt(table):
+    return f"""
+Imagine that you are a researcher analyzing a bibliographic dataset. The table \
+below provides data on cumulative document production by year per document \
+source for the top {table.shape[0]} most productive sources in the dataset. \
+Use the information in the table to draw conclusions about the cumulative \
+productivity per year of the sources. \
+In your analysis, be sure to describe in a clear and concise way, any findings \
+or any patterns you observe, and identify any outliers or anomalies in the \
+data. Limit your description to one paragraph with no more than 250 words.
+
+{table.to_markdown()}
+
+
+"""
