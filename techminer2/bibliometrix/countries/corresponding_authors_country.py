3c27529d39757ed61b755282be4e35c94dda9f99
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-09 21:49:47 -0500

    FEA chatgpt prompt for analyzing corresponding authors country

--- techminer2/bibliometrix/countries/corresponding_authors_country.py
+++ techminer2/bibliometrix/countries/corresponding_authors_country.py
@@ -8,32 +8,62 @@ Corresponding Author's Country
 >>> file_name = "sphinx/_static/bibliometrix__corresponding_authors_country.html"
 
 >>> from techminer2 import bibliometrix
->>> bibliometrix.countries.corresponding_authors_country(
+>>> r = bibliometrix.countries.corresponding_authors_country(
 ...     topics_length=20,
 ...     directory=directory,
-... ).plot_.write_html(file_name)
+... )
+>>> r.plot_.write_html(file_name)
 
 .. raw:: html
 
-    <iframe src="../../../_static/bibliometrix__corresponding_authors_country.html" height="600px" width="100%" frameBorder="0"></iframe>
+    <iframe src="../../../_static/bibliometrix__corresponding_authors_country.html"height="600px" width="100%" frameBorder="0"></iframe>
 
 
->>> from techminer2 import bibliometrix
->>> bibliometrix.countries.corresponding_authors_country(
-...     directory=directory, 
-...     topics_length=20,
-... ).table_.head()
-                single_publication  multiple_publication  mcp_ratio
-countries                                                          
-United Kingdom                   4                     3       0.75
-Australia                        4                     3       0.75
-United States                    4                     2       0.50
-Ireland                          4                     1       0.25
-China                            2                     3       1.50
+
+>>> r.table_.head()
+                single_publication  multiple_publication mcp_ratio
+countries                                                         
+United Kingdom                   4                     3      0.75
+Australia                        4                     3      0.75
+United States                    4                     2       0.5
+Ireland                          4                     1      0.25
+China                            2                     3       1.5
+
+>>> print(r.prompt_)
+<BLANKLINE>
+Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on top 20 most frequent countries in the dataset. 'single publication' is the number of documents in which all the authors are from the same country. 'multiple publication' is the number of documents in which the authors are from different countries. 'mcp ratio' is the ratio between 'multiple publication' and 'single publication'. The higher the ratio, the higher the collaboration between countries. Use the information in the table to draw conclusions about the level of collaboration between countries in the dataset. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
+<BLANKLINE>
+|    | countries            |   single_publication |   multiple_publication | mcp_ratio   |
+|---:|:---------------------|---------------------:|-----------------------:|:------------|
+|  0 | United Kingdom       |                    4 |                      3 | 0.75        |
+|  1 | Australia            |                    4 |                      3 | 0.75        |
+|  2 | United States        |                    4 |                      2 | 0.5         |
+|  3 | Ireland              |                    4 |                      1 | 0.25        |
+|  4 | China                |                    2 |                      3 | 1.5         |
+|  5 | Italy                |                    4 |                      1 | 0.25        |
+|  6 | Germany              |                    2 |                      2 | 1.0         |
+|  7 | Switzerland          |                    1 |                      3 | 3.0         |
+|  8 | Bahrain              |                    2 |                      2 | 1.0         |
+|  9 | Hong Kong            |                    0 |                      3 | -           |
+| 10 | Spain                |                    2 |                      0 | 0.0         |
+| 11 | Indonesia            |                    2 |                      0 | 0.0         |
+| 12 | Luxembourg           |                    1 |                      1 | 1.0         |
+| 13 | United Arab Emirates |                    1 |                      1 | 1.0         |
+| 14 | Palestine            |                    1 |                      0 | 0.0         |
+| 15 | Romania              |                    0 |                      1 | -           |
+| 16 | Poland               |                    0 |                      1 | -           |
+| 17 | Netherlands          |                    0 |                      1 | -           |
+| 18 | France               |                    0 |                      1 | -           |
+| 19 | Belgium              |                    1 |                      0 | 0.0         |
+<BLANKLINE>
+<BLANKLINE>
+<BLANKLINE>
+
 
 """
 from dataclasses import dataclass
 
+import numpy as np
 import plotly.express as px
 
 from ...techminer.indicators.collaboration_indicators_by_topic import (
@@ -45,6 +75,7 @@ from ...techminer.indicators.collaboration_indicators_by_topic import (
 class _Results:
     plot_: None
     table_: None
+    prompt_: None
 
 
 def corresponding_authors_country(
@@ -77,7 +108,29 @@ def corresponding_authors_country(
     results.table_ = table
     table = table.reset_index()
     results.plot_ = _make_plot(table)
+    results.prompt_ = _create_prompt(table)
     return results
+
+
+def _create_prompt(table):
+    return f"""
+Imagine that you are a researcher analyzing a bibliographic dataset. The table \
+below provides data on top {table.shape[0]} most frequent countries in the \
+dataset. 'single publication' is the number of documents in which all the \
+authors are from the same country. 'multiple publication' is the number \
+of documents in which the authors are from different countries. \
+'mcp ratio' is the ratio between 'multiple publication' and 'single publication'. \
+The higher the ratio, the higher the collaboration between countries. \
+Use the information in the table to draw conclusions about the level of \
+collaboration between countries in the dataset. In your analysis, be sure to \
+describe in a clear and concise way, any findings or any patterns you \
+observe, and identify any outliers or anomalies in the data. \
+Limit your description to one paragraph with no more than 250 words.
+
+{table.to_markdown()}
+
+
+"""
 
 
 def _make_table(
@@ -87,7 +140,6 @@ def _make_table(
     end_year,
     **filters,
 ):
-
     indicators = collaboration_indicators_by_topic(
         "countries",
         directory=directory,
@@ -102,11 +154,11 @@ def _make_table(
     indicators = indicators.assign(
         mcp_ratio=indicators["multiple_publication"] / indicators["single_publication"]
     )
+    indicators = indicators.replace(np.inf, "-")
     return indicators
 
 
 def _make_plot(indicators):
-
     indicators = indicators.melt(
         id_vars="countries", value_vars=["single_publication", "multiple_publication"]
     )
