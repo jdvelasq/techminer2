6e6a0fbcc5e32ea0680ba5ebdfc479f15efdf53f
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-09 21:18:30 -0500

    FEA chatgpt prompt to analyze country cumulative production per year

--- techminer2/bibliometrix/countries/country_dynamics.py
+++ techminer2/bibliometrix/countries/country_dynamics.py
@@ -8,19 +8,45 @@ Country Dynamics
 >>> directory = "data/regtech/"
 >>> file_name = "sphinx/_static/bibliometrix__country_dynamics.html"
 
-
 >>> from techminer2 import bibliometrix
->>> bibliometrix.countries.country_dynamics(
+>>> r = bibliometrix.countries.country_dynamics(
 ...     topics_length=5, 
 ...     directory=directory,
-... ).write_html(file_name)
+... )
+>>> r.plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="../../../_static/bibliometrix__country_dynamics.html" height="600px" width="100%" frameBorder="0"></iframe>
 
 
+>>> print(r.table_.head().to_markdown())
+|    |   year | countries      |   cum_OCC |
+|---:|-------:|:---------------|----------:|
+|  0 |   2016 | United Kingdom |         0 |
+|  1 |   2017 | United Kingdom |         0 |
+|  2 |   2018 | United Kingdom |         3 |
+|  3 |   2019 | United Kingdom |         4 |
+|  4 |   2020 | United Kingdom |         6 |
+
+>>> print(r.prompt_)
+<BLANKLINE>
+Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on cumulative document production by year per country for the top 5 most productive countries in the dataset. Use the information in the table to draw conclusions about the cumulative productivity per year of the countries. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
+<BLANKLINE>
+| countries      |   2016 |   2017 |   2018 |   2019 |   2020 |   2021 |   2022 |   2023 |
+|:---------------|-------:|-------:|-------:|-------:|-------:|-------:|-------:|-------:|
+| Australia      |      0 |      2 |      2 |      2 |      5 |      7 |      7 |      7 |
+| China          |      0 |      1 |      1 |      1 |      1 |      1 |      4 |      5 |
+| Ireland        |      0 |      0 |      1 |      2 |      3 |      4 |      5 |      5 |
+| United Kingdom |      0 |      0 |      3 |      4 |      6 |      6 |      7 |      7 |
+| United States  |      1 |      1 |      2 |      4 |      4 |      4 |      5 |      6 |
+<BLANKLINE>
+<BLANKLINE>
+<BLANKLINE>
+
+
 """
+
 from .._dynamics import _dynamics
 
 
@@ -38,7 +64,7 @@ def country_dynamics(
 ):
     """Makes a dynamics chat for top sources."""
 
-    return _dynamics(
+    results = _dynamics(
         criterion="countries",
         topics_length=topics_length,
         topic_min_occ=topic_min_occ,
@@ -51,3 +77,28 @@ def country_dynamics(
         end_year=end_year,
         **filters,
     )
+
+    table = results.table_.copy()
+    table = table[["countries", "year", "cum_OCC"]]
+    table = table.pivot(index="countries", columns="year", values="cum_OCC")
+    table = table.fillna(0)
+    results.prompt_ = _create_prompt(table)
+
+    return results
+
+
+def _create_prompt(table):
+    return f"""
+Imagine that you are a researcher analyzing a bibliographic dataset. The table \
+below provides data on cumulative document production by year per \
+country for the top {table.shape[0]} most productive countries in the dataset. \
+Use the information in the table to draw conclusions about the cumulative \
+productivity per year of the countries. \
+In your analysis, be sure to describe in a clear and concise way, any findings \
+or any patterns you observe, and identify any outliers or anomalies in the \
+data. Limit your description to one paragraph with no more than 250 words.
+
+{table.to_markdown()}
+
+
+"""
