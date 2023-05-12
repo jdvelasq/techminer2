b9360b647fb26bb13053ee655258abc28175279f
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-12 11:38:45 -0500

    FEA chatgpt prompt for text screening function in TLAB

--- techminer2/tlab/lexical_tools/text_screening.py
+++ techminer2/tlab/lexical_tools/text_screening.py
@@ -2,79 +2,35 @@
 Text Screening
 ===============================================================================
 
+This funcion searchs for them specified in the parameter ``search_for`` in the
+abstracts of the records and creates a file with the report.
 
 >>> directory = "data/regtech/"
 
 >>> from techminer2 import tlab
 >>> tlab.lexical_tools.text_screening(
 ...     search_for='regtech',
-...     top_n=2,
 ...     directory=directory,
 ... )
---INFO-- Saved HTML report: data/regtech/reports/text_screening.html
-<BLANKLINE>
-*** Arner DW, 2017, HANDB OF BLOCKCHAIN, DIGIT FINANC, AND INCL, VOL 1: CRYPTOCURR, FINTECH, INSURTECH, AND REGUL, P359
-<BLANKLINE>
-Regulatory technology or 'REGTECH' is the use of technology, particularly
-information technology, in the context of regulatory monitoring, reporting and
-compliance. REGTECH to date has focused on the digitization of manual reporting
-and compliance processes, for example in the context of know-your-customer
-requirements. this offers tremendous cost savings to the financial services
-industry and regulators. however, the potential of REGTECH is far greater - it
-could enable close to real-time monitoring and a proportionate regulatory regime
-that addresses risk and facilitates more efficient regulatory compliance. we
-argue that the transformative nature of technology will only be captured by a
-new approach that sits at the nexus between data, digital identity, and
-regulation. we seek to expose the inadequacy and lack of ambition of simply
-digitizing analogue processes in a digital financial world. the development of
-financial technology ('fintech'), rapid developments in emerging markets, and
-the recent pro-active stance of regulators in developing regulatory sandboxes,
-represent a unique combination of events, which could facilitate the transition
-from one regulatory model to another. 2018 elsevier inc. all rights reserved.
-<BLANKLINE>
-<BLANKLINE>
-*** Arner DW, 2017, NORTHWEST J INTL LAW BUS, V37, P373
-<BLANKLINE>
-Regulatory change and technological developments following the 2008 global
-financial crisis are changing the nature of financial markets, services, and
-institutions. at the juncture of these phenomena lies regulatory technology or
-regtechthe use of technology, particularly information technology, in the
-context of regulatory monitoring, reporting, and compliance. regulating rapidly
-transforming financial systems requires increasing the use of and reliance on
-REGTECH. whilst the principal regulatory objectives (e.g., financial stability,
-prudential safety and soundness, consumer protection and market integrity, and
-market competition and development) remain, their means of application are
-increasingly inadequate. REGTECH developments are leading towards a paradigm
-shift necessitating the reconceptualization of financial regulation. REGTECH to
-date has focused on the digitization of manual reporting and compliance
-processes. this offers tremendous cost savings to the financial services
-industry and regulators. however, the potential of REGTECH is far greater  it
-has the potential to enable a nearly real-time and proportionate regulatory
-regime that identifies and addresses risk while facilitating more efficient
-regulatory compliance. we argue that the transformative nature of technology
-will only be captured by a new approach at the nexus of data, digital identity,
-and regulation. this paper seeks to expose the inadequacy of digitizing analogue
-processes in a digital financial world, sets the foundation for a practical
-understanding of REGTECH, and proposes sequenced reforms that could benefit
-regulators, industry, and entrepreneurs in the financial sector and other
-industries. 2017. all rights reserved.
-<BLANKLINE>
-<BLANKLINE>
+--INFO-- The file 'data/regtech/reports/text_screening.txt' was created
 
 
 """
+import os.path
+import sys
 import textwrap
 
 from ..._load_abstracts import load_abstracts
-from ..._load_template import load_template
 from ..._read_records import read_records
-from ..._save_html_report import save_html_report
 from ..concordances.concordances import _select_abstracts
+
+# from ..._load_template import load_template
+# from ..._save_html_report import save_html_report
 
 
 def text_screening(
     search_for,
-    top_n=5,
+    # top_n=5,
     directory="./",
     database="documents",
     start_year=None,
@@ -95,9 +51,9 @@ def text_screening(
     abstracts = abstracts[abstracts.article.isin(records.article)]
     abstracts = _join_phrases(abstracts)
     abstracts = _select_abstracts(abstracts, search_for)
-    _create_report(directory, abstracts)
-    abstracts = abstracts.head(top_n)
-    _print_abstracts(abstracts)
+    # _create_report(directory, abstracts)
+    # abstracts = abstracts.head(top_n)
+    _print_abstracts(directory, abstracts)
 
 
 def _join_phrases(abstracts):
@@ -108,32 +64,51 @@ def _join_phrases(abstracts):
     abstracts = abstracts.groupby(["article", "global_citations"], as_index=False).agg(
         list
     )
-    # abstracts.phrase = abstracts.phrase.map(lambda x: [y.title() for y in x])
     abstracts.phrase = abstracts.phrase.str.join(" ")
     return abstracts
 
 
-def _create_report(directory, abstracts):
+# def _create_report(directory, abstracts):
 
-    abstracts = abstracts.copy()
-    abstracts = abstracts[["article", "phrase"]]
-    abstracts = abstracts.groupby("article", as_index=False).aggregate(
-        lambda x: " <br> ".join(x)
-    )
+#     abstracts = abstracts.copy()
+#     abstracts = abstracts[["article", "phrase"]]
+#     abstracts = abstracts.groupby("article", as_index=False).aggregate(
+#         lambda x: " <br> ".join(x)
+#     )
 
-    report_name = "text_screening.html"
-    template = load_template(report_name)
-    html = template.render(
-        concordances=zip(abstracts.article.tolist(), abstracts.phrase.tolist())
-    )
-    save_html_report(directory, html, report_name)
+#     report_name = "text_screening.html"
+#     template = load_template(report_name)
+#     html = template.render(
+#         concordances=zip(abstracts.article.tolist(), abstracts.phrase.tolist())
+#     )
+#     save_html_report(directory, html, report_name)
 
 
-def _print_abstracts(abstracts):
-    print()
-    for article, phrase in zip(abstracts.article, abstracts.phrase):
-        print("*** " + article)
-        print()
-        print(textwrap.fill(phrase, width=80))
-        print()
-        print()
+def _print_abstracts(directory, abstracts):
+    file_path = os.path.join(directory, "reports/text_screening.txt")
+    prompt = '''Summarize the text delimited between """ and """ in the following lines, in 50 words or less.
+    
+"""
+
+"""
+'''
+
+    with open(file_path, "w", encoding="utf-8") as file:
+        print("-" * 80, file=file)
+        print(prompt, file=file)
+        print("-" * 80, file=file)
+
+        for article, phrase in zip(abstracts.article, abstracts.phrase):
+            print("*** " + article, file=file)
+
+            text = textwrap.fill(phrase, width=80)
+            text = text.split("\n")
+            text = [x.strip() for x in text]
+            text = " \\\n".join(text)
+            text = '\n"""\n' + text + '\n"""'
+
+            print(text, file=file)
+            print(file=file)
+            print(file=file)
+
+    sys.stdout.write(f"--INFO-- The file '{file_path}' was created\n")
