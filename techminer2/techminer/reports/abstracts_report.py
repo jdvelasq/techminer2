9c196384540cb0f9f47397fe5f2ad7fe459aa9a8
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-15 13:41:56 -0500

    REF improve abstracts_report.py using ChatGPT

--- techminer2/techminer/reports/abstracts_report.py
+++ techminer2/techminer/reports/abstracts_report.py
@@ -46,142 +46,125 @@ def abstracts_report(
         **filters,
     )
 
-    if criterion is not None:
+    if criterion:
         records = _sort_by_custom_terms(criterion, custom_topics, records)
     else:
-        records = records.sort_values(
-            by=["global_citations", "local_citations"],
-            ascending=[False, False],
-        )
-
-    # records = records.head(n_abstracts)
+        records = _sort_by_default_criteria(records)
 
     _write_report(criterion, file_name, use_textwrap, directory, records)
 
 
 def _sort_by_custom_terms(criterion, custom_topics, records):
-    selected_records = records[["article", criterion]]
-    selected_records[criterion] = selected_records[criterion].str.split(";")
-    selected_records = selected_records.explode(criterion)
-    selected_records[criterion] = selected_records[criterion].str.strip()
-    selected_records = selected_records[selected_records[criterion].isin(custom_topics)]
-    records = records[records["article"].isin(selected_records["article"])]
+    def _filter_records_by_custom_topics(records):
+        records = records.copy()
+        selected_records = records[["article", criterion]]
+        selected_records[criterion] = selected_records[criterion].str.split(";")
+        selected_records = selected_records.explode(criterion)
+        selected_records[criterion] = selected_records[criterion].str.strip()
+        selected_records = selected_records[
+            selected_records[criterion].isin(custom_topics)
+        ]
+        return records[records["article"].isin(selected_records["article"])]
 
-    ##
-    records["TOPICS"] = records[criterion].copy()
-    records["TOPICS"] = records["TOPICS"].str.split(";")
-    records["TOPICS"] = records["TOPICS"].map(lambda x: [y.strip() for y in x])
+    def _convert_topics_to_list(records):
+        records = records.copy()
+        records["TOPICS"] = records[criterion].copy()
+        records["TOPICS"] = records["TOPICS"].str.split(";")
+        records["TOPICS"] = records["TOPICS"].map(lambda x: [y.strip() for y in x])
+        return records
 
-    records["POINTS"] = ""
-    for topic in custom_topics:
-        records["POINTS"] += records["TOPICS"].map(lambda x: "1" if topic in x else "0")
+    def _compute_points(records):
+        records = records.copy()
+        records["POINTS"] = records["TOPICS"].apply(
+            lambda x: "".join(["1" if topic in x else "0" for topic in custom_topics])
+        )
+        return records
 
+    def _sort_by_points(records):
+        records = records.copy()
+        records = records.sort_values(
+            by=["POINTS", "global_citations", "local_citations"],
+            ascending=[False, False, False],
+        )
+        return records
+
+    def _reorder_terms(criterion, custom_topics, records):
+        records = records.copy()
+        records["TERMS"] = records[criterion].str.split(";")
+        records["TERMS"] = records["TERMS"].map(lambda x: [y.strip() for y in x])
+        records["TERMS_1"] = records["TERMS"].map(
+            lambda x: [
+                "(*) " + custom_topic
+                for custom_topic in custom_topics
+                if custom_topic in x
+            ],
+            na_action="ignore",
+        )
+        records["TERMS_2"] = records["TERMS"].map(
+            lambda x: [y for y in x if y not in custom_topics], na_action="ignore"
+        )
+        records["TERMS"] = records["TERMS_1"] + records["TERMS_2"]
+        records[criterion] = records["TERMS"].str.join("; ")
+
+        return records
+
+    records = _filter_records_by_custom_topics(records)
+    records = _convert_topics_to_list(records)
+    records = _compute_points(records)
+    records = _sort_by_points(records)
+    records = _reorder_terms(criterion, custom_topics, records)
+    return records
+
+
+def _sort_by_default_criteria(records):
     records = records.sort_values(
-        by=["POINTS", "global_citations", "local_citations"],
-        ascending=[False, False, False],
+        by=["global_citations", "local_citations"],
+        ascending=[False, False],
     )
-
-    # records["RNK"] = records.groupby("POINTS")["global_citations"].rank(
-    #     ascending=False, method="dense"
-    # )
-
-    # records = records[records["RNK"] < 10]
-
-    records["TERMS"] = records[criterion].str.split(";")
-    records["TERMS"] = records["TERMS"].map(lambda x: [y.strip() for y in x])
-    records["TERMS_1"] = records["TERMS"].map(
-        lambda x: [
-            "(*) " + custom_topic for custom_topic in custom_topics if custom_topic in x
-        ],
-        na_action="ignore",
-    )
-    records["TERMS_2"] = records["TERMS"].map(
-        lambda x: [y for y in x if y not in custom_topics], na_action="ignore"
-    )
-    records["TERMS"] = records["TERMS_1"] + records["TERMS_2"]
-    records[criterion] = records["TERMS"].str.join("; ")
     return records
 
 
 def _write_report(criterion, file_name, use_textwrap, directory, records):
+    def format_text(text, use_textwrap):
+        if pd.isna(text):
+            return ""
+
+        if use_textwrap:
+            return textwrap.fill(
+                text,
+                width=87,
+                initial_indent=" " * 0,
+                subsequent_indent=" " * 3,
+                fix_sentence_endings=True,
+            )
+
+        return text
+
+    def print_field(field, text, out_file):
+        if text:
+            print(f"{field} {text}", file=out_file)
+
     file_path = os.path.join(directory, "reports", file_name)
 
     with open(file_path, "w", encoding="utf-8") as out_file:
         counter = 0
 
-        text_article = ""
-
         for _, row in records.iterrows():
-            if use_textwrap:
-                if not pd.isna(row["article"]):
-                    text_article = textwrap.fill(
-                        row["article"],
-                        width=87,
-                        initial_indent=" " * 0,
-                        subsequent_indent=" " * 3,
-                        fix_sentence_endings=True,
-                    )
-                if not pd.isna(row["title"]):
-                    text_title = textwrap.fill(
-                        row["title"],
-                        width=87,
-                        initial_indent=" " * 0,
-                        subsequent_indent=" " * 3,
-                        fix_sentence_endings=True,
-                    )
-                if not pd.isna(row[criterion]):
-                    text_criterion = textwrap.fill(
-                        row[criterion],
-                        width=87,
-                        initial_indent=" " * 0,
-                        subsequent_indent=" " * 3,
-                        fix_sentence_endings=True,
-                    )
-                if "abstract" in row.index and not pd.isna(row["abstract"]):
-                    text_abstract = textwrap.fill(
-                        row["abstract"],
-                        width=87,
-                        initial_indent=" " * 0,
-                        subsequent_indent=" " * 3,
-                        fix_sentence_endings=True,
-                    )
-                else:
-                    text_abstract = ""
-
-            else:
-                text_article = row["article"]
-                text_title = row["title"]
-                text_criterion = row[criterion]
-                if not pd.isna(row["abstract"]):
-                    text_abstract = row["abstract"]
-
+            text_article = format_text(row["article"], use_textwrap)
+            text_title = format_text(row["title"], use_textwrap)
+            text_criterion = format_text(row[criterion], use_textwrap)
+            text_abstract = format_text(row.get("abstract", ""), use_textwrap)
             text_citation = str(row["global_citations"])
 
-            # print("-" * 90, file=out_file)
-            print("-- {:03d} ".format(counter) + "-" * 83, file=out_file)
-            print("AR ", end="", file=out_file)
-            print(text_article, file=out_file)
-
-            print("TI ", end="", file=out_file)
-            print(text_title, file=out_file)
-
-            print("KW ", end="", file=out_file)
-            print(text_criterion, file=out_file)
-
-            print("TC ", end="", file=out_file)
-            print(text_citation, file=out_file)
-
-            print("AB ", end="", file=out_file)
-            print(text_abstract, file=out_file)
-            # print('"""', end="", file=out_file)
-            # text_abstract = text_abstract.split("\n")
-            # text_abstract = [x.strip() for x in text_abstract]
-            # text_abstract = " \\\n".join(text_abstract)
-            # print(text_abstract, file=out_file)
-            # print('"""', end="\n", file=out_file)
+            print(f"-- {counter:03d} " + "-" * 83, file=out_file)
+            print_field("AR", text_article, out_file)
+            print_field("TI", text_title, out_file)
+            print_field("KW", text_criterion, out_file)
+            print_field("TC", text_citation, out_file)
+            print_field("AB", text_abstract, out_file)
 
             print("\n", file=out_file)
 
             counter += 1
 
-    sys.stdout.write(f"--INFO-- The file '{file_path}' was created\n")
+    print(f"--INFO-- The file '{file_path}' was created")
