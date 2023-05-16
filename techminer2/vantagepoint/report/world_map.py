0235aa89f7a466fc585378d4237c593b3911af02
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-16 17:32:08 -0500

    FEA ChatGPT prompt for world map in VantagePoint module

--- techminer2/vantagepoint/report/world_map.py
+++ techminer2/vantagepoint/report/world_map.py
@@ -1,5 +1,5 @@
 """
-World Map
+World Map (GPT)
 ===============================================================================
 
 
@@ -7,50 +7,131 @@ World Map
 >>> file_name = "sphinx/_static/vantagepoint__world_map.html"
 
 >>> from techminer2 import vantagepoint
->>> vantagepoint.report.world_map(
-...     criterion="countries", 
+>>> obj = vantagepoint.analyze.extract_topics(
+...     criterion='countries',
 ...     directory=directory,
-... ).write_html(file_name)
+...     topics_length=None,
+... )
+>>> chart = vantagepoint.report.world_map(
+...     obj, 
+...     title="Country scientific production",
+...     colormap="Blues",
+... )
+>>> chart.plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="../../../../_static/vantagepoint__world_map.html" height="450px" width="100%" frameBorder="0"></iframe>
 
+    
+>>> chart.table_.head()
+countries
+United Kingdom    7
+Australia         7
+United States     6
+Ireland           5
+China             5
+Name: OCC, dtype: int64
+
+>>> print(chart.prompt_)
+Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
+<BLANKLINE>
+| countries            |   OCC |
+|:---------------------|------:|
+| United Kingdom       |     7 |
+| Australia            |     7 |
+| United States        |     6 |
+| Ireland              |     5 |
+| China                |     5 |
+| Italy                |     5 |
+| Germany              |     4 |
+| Switzerland          |     4 |
+| Bahrain              |     4 |
+| Hong Kong            |     3 |
+| Luxembourg           |     2 |
+| United Arab Emirates |     2 |
+| Spain                |     2 |
+| Indonesia            |     2 |
+| Greece               |     1 |
+| Japan                |     1 |
+| Jordan               |     1 |
+| South Africa         |     1 |
+| Ukraine              |     1 |
+| Malaysia             |     1 |
+| Palestine            |     1 |
+| India                |     1 |
+| Taiwan               |     1 |
+| France               |     1 |
+| Poland               |     1 |
+| Romania              |     1 |
+| Singapore            |     1 |
+| Belgium              |     1 |
+| Netherlands          |     1 |
+<BLANKLINE>
+<BLANKLINE>
 
 """
-from ..._plots.world_map_plot import world_map_plot
-from ...techminer.indicators.indicators_by_topic import indicators_by_topic
+from dataclasses import dataclass
 
-TEXTLEN = 40
+import plotly.express as px
+
+from ... import chatgpt
+
+
+@dataclass(init=False)
+class _Chart:
+    plot_: None
+    table_: None
+    prompt_: None
 
 
 def world_map(
-    criterion,
-    directory="./",
-    database="documents",
-    metric="OCC",
-    start_year=None,
-    end_year=None,
-    colormap="Greys",
+    obj,
     title=None,
-    **filters,
+    colormap="Blues",
 ):
-    """Worldmap"""
-
-    indicators = indicators_by_topic(
-        criterion=criterion,
-        directory=directory,
-        database=database,
-        start_year=start_year,
-        end_year=end_year,
-        **filters,
-    )
-
-    indicators = indicators[metric]
-
-    return world_map_plot(
-        dataframe=indicators,
-        metric=metric,
-        colormap=colormap,
+    result = _Chart()
+    result.plot_ = _create_plot(
+        obj,
         title=title,
+        colormap=colormap,
     )
+
+    result.table_ = obj.table_[obj.metric_]
+    result.prompt_ = chatgpt.generate_prompt(result.table_)
+
+    return result
+
+
+def _create_plot(
+    obj,
+    title,
+    colormap,
+):
+    worldmap_data = px.data.gapminder()[["country", "continent", "iso_alpha"]]
+    worldmap_data = worldmap_data.drop_duplicates()
+    worldmap_data = worldmap_data.reset_index(drop=True)
+    worldmap_data.index = worldmap_data.country
+
+    dataframe = obj.table_.copy()
+    dataframe.index = dataframe.index.rename("country")
+    dataframe = dataframe.sort_index()
+
+    worldmap_data = worldmap_data.join(dataframe, how="left")
+    worldmap_data = worldmap_data.fillna(0)
+
+    fig = px.choropleth(
+        worldmap_data,
+        locations="iso_alpha",
+        color=obj.metric_,
+        hover_name="country",
+        hover_data=[
+            col for col in dataframe.columns if col not in ["country", "iso_alpha"]
+        ],
+        color_continuous_scale=colormap,
+        scope="world",
+    )
+    fig.update_layout(coloraxis_showscale=False)
+    fig.update_layout(title=title)
+
+    return fig
