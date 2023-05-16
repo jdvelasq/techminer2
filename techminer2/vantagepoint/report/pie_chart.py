450c5229223e386cee7e9a1af43b28629609e948
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-16 16:38:54 -0500

    FEA ChatGPT for pie chart in VantagePoint module

--- techminer2/vantagepoint/report/pie_chart.py
+++ techminer2/vantagepoint/report/pie_chart.py
@@ -1,5 +1,5 @@
 """
-Pie Chart
+Pie Chart (GPT)
 ===============================================================================
 
 
@@ -7,18 +7,19 @@ Pie Chart
 >>> file_name = "sphinx/_static/vantagepoint__pie_chart.html"
 
 >>> from techminer2 import vantagepoint
->>> r = vantagepoint.report.pie_chart(
-...     criterion='author_keywords',
-...     topics_length=15,
-...     directory=directory,
+>>> obj = vantagepoint.analyze.extract_topics(
+...    criterion='author_keywords',
+...    directory=directory,
+...    topics_length=10,
 ... )
->>> r.plot_.write_html(file_name)
+>>> chart = vantagepoint.report.pie_chart(obj, title="Most Frequent Author Keywords")
+>>> chart.plot_.write_html(file_name)
 
 .. raw:: html
 
     <iframe src="../../_static/vantagepoint__pie_chart.html" height="600px" width="100%" frameBorder="0"></iframe>
 
->>> r.table_.head()
+>>> chart.table_.head()
 author_keywords
 regtech                  28
 fintech                  12
@@ -27,38 +28,73 @@ compliance                7
 regulation                5
 Name: OCC, dtype: int64
 
+>>> print(chart.prompt_)
+Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
+<BLANKLINE>
+| author_keywords         |   OCC |
+|:------------------------|------:|
+| regtech                 |    28 |
+| fintech                 |    12 |
+| regulatory technology   |     7 |
+| compliance              |     7 |
+| regulation              |     5 |
+| financial services      |     4 |
+| financial regulation    |     4 |
+| artificial intelligence |     4 |
+| anti-money laundering   |     3 |
+| risk management         |     3 |
+<BLANKLINE>
+<BLANKLINE>
+
+
+
 
 """
-from .chart import chart
+from dataclasses import dataclass
+
+import plotly.express as px
+
+from ... import chatgpt
+
+
+@dataclass(init=False)
+class _Chart:
+    plot_: None
+    table_: None
+    prompt_: None
 
 
 def pie_chart(
-    criterion,
-    directory="./",
-    database="documents",
-    start_year=None,
-    end_year=None,
-    topics_length=20,
-    topic_min_occ=None,
-    topic_min_citations=None,
-    custom_topics=None,
+    obj,
     title=None,
-    **filters,
+    hole=0.4,
 ):
-    """Plots a bar chart from a column of a dataframe."""
-
-    return chart(
-        criterion=criterion,
-        directory=directory,
-        database=database,
-        metric="OCC",
-        start_year=start_year,
-        end_year=end_year,
-        topics_length=topics_length,
-        topic_min_occ=topic_min_occ,
-        topic_min_citations=topic_min_citations,
-        custom_topics=custom_topics,
+    result = _Chart()
+    result.plot_ = _create_plot(
+        obj,
         title=title,
-        plot="pie",
-        **filters,
+        hole=hole,
     )
+
+    result.table_ = obj.table_[obj.metric_]
+    result.prompt_ = chatgpt.generate_prompt(result.table_)
+
+    return result
+
+
+def _create_plot(
+    obj,
+    title,
+    hole,
+):
+    fig = px.pie(
+        obj.table_,
+        values=obj.metric_,
+        names=obj.table_.index.to_list(),
+        hole=hole,
+        hover_data=obj.table_.columns.to_list(),
+        title=title if title is not None else "",
+    )
+    fig.update_traces(textinfo="percent+value")
+    fig.update_layout(legend={"y": 0.5})
+    return fig
