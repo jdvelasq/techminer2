a0566bdb644515001430ed71736391a63fcbf5cc
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-23 16:10:28 -0500

    FIX most local cited organizations

--- techminer2/bibliometrix/organizations/most_local_cited_organizations.py
+++ techminer2/bibliometrix/organizations/most_local_cited_organizations.py
@@ -30,8 +30,7 @@ Harvard University Weatherhead Center for International Affairs     8
 Name: local_citations, dtype: int64
 
 >>> print(r.prompt_)
-<BLANKLINE>
-Imagine that you are a researcher analyzing a bibliographic dataset. The table below provides data on top 20 most local cited organizations in the references of the documents in the dataset ('local_citations' column). Use the information in the table to draw conclusions about the impact and relevance of the reseach published by the cited organizations. In your analysis, be sure to describe in a clear and concise way, any findings or any patterns you observe, and identify any outliers or anomalies in the data. Limit your description to one paragraph with no more than 250 words.
+Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
 <BLANKLINE>
 | organizations                                                   |   local_citations |
 |:----------------------------------------------------------------|------------------:|
@@ -57,26 +56,29 @@ Imagine that you are a researcher analyzing a bibliographic dataset. The table b
 | Zayed University                                                |                 4 |
 <BLANKLINE>
 <BLANKLINE>
-<BLANKLINE>
-    
+
+
+
 """
-# from ...vantagepoint.report.chart import chart
+from ... import vantagepoint
 
 
 def most_local_cited_organizations(
     directory="./",
     topics_length=20,
     topic_min_occ=None,
+    topic_max_occ=None,
     topic_min_citations=None,
-    plot="cleveland",
+    topic_max_citations=None,
+    custom_topics=None,
     database="documents",
     start_year=None,
     end_year=None,
     **filters,
 ):
-    """Most Local Cited Organizations (from Reference Lists)."""
+    """Most local cited organizations."""
 
-    obj = chart(
+    obj = vantagepoint.analyze.extract_topics(
         criterion="organizations",
         directory=directory,
         database=database,
@@ -85,30 +87,18 @@ def most_local_cited_organizations(
         end_year=end_year,
         topics_length=topics_length,
         topic_min_occ=topic_min_occ,
+        topic_max_occ=topic_max_occ,
         topic_min_citations=topic_min_citations,
-        custom_topics=None,
-        title="Most Local Cited Organizations (from Reference Lists)",
-        plot=plot,
+        topic_max_citations=topic_max_citations,
+        custom_topics=custom_topics,
         **filters,
     )
 
-    obj.prompt_ = _create_prompt(obj.table_)
+    chart = vantagepoint.report.cleveland_chart(
+        obj,
+        title="Most Local Cited Organizations",
+        x_label="Local Citations",
+        y_label="ORGANIZATIONS",
+    )
 
-    return obj
-
-
-def _create_prompt(table):
-    return f"""
-Imagine that you are a researcher analyzing a bibliographic dataset. The table \
-below provides data on top {table.shape[0]} most local cited organizations in the \
-references of the documents in the dataset ('local_citations' column). Use the \
-information in the table to draw conclusions about the impact and relevance of \
-the reseach published by the cited organizations. In your analysis, be sure \
-to describe in a clear and concise way, any findings or any patterns you \
-observe, and identify any outliers or anomalies in the data. \
-Limit your description to one paragraph with no more than 250 words.
-
-{table.to_markdown()}
-
-
-"""
+    return chart
