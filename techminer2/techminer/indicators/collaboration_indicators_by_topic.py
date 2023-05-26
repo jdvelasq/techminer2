5cae5066963d92fe0d5cf4c2be2c8ace6dbb8cf0
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-26 17:17:57 -0500

    Update collaboration_indicators_by_topic.py

--- techminer2/techminer/indicators/collaboration_indicators_by_topic.py
+++ techminer2/techminer/indicators/collaboration_indicators_by_topic.py
@@ -1,12 +1,15 @@
 """
-Collaboration Indicators by Topic
+Collaboration Indicators by Topic --- ChatGPT
 ===============================================================================
 
 
->>> directory = "data/regtech/"
+>>> root_dir = "data/regtech/"
 
 >>> from techminer2 import techminer
->>> techminer.indicators.collaboration_indicators_by_topic("countries", directory=directory).head()
+>>> techminer.indicators.collaboration_indicators_by_topic(
+...     "countries",
+...     root_dir=root_dir,
+... ).head()
                 OCC  global_citations  ...  multiple_publication  mp_ratio
 countries                              ...                                
 United Kingdom    7               199  ...                     3      0.43
@@ -18,7 +21,12 @@ China             5                27  ...                     3      0.60
 [5 rows x 6 columns]
 
 
->>> print(techminer.indicators.collaboration_indicators_by_topic("countries", directory=directory).head().to_markdown())
+>>> print(
+...     techminer.indicators.collaboration_indicators_by_topic(
+...         "countries",
+...         root_dir=root_dir,
+...     ).head().to_markdown()
+... )
 | countries      |   OCC |   global_citations |   local_citations |   single_publication |   multiple_publication |   mp_ratio |
 |:---------------|------:|-------------------:|------------------:|---------------------:|-----------------------:|-----------:|
 | United Kingdom |     7 |                199 |                34 |                    4 |                      3 |       0.43 |
@@ -27,17 +35,22 @@ China             5                27  ...                     3      0.60
 | Ireland        |     5 |                 55 |                22 |                    4 |                      1 |       0.2  |
 | China          |     5 |                 27 |                 5 |                    2 |                      3 |       0.6  |
 
+
+
+# pylint: disable=line-too-long
+# noqa: E501
+
 """
 
 
 import numpy as np
 
-from ...record_utils import read_records
+from ... import record_utils
 
 
 def collaboration_indicators_by_topic(
     criterion,
-    directory="./",
+    root_dir="./",
     database="documents",
     start_year=None,
     end_year=None,
@@ -74,13 +87,17 @@ def collaboration_indicators_by_topic(
 
         - OCC: the number of occurrences of the criterion value.
 
-        - global_citations: the number of citations received by the documents with the criterion value.
+        - global_citations: the number of citations received by the documents \
+        with the criterion value.
 
-        - local_citations: the number of citations received by the documents in the database.
+        - local_citations: the number of citations received by the documents
+        in the database.
 
-        - single_publication: the number of documents with a single publication.
+        - single_publication: the number of documents with a single
+        publication.
 
-        - multiple_publication: the number of documents with multiple publications.
+        - multiple_publication: the number of documents with multiple
+        publications.
 
         - mp_ratio: the ratio between multiple_publication and OCC.
 
@@ -88,8 +105,8 @@ def collaboration_indicators_by_topic(
     """
 
     # Read documents from the database
-    documents = read_records(
-        root_dir=directory,
+    documents = record_utils.read_records(
+        root_dir=root_dir,
         database=database,
         start_year=start_year,
         end_year=end_year,
@@ -119,6 +136,7 @@ def collaboration_indicators_by_topic(
             "article",
         ]
     ].copy()
+
     exploded[criterion] = exploded[criterion].str.split(";")
     exploded = exploded.explode(criterion)
     exploded[criterion] = exploded[criterion].str.strip()
@@ -140,7 +158,8 @@ def collaboration_indicators_by_topic(
     )
     indicators["mp_ratio"] = indicators["mp_ratio"].round(2)
 
-    # Sort the topics by number of occurrences, global citations, and local citations
+    # Sort the topics by number of occurrences, global citations, and local
+    # citations
     indicators = indicators.sort_values(
         by=["OCC", "global_citations", "local_citations"],
         ascending=[False, False, False],
