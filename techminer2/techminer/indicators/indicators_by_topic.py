0766d9738442f7fdfaca1bc4e95f1ff3516a2e65
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-26 23:48:57 -0500

    Update indicators_by_topic.py

--- techminer2/techminer/indicators/indicators_by_topic.py
+++ techminer2/techminer/indicators/indicators_by_topic.py
@@ -13,14 +13,13 @@ Indicators by Topic --- ChatGPT
 ... ).head() # doctest: +NORMALIZE_WHITESPACE
             OCC  ...  local_citations_per_document
 authors          ...                              
-Arner DW      3  ...                             2
-Buckley RP    3  ...                             2
-Lin W         2  ...                             2
-Brennan R     2  ...                             1
-Sarea A       2  ...                             2
+Arner DW      3  ...                          2.67
+Buckley RP    3  ...                          2.67
+Lin W         2  ...                          2.00
+Brennan R     2  ...                          1.50
+Sarea A       2  ...                          2.00
 <BLANKLINE>
 [5 rows x 5 columns]
-
 
 >>> from pprint import pprint
 >>> pprint(sorted(techminer.indicators.indicators_by_topic('authors',
@@ -35,7 +34,7 @@ Sarea A       2  ...                             2
 
 """
 
-from ... import record_utils
+from ... import load_utils, record_utils
 
 
 def indicators_by_topic(
@@ -48,6 +47,42 @@ def indicators_by_topic(
 ):
     """column indicators"""
 
+    def select_columns(records, criterion):
+        return records[[criterion, "global_citations", "local_citations"]]
+
+    def explode_criterion(records, criterion):
+        records = records.copy()
+        records[criterion] = records[criterion].str.split(";")
+        records = records.explode(criterion)
+        records[criterion] = records[criterion].str.strip()
+        return records
+
+    def compute_basic_indicators(records, criterion):
+        indicators = (
+            records.groupby(criterion, as_index=True)
+            .sum()
+            .sort_values(by="OCC", ascending=False)
+        )
+        indicators = indicators.astype(int)
+        indicators = indicators[["OCC", "global_citations", "local_citations"]]
+        return indicators
+
+    def compute_global_citations_per_document(indicators):
+        indicators = indicators.assign(
+            global_citations_per_document=(
+                indicators.global_citations / indicators.OCC
+            ).round(2)
+        )
+        return indicators
+
+    def compute_local_citations_per_document(indicators):
+        indicators = indicators.assign(
+            local_citations_per_document=(
+                indicators.local_citations / indicators.OCC
+            ).round(2)
+        )
+        return indicators
+
     records = record_utils.read_records(
         root_dir=root_dir,
         database=database,
@@ -56,42 +91,16 @@ def indicators_by_topic(
         **filters,
     )
 
-    indicators = _column_indicators_from_records(criterion, records)
+    records = select_columns(records, criterion)
+    records = explode_criterion(records, criterion)
 
-    return indicators
+    records["OCC"] = 1
 
+    indicators = compute_basic_indicators(records, criterion)
+    indicators = compute_global_citations_per_document(indicators)
+    indicators = compute_local_citations_per_document(indicators)
 
-def _column_indicators_from_records(column, records):
-    records = records.assign(OCC=1)
-    columns = [column, "OCC"]
-    if "local_citations" in records.columns:
-        columns.append("local_citations")
-    if "global_citations" in records.columns:
-        columns.append("global_citations")
-    records = records[columns]
+    stopwords = load_utils.load_stopwords(root_dir=root_dir)
+    indicators = indicators.drop(stopwords, axis=0)
 
-    records[column] = records[column].str.split(";")
-    records = records.explode(column)
-    records[column] = records[column].str.strip()
-
-    indicators = (
-        records.groupby(column, as_index=True)
-        .sum()
-        .sort_values(by="OCC", ascending=False)
-    )
-
-    if "global_citations" in records.columns:
-        indicators = indicators.assign(
-            global_citations_per_document=(
-                indicators.global_citations / indicators.OCC
-            ).round(2)
-        )
-    if "local_citations" in records.columns:
-        indicators = indicators.assign(
-            local_citations_per_document=(
-                indicators.local_citations / indicators.OCC
-            ).round(2)
-        )
-
-    indicators = indicators.astype(int)
     return indicators
