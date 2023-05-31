2540d9396243793f7f72c72fdfb945c10f357cdf
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-31 15:10:33 -0500

    FEA treemap

--- techminer2/vantagepoint/report/treemap.py
+++ techminer2/vantagepoint/report/treemap.py
@@ -5,12 +5,16 @@ Treemap
 
 
 
+Example
+-------------------------------------------------------------------------------
+
+
 >>> root_dir = "data/regtech/"
 >>> file_name = "sphinx/_static/vantagepoint__treemap.html"
 
 >>> from techminer2 import vantagepoint
 >>> obj = vantagepoint.analyze.list_view(
-...    criterion='author_keywords',
+...    field='author_keywords',
 ...    root_dir=root_dir,
 ... )
 >>> chart = vantagepoint.report.treemap(obj, title="Most Frequent Author Keywords")
@@ -30,108 +34,88 @@ regulation                5
 Name: OCC, dtype: int64
 
 >>> print(chart.prompt_)
-Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
+Analyze the table below, which provides bibliometric indicators for the field 'author_keywords' in a scientific bibliography database. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
 <BLANKLINE>
-| author_keywords         |   OCC |
-|:------------------------|------:|
-| regtech                 |    28 |
-| fintech                 |    12 |
-| regulatory technology   |     7 |
-| compliance              |     7 |
-| regulation              |     5 |
-| financial services      |     4 |
-| financial regulation    |     4 |
-| artificial intelligence |     4 |
-| anti-money laundering   |     3 |
-| risk management         |     3 |
-| innovation              |     3 |
-| blockchain              |     3 |
-| suptech                 |     3 |
-| semantic technologies   |     2 |
-| data protection         |     2 |
-| smart contracts         |     2 |
-| charitytech             |     2 |
-| english law             |     2 |
-| accountability          |     2 |
-| data protection officer |     2 |
+| author_keywords         |   OCC |   global_citations |   local_citations |   global_citations_per_document |   local_citations_per_document |
+|:------------------------|------:|-------------------:|------------------:|--------------------------------:|-------------------------------:|
+| regtech                 |    28 |                329 |                74 |                           11.75 |                           2.64 |
+| fintech                 |    12 |                249 |                49 |                           20.75 |                           4.08 |
+| regulatory technology   |     7 |                 37 |                14 |                            5.29 |                           2    |
+| compliance              |     7 |                 30 |                 9 |                            4.29 |                           1.29 |
+| regulation              |     5 |                164 |                22 |                           32.8  |                           4.4  |
+| financial services      |     4 |                168 |                20 |                           42    |                           5    |
+| financial regulation    |     4 |                 35 |                 8 |                            8.75 |                           2    |
+| artificial intelligence |     4 |                 23 |                 6 |                            5.75 |                           1.5  |
+| anti-money laundering   |     3 |                 21 |                 4 |                            7    |                           1.33 |
+| risk management         |     3 |                 14 |                 8 |                            4.67 |                           2.67 |
 <BLANKLINE>
 <BLANKLINE>
 
 
 
-
+# pylint: disable=line-too-long
 """
-from dataclasses import dataclass
+import plotly.graph_objs as go
 
-import plotly.express as px
-import plotly.graph_objects as go
-
-from ... import chatgpt
-
-
-@dataclass(init=False)
-class _Chart:
-    plot_: None
-    table_: None
-    prompt_: None
+from ...classes import BasicChart
+from ...utils import check_listview
 
 
 def treemap(
     obj,
     title=None,
 ):
-    """Bar chart.
+    """Creates a treemap.
 
-    Parameters
-    ----------
-    obj : techminer2.vantagepoint.analyze.Analyze
-        An object of type Analyze.
-    title : str, optional
-        Title of the chart, by default None
+    Args:
+        obj (vantagepoint.analyze.list_view): A list view object.
+        title (str, optional): Title. Defaults to None.
+
+    Returns:
+        BasicChart: A basic chart object.
+
 
     """
 
-    result = _Chart()
-    result.plot_ = _create_plot(
-        obj,
-        title=title,
-    )
+    def create_plot():
+        """Creates a plotly treemap."""
 
-    result.table_ = obj.table_[obj.metric_]
-    result.prompt_ = chatgpt.generate_prompt_bibliographic_indicators(
-        result.table_
-    )
-
-    return result
-
-
-def _create_plot(
-    obj,
-    title=None,
-):
-    fig = go.Figure()
-    fig.add_trace(
-        go.Treemap(
-            labels=obj.table_.index,
-            parents=[""] * len(obj.table_),
-            values=obj.table_[obj.metric_],
-            textinfo="label+value",
+        fig = go.Figure()
+        fig.add_trace(
+            go.Treemap(
+                labels=obj.table_.index,
+                parents=[""] * len(obj.table_),
+                values=obj.table_[obj.metric_],
+                textinfo="label+value",
+            )
         )
-    )
-    fig.update_traces(marker={"cornerradius": 5})
-    fig.update_layout(
-        showlegend=False,
-        margin={"t": 30, "l": 0, "r": 0, "b": 0},
-        title=title if title is not None else "",
-    )
+        fig.update_traces(marker={"cornerradius": 5})
+        fig.update_layout(
+            showlegend=False,
+            margin={"t": 30, "l": 0, "r": 0, "b": 0},
+            title=title if title is not None else "",
+        )
 
-    # Change the colors of the treemap white
-    fig.update_traces(
-        marker={"line": {"color": "darkslategray", "width": 1}},
-        marker_colors=["white"] * len(obj.table_),
-    )
+        # Change the colors of the treemap white
+        fig.update_traces(
+            marker={"line": {"color": "darkslategray", "width": 1}},
+            marker_colors=["white"] * len(obj.table_),
+        )
 
-    # Change the font size of the labels
-    fig.update_traces(textfont_size=12)
+        # Change the font size of the labels
+        fig.update_traces(textfont_size=12)
 
-    return fig
+        return fig
+
+    #
+    # Main code:
+    #
+
+    check_listview(obj)
+
+    chart = BasicChart()
+    chart.plot_ = create_plot()
+    chart.table_ = obj.table_[obj.metric_]
+    chart.prompt_ = obj.prompt_
+
+    return chart
