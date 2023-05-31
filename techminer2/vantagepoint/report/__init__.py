44ced32c846dee11cfc7237ffda0d5741df3ea26
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-31 14:38:34 -0500

    Update __init__.py

--- techminer2/vantagepoint/report/__init__.py
+++ techminer2/vantagepoint/report/__init__.py
@@ -1,3 +1,8 @@
+"""
+VantagePoint plotting functions based on the user interface.
+
+"""
+
 from .bar_chart import bar_chart
 from .bubble_chart import bubble_chart
 from .cleveland_dot_chart import cleveland_dot_chart
@@ -9,3 +14,17 @@ from .pie_chart import pie_chart
 from .treemap import treemap
 from .word_cloud import word_cloud
 from .world_map import world_map
+
+__all__ = [
+    "bar_chart",
+    "bubble_chart",
+    "cleveland_dot_chart",
+    "column_chart",
+    "gantt_chart",
+    "heat_map",
+    "line_chart",
+    "pie_chart",
+    "treemap",
+    "word_cloud",
+    "world_map",
+]
