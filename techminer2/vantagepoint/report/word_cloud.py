9025331543d9105199c5dbdf8a0075c241499048
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-31 15:26:32 -0500

    FEA word-cloud

--- techminer2/vantagepoint/report/word_cloud.py
+++ techminer2/vantagepoint/report/word_cloud.py
@@ -4,16 +4,21 @@ Word cloud
 ===============================================================================
 
 
+
+
+Example
+-------------------------------------------------------------------------------
+
 >>> root_dir = "data/regtech/"
->>> file_name = "sphinx/images/vantagepoint__word_cloud.png"
+>>> file_name = "sphinx/_static/vantagepoint__word_cloud.html"
 
 >>> from techminer2 import vantagepoint
 >>> obj = vantagepoint.analyze.list_view(
-...     criterion='author_keywords',
+...     field='author_keywords',
 ...     root_dir=root_dir,
 ... )
 >>> chart = vantagepoint.report.word_cloud(obj, title="Most Frequent Author Keywords")
->>> chart.plot_.savefig(file_name)
+>>> chart.plot_.write_html(file_name)
 
 .. image:: ../../images/vantagepoint__word_cloud.png
     :width: 900px
@@ -30,48 +35,34 @@ Name: OCC, dtype: int64
 
 
 >>> print(chart.prompt_)
-Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
+Analyze the table below, which provides bibliometric indicators for the field 'author_keywords' in a scientific bibliography database. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
 <BLANKLINE>
-| author_keywords         |   OCC |
-|:------------------------|------:|
-| regtech                 |    28 |
-| fintech                 |    12 |
-| regulatory technology   |     7 |
-| compliance              |     7 |
-| regulation              |     5 |
-| financial services      |     4 |
-| financial regulation    |     4 |
-| artificial intelligence |     4 |
-| anti-money laundering   |     3 |
-| risk management         |     3 |
-| innovation              |     3 |
-| blockchain              |     3 |
-| suptech                 |     3 |
-| semantic technologies   |     2 |
-| data protection         |     2 |
-| smart contracts         |     2 |
-| charitytech             |     2 |
-| english law             |     2 |
-| accountability          |     2 |
-| data protection officer |     2 |
+| author_keywords         |   OCC |   global_citations |   local_citations |   global_citations_per_document |   local_citations_per_document |
+|:------------------------|------:|-------------------:|------------------:|--------------------------------:|-------------------------------:|
+| regtech                 |    28 |                329 |                74 |                           11.75 |                           2.64 |
+| fintech                 |    12 |                249 |                49 |                           20.75 |                           4.08 |
+| regulatory technology   |     7 |                 37 |                14 |                            5.29 |                           2    |
+| compliance              |     7 |                 30 |                 9 |                            4.29 |                           1.29 |
+| regulation              |     5 |                164 |                22 |                           32.8  |                           4.4  |
+| financial services      |     4 |                168 |                20 |                           42    |                           5    |
+| financial regulation    |     4 |                 35 |                 8 |                            8.75 |                           2    |
+| artificial intelligence |     4 |                 23 |                 6 |                            5.75 |                           1.5  |
+| anti-money laundering   |     3 |                 21 |                 4 |                            7    |                           1.33 |
+| risk management         |     3 |                 14 |                 8 |                            4.67 |                           2.67 |
 <BLANKLINE>
 <BLANKLINE>
 
+
+
+# pylint: disable=line-too-long
 """
-from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.tools as tls
 from wordcloud import WordCloud
 
-from ... import chatgpt
-
-
-@dataclass(init=False)
-class _Chart:
-    plot_: None
-    table_: None
-    prompt_: None
+from ...classes import BasicChart
 
 
 def word_cloud(
@@ -79,45 +70,52 @@ def word_cloud(
     title=None,
     figsize=(10, 10),
 ):
-    result = _Chart()
-    result.plot_ = _create_plot(
-        obj,
-        title=title,
-        figsize=figsize,
-    )
+    """Creates a word cloud.
 
-    result.table_ = obj.table_[obj.metric_]
-    result.prompt_ = chatgpt.generate_prompt_bibliographic_indicators(
-        result.table_
-    )
+    Args:
+        obj (vantagepoint.analyze.list_view): A list view object.
+        title (str, optional): Title. Defaults to None.
+        figsize (tuple, optional): Figure size. Defaults to (10, 10).
 
-    return result
+    Returns:
+        BasicChart: A basic chart object.
 
 
-def _create_plot(
-    obj,
-    title,
-    figsize,
-):
-    x_mask, y_mask = np.ogrid[:300, :300]
-    mask = (x_mask - 150) ** 2 + (y_mask - 150) ** 2 > 130**2
-    mask = 255 * mask.astype(int)
+    """
 
-    wordcloud = WordCloud(background_color="white", repeat=True, mask=mask)
+    def create_plot():
+        x_mask, y_mask = np.ogrid[:300, :300]
+        mask = (x_mask - 150) ** 2 + (y_mask - 150) ** 2 > 130**2
+        mask = 255 * mask.astype(int)
 
-    text = dict(zip(obj.table_.index, obj.table_[obj.metric_]))
-    wordcloud.generate_from_frequencies(text)
-    wordcloud.recolor(color_func=_recolor)
+        wordcloud = WordCloud(background_color="white", repeat=True, mask=mask)
 
-    plt.Figure(figsize=figsize)
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    if title is not None:
-        plt.title(title)
-    plt.tight_layout(pad=0)
+        text = dict(zip(obj.table_.index, obj.table_[obj.metric_]))
+        wordcloud.generate_from_frequencies(text)
+        wordcloud.recolor(color_func=_recolor)
 
-    return plt.gcf()
+        plt.Figure(figsize=figsize)
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        if title is not None:
+            plt.title(title)
+        plt.tight_layout(pad=0)
 
+        fig = tls.mpl_to_plotly(plt.gcf())
 
-def _recolor(word, **kwargs):
-    return "black"
+        return fig
+
+    def _recolor(word, **kwargs):
+        # pylint: disable=unused-argument
+        return "black"
+
+    #
+    # Main code:
+    #
+
+    chart = BasicChart()
+    chart.plot_ = create_plot()
+    chart.table_ = obj.table_[obj.metric_]
+    chart.prompt_ = obj.prompt_
+
+    return chart
