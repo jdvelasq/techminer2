be95fa701e6f24517a1545ab15efa5ffeabadd82
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-05-31 14:52:47 -0500

    FEA line-chart

--- techminer2/vantagepoint/report/line_chart.py
+++ techminer2/vantagepoint/report/line_chart.py
@@ -4,12 +4,16 @@ Line Chart
 ===============================================================================
 
 
+
+Example
+-------------------------------------------------------------------------------
+
 >>> root_dir = "data/regtech/"
 >>> file_name = "sphinx/_static/vantagepoint__line_chart.html"
 
 >>> from techminer2 import vantagepoint
 >>> obj = vantagepoint.analyze.list_view(
-...    criterion='author_keywords',
+...    field='author_keywords',
 ...    root_dir=root_dir,
 ... )
 >>> chart = vantagepoint.report.line_chart(obj, title="Most Frequent Author Keywords")
@@ -30,128 +34,102 @@ regulation                5
 Name: OCC, dtype: int64
 
 >>> print(chart.prompt_)
-Analyze the table below, which provides bibliographic indicators for a collection of research articles. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
+Analyze the table below, which provides bibliometric indicators for the field 'author_keywords' in a scientific bibliography database. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
 <BLANKLINE>
-| author_keywords         |   OCC |
-|:------------------------|------:|
-| regtech                 |    28 |
-| fintech                 |    12 |
-| regulatory technology   |     7 |
-| compliance              |     7 |
-| regulation              |     5 |
-| financial services      |     4 |
-| financial regulation    |     4 |
-| artificial intelligence |     4 |
-| anti-money laundering   |     3 |
-| risk management         |     3 |
-| innovation              |     3 |
-| blockchain              |     3 |
-| suptech                 |     3 |
-| semantic technologies   |     2 |
-| data protection         |     2 |
-| smart contracts         |     2 |
-| charitytech             |     2 |
-| english law             |     2 |
-| accountability          |     2 |
-| data protection officer |     2 |
+| author_keywords         |   OCC |   global_citations |   local_citations |   global_citations_per_document |   local_citations_per_document |
+|:------------------------|------:|-------------------:|------------------:|--------------------------------:|-------------------------------:|
+| regtech                 |    28 |                329 |                74 |                           11.75 |                           2.64 |
+| fintech                 |    12 |                249 |                49 |                           20.75 |                           4.08 |
+| regulatory technology   |     7 |                 37 |                14 |                            5.29 |                           2    |
+| compliance              |     7 |                 30 |                 9 |                            4.29 |                           1.29 |
+| regulation              |     5 |                164 |                22 |                           32.8  |                           4.4  |
+| financial services      |     4 |                168 |                20 |                           42    |                           5    |
+| financial regulation    |     4 |                 35 |                 8 |                            8.75 |                           2    |
+| artificial intelligence |     4 |                 23 |                 6 |                            5.75 |                           1.5  |
+| anti-money laundering   |     3 |                 21 |                 4 |                            7    |                           1.33 |
+| risk management         |     3 |                 14 |                 8 |                            4.67 |                           2.67 |
 <BLANKLINE>
 <BLANKLINE>
 
 
 
+
+# pylint: disable=line-too-long
 """
-from dataclasses import dataclass
-
 import plotly.express as px
 
-from ... import chatgpt
-
-
-@dataclass(init=False)
-class _Chart:
-    plot_: None
-    table_: None
-    prompt_: None
+from ...classes import BasicChart
+from ...utils import check_listview
 
 
 def line_chart(
     obj,
     title=None,
-    x_label=None,
-    y_label=None,
+    field_label=None,
+    metric_label=None,
 ):
-    """Bar chart.
+    """Creates a line chart.
 
-    Parameters
-    ----------
-    obj : techminer2.vantagepoint.analyze.Analyze
-        An object of type Analyze.
-    title : str, optional
-        Title of the chart, by default None
-    x_label : str, optional
-        Label of the x-axis, by default None
-    y_label : str, optional
-        Label of the y-axis, by default None
+    Args:
+        obj (vantagepoint.analyze.list_view): A list view object.
+        title (str, optional): Title. Defaults to None.
+        metric_label (str, optional): Metric label. Defaults to None.
+        field_label (str, optional): Field label. Defaults to None.
+
+    Returns:
+        BasicChart: A basic chart object.
 
     """
 
-    result = _Chart()
-    result.plot_ = _create_plot(
-        obj,
-        title=title,
-        x_label=x_label,
-        y_label=y_label,
-    )
+    def create_plot():
+        figure = px.line(
+            obj.table_,
+            x=None,
+            y=obj.metric_,
+            hover_data=obj.table_.columns.to_list(),
+            markers=True,
+        )
 
-    result.table_ = obj.table_[obj.metric_]
-    result.prompt_ = chatgpt.generate_prompt_bibliographic_indicators(
-        result.table_
-    )
+        figure.update_layout(
+            paper_bgcolor="white",
+            plot_bgcolor="white",
+            title_text=title if title is not None else "",
+        )
+        figure.update_traces(
+            marker=dict(size=9, line=dict(color="darkslategray", width=2)),
+            marker_color="rgb(171,171,171)",
+            line=dict(color="darkslategray"),
+        )
+        figure.update_xaxes(
+            linecolor="gray",
+            linewidth=2,
+            gridcolor="lightgray",
+            griddash="dot",
+            tickangle=270,
+            title_text=field_label
+            if field_label is not None
+            else obj.criterion_.replace("_", " ").upper(),
+        )
+        figure.update_yaxes(
+            linecolor="gray",
+            linewidth=2,
+            gridcolor="lightgray",
+            griddash="dot",
+            title_text=metric_label
+            if metric_label is not None
+            else obj.metric_.replace("_", " ").upper(),
+        )
+        return figure
 
-    return result
+    #
+    # Main code
+    #
 
+    check_listview(obj)
 
-def _create_plot(
-    obj,
-    title=None,
-    x_label=None,
-    y_label=None,
-):
-    figure = px.line(
-        obj.table_,
-        x=None,
-        y=obj.metric_,
-        hover_data=obj.table_.columns.to_list(),
-        markers=True,
-    )
+    chart = BasicChart()
+    chart.plot_ = create_plot()
+    chart.table_ = obj.table_[obj.metric_]
+    chart.prompt_ = obj.prompt_
 
-    figure.update_layout(
-        paper_bgcolor="white",
-        plot_bgcolor="white",
-        title_text=title if title is not None else "",
-    )
-    figure.update_traces(
-        marker=dict(size=9, line=dict(color="darkslategray", width=2)),
-        marker_color="rgb(171,171,171)",
-        line=dict(color="darkslategray"),
-    )
-    figure.update_xaxes(
-        linecolor="gray",
-        linewidth=2,
-        gridcolor="lightgray",
-        griddash="dot",
-        tickangle=270,
-        title_text=x_label
-        if x_label is not None
-        else obj.criterion_.replace("_", " ").upper(),
-    )
-    figure.update_yaxes(
-        linecolor="gray",
-        linewidth=2,
-        gridcolor="lightgray",
-        griddash="dot",
-        title_text=y_label
-        if y_label is not None
-        else obj.metric_.replace("_", " ").upper(),
-    )
-    return figure
+    return chart
