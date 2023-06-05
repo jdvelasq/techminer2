10e9be8614fa7fe5ea0b30f88ce71a231fa8adef
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-06-05 08:06:33 -0500

    Update __init__.py

--- techminer2/vantagepoint/refine/__init__.py
+++ techminer2/vantagepoint/refine/__init__.py
@@ -2,10 +2,6 @@ from .apply_countries_thesaurus import apply_countries_thesaurus
 from .apply_keywords_thesaurus import apply_keywords_thesaurus
 from .apply_organizations_thesaurus import apply_organizations_thesaurus
 from .apply_thesaurus import apply_thesaurus
-
-# from .create_countries_thesaurus import create_countries_thesaurus
-# from .create_keywords_thesaurus import create_keywords_thesaurus
-# from .create_organizations_thesaurus import create_organizations_thesaurus
 from .create_thesaurus import create_thesaurus
 from .find_abbreviations import find_abbreviations
 from .find_string import find_string
