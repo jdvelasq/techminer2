3b695fe9a6664ee234f92ce66b8e7e90dacb8f4e
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-06-06 10:08:19 -0500

    FEA bar-chart

--- techminer2/vantagepoint/report/bar_chart.py
+++ techminer2/vantagepoint/report/bar_chart.py
@@ -25,31 +25,31 @@ Example
 
 >>> chart.table_.head()
 author_keywords
-regtech                  28
-fintech                  12
-regulatory technology     7
-compliance                7
-regulation                5
+REGTECH               28
+FINTECH               12
+COMPLIANCE             7
+REGULATION             5
+FINANCIAL_SERVICES     4
 Name: OCC, dtype: int64
-
 
 >>> print(chart.prompt_)
 Analyze the table below, which provides bibliometric indicators for the field 'author_keywords' in a scientific bibliography database. Identify any notable patterns, trends, or outliers in the data, and discuss their implications for the research field. Be sure to provide a concise summary of your findings in no more than 150 words.
 <BLANKLINE>
-| author_keywords         |   OCC |   global_citations |   local_citations |   global_citations_per_document |   local_citations_per_document |
-|:------------------------|------:|-------------------:|------------------:|--------------------------------:|-------------------------------:|
-| regtech                 |    28 |                329 |                74 |                           11.75 |                           2.64 |
-| fintech                 |    12 |                249 |                49 |                           20.75 |                           4.08 |
-| regulatory technology   |     7 |                 37 |                14 |                            5.29 |                           2    |
-| compliance              |     7 |                 30 |                 9 |                            4.29 |                           1.29 |
-| regulation              |     5 |                164 |                22 |                           32.8  |                           4.4  |
-| financial services      |     4 |                168 |                20 |                           42    |                           5    |
-| financial regulation    |     4 |                 35 |                 8 |                            8.75 |                           2    |
-| artificial intelligence |     4 |                 23 |                 6 |                            5.75 |                           1.5  |
-| anti-money laundering   |     3 |                 21 |                 4 |                            7    |                           1.33 |
-| risk management         |     3 |                 14 |                 8 |                            4.67 |                           2.67 |
+| author_keywords                 |   OCC |   global_citations |   local_citations |   global_citations_per_document |   local_citations_per_document |
+|:--------------------------------|------:|-------------------:|------------------:|--------------------------------:|-------------------------------:|
+| REGTECH                         |    28 |                329 |                74 |                           11.75 |                           2.64 |
+| FINTECH                         |    12 |                249 |                49 |                           20.75 |                           4.08 |
+| COMPLIANCE                      |     7 |                 30 |                 9 |                            4.29 |                           1.29 |
+| REGULATION                      |     5 |                164 |                22 |                           32.8  |                           4.4  |
+| FINANCIAL_SERVICES              |     4 |                168 |                20 |                           42    |                           5    |
+| FINANCIAL_REGULATION            |     4 |                 35 |                 8 |                            8.75 |                           2    |
+| REGULATORY_TECHNOLOGY (REGTECH) |     4 |                 30 |                10 |                            7.5  |                           2.5  |
+| ARTIFICIAL_INTELLIGENCE         |     4 |                 23 |                 6 |                            5.75 |                           1.5  |
+| ANTI-MONEY_LAUNDERING           |     3 |                 21 |                 4 |                            7    |                           1.33 |
+| RISK_MANAGEMENT                 |     3 |                 14 |                 8 |                            4.67 |                           2.67 |
 <BLANKLINE>
 <BLANKLINE>
+
 
 
 # pylint: disable=line-too-long
