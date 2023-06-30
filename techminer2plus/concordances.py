897346e3a3a47ab50b0df0d9f392ddc0e5719356
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-06-30 11:26:39 -0500

    Update concordances.py

--- techminer2plus/concordances.py
+++ techminer2plus/concordances.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+# pylint: disable=line-too-long
 """
 .. _concordances:
 
@@ -12,46 +13,7 @@ Abstract concordances exploration tool.
 >>> root_dir = "data/regtech/"
 
 
-* Computational interface:
-
->>> contexts_, frame_, prompt_ = concordances(
-...     root_dir=root_dir,
-...     search_for='REGTECH',
-...     top_n=10,
-... )
->>> print(contexts_)
-                                                             REGTECH can provide an invaluable tool, in a BUSINESS as usual E >>>
-                                                             REGTECH developments are leading towards a paradigm shift necess >>>
-                                                             REGTECH to date has focused on the DIGITIZATION of manual REPORT >>>
-                                                             REGTECH will not eliminate policy considerations, nor will IT re >>>
-           although also not a panacea, the DEVELOPMENT of " REGTECH " solutions will help clear away volumes of work that un >>>
-<<< s the promise and potential of REGULATORY_TECHNOLOGIES ( REGTECH ), a new and vital dimension to FINTECH
-<<< paper, the authors propose a novel, regular TECHNOLOGY ( REGTECH ) cum automated legal text approach for financial TRANSA >>>
-                     2020 the authorsregulatory TECHNOLOGY ( REGTECH )
-                                     REGULATORY_TECHNOLOGY ( REGTECH ) is an emerging TECHNOLOGY trend leveraging INFORMATION >>>
-<<< llustrate the impact of adopting REGULATORY_TECHNOLOGY ( REGTECH ) INNOVATIONS in BANKS on MONEY_LAUNDERING prevention ef >>>
-<<< rpose of this paper is to explore the solutions that AI, REGTECH and CHARITYTECH provide to charities in navigating the v >>>
-                                                in contrast, REGTECH has recently brought great SUCCESS to financial COMPLIAN >>>
-<<< the area of FINANCIAL_REGULATION (REGULATORY_TECHNOLOGY: REGTECH ) can significantly improve FINANCIAL_DEVELOPMENT outcomes
-<<< that together they are underpinning the DEVELOPMENT of a REGTECH ECOSYSTEM in EUROPE and will continue to do so
-                                 an option is to incorporate REGTECH into the DIGITAL_TRANSFORMATION STRATEGY of a MANAGEMENT >>>
-<<< egulator based SELF_ASSESSMENT checklist to establish if REGTECH best practice could improve the demonstration of GDPR_CO >>>
-                 the chapter notes that the full BENEFITS of REGTECH will only materialise if the pitfalls of a fragmented to >>>
-<<< ld, sets the foundation for a practical understanding of REGTECH , and proposes sequenced reforms that could BENEFIT regu >>>
-                                   however, the potential of REGTECH is far greater  IT has the potential to enable a nearly  >>>
-                        this paper explores the potential of REGTECH and the merit of incorporating IT into a SMART_TREASURY  >>>
-<<< ral awareness concerning the ADOPTION and integration of REGTECH PLATFORMS for fighting MONEY_LAUNDERING
-<<< ING through REGTECH and cost  and time-saving aspects of REGTECH , drive MONEY_LAUNDERING prevention effectiveness to a h >>>
-                 nevertheless, a sophisticated deployment of REGTECH should help focus regulatory discretion and PUBLIC_POLIC >>>
-<<< ndings provide specific insights about the deployment of REGTECH capabilities in BANKS in regional BANKING centers of mod >>>
-<<< and regulators, and provided an ENVIRONMENT within which REGTECH can flourish
-<<< l systems requires increasing the use of and reliance on REGTECH 
-<<< ide insights for other societies in developing their own REGTECH ECOSYSTEMS in order to support more efficient, stable, i >>>
-                                             europes road to REGTECH has rested upon four apparently unrelated pillars: (1) e >>>
-<<<  five-year research programme to highlight the role that REGTECH can play in making REGULATORY_COMPLIANCE more efficient  >>>
-<<< otwithstanding the RISK_REDUCTIONS and cost savings that REGTECH can deliver
-<<< emantically enabled applications can be made possible by REGTECH 
-
+* **USER COMPUTATIONAL INTERFACE:**
 
 >>> concordances = (
 ...     tm2p.Records(root_dir=root_dir)
@@ -236,8 +198,46 @@ SMART_TREASURY department
 
 
 
+* **COMPUTATIONAL API:**
 
-# pylint: disable=line-too-long
+>>> contexts_, frame_, prompt_ = concordances(
+...     root_dir=root_dir,
+...     search_for='REGTECH',
+...     top_n=10,
+... )
+>>> print(contexts_)
+                                                             REGTECH can provide an invaluable tool, in a BUSINESS as usual E >>>
+                                                             REGTECH developments are leading towards a paradigm shift necess >>>
+                                                             REGTECH to date has focused on the DIGITIZATION of manual REPORT >>>
+                                                             REGTECH will not eliminate policy considerations, nor will IT re >>>
+           although also not a panacea, the DEVELOPMENT of " REGTECH " solutions will help clear away volumes of work that un >>>
+<<< s the promise and potential of REGULATORY_TECHNOLOGIES ( REGTECH ), a new and vital dimension to FINTECH
+<<< paper, the authors propose a novel, regular TECHNOLOGY ( REGTECH ) cum automated legal text approach for financial TRANSA >>>
+                     2020 the authorsregulatory TECHNOLOGY ( REGTECH )
+                                     REGULATORY_TECHNOLOGY ( REGTECH ) is an emerging TECHNOLOGY trend leveraging INFORMATION >>>
+<<< llustrate the impact of adopting REGULATORY_TECHNOLOGY ( REGTECH ) INNOVATIONS in BANKS on MONEY_LAUNDERING prevention ef >>>
+<<< rpose of this paper is to explore the solutions that AI, REGTECH and CHARITYTECH provide to charities in navigating the v >>>
+                                                in contrast, REGTECH has recently brought great SUCCESS to financial COMPLIAN >>>
+<<< the area of FINANCIAL_REGULATION (REGULATORY_TECHNOLOGY: REGTECH ) can significantly improve FINANCIAL_DEVELOPMENT outcomes
+<<< that together they are underpinning the DEVELOPMENT of a REGTECH ECOSYSTEM in EUROPE and will continue to do so
+                                 an option is to incorporate REGTECH into the DIGITAL_TRANSFORMATION STRATEGY of a MANAGEMENT >>>
+<<< egulator based SELF_ASSESSMENT checklist to establish if REGTECH best practice could improve the demonstration of GDPR_CO >>>
+                 the chapter notes that the full BENEFITS of REGTECH will only materialise if the pitfalls of a fragmented to >>>
+<<< ld, sets the foundation for a practical understanding of REGTECH , and proposes sequenced reforms that could BENEFIT regu >>>
+                                   however, the potential of REGTECH is far greater  IT has the potential to enable a nearly  >>>
+                        this paper explores the potential of REGTECH and the merit of incorporating IT into a SMART_TREASURY  >>>
+<<< ral awareness concerning the ADOPTION and integration of REGTECH PLATFORMS for fighting MONEY_LAUNDERING
+<<< ING through REGTECH and cost  and time-saving aspects of REGTECH , drive MONEY_LAUNDERING prevention effectiveness to a h >>>
+                 nevertheless, a sophisticated deployment of REGTECH should help focus regulatory discretion and PUBLIC_POLIC >>>
+<<< ndings provide specific insights about the deployment of REGTECH capabilities in BANKS in regional BANKING centers of mod >>>
+<<< and regulators, and provided an ENVIRONMENT within which REGTECH can flourish
+<<< l systems requires increasing the use of and reliance on REGTECH 
+<<< ide insights for other societies in developing their own REGTECH ECOSYSTEMS in order to support more efficient, stable, i >>>
+                                             europes road to REGTECH has rested upon four apparently unrelated pillars: (1) e >>>
+<<<  five-year research programme to highlight the role that REGTECH can play in making REGULATORY_COMPLIANCE more efficient  >>>
+<<< otwithstanding the RISK_REDUCTIONS and cost savings that REGTECH can deliver
+<<< emantically enabled applications can be made possible by REGTECH 
+
 """
 import os.path
 import textwrap
@@ -354,10 +354,7 @@ def concordances(
     cited_by_filter: tuple = (None, None),
     **filters,
 ):
-    """Checks the occurrence contexts of a given text in the abstract's phrases.
-
-    :meta private:
-    """
+    """Checks the occurrence contexts of a given text in the abstract's phrases."""
 
     def get_phrases(records):
         """Gets the phrases with the searched text."""
