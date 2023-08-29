3e587d31d26784a4b32e9ccba82a235f2a6e1213
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-08-29 17:04:33 -0500

    Update __init__.py

--- techminer2/refine/words/__init__.py
+++ techminer2/refine/words/__init__.py
@@ -2,6 +2,7 @@ from .apply_thesaurus import apply_thesaurus
 from .find_abbreviations import find_abbreviations
 from .find_string import find_string
 from .fuzzy_search import fuzzy_search
+from .join_thesaurus_keys import join_thesaurus_keys
 from .misspelling_search import misspelling_search
 from .sort_thesaurus import sort_thesaurus
 from .words_frequency import words_frequency
