09076fbcc9f6e991018fc9426d6e85673a1d41a2
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-08-29 17:04:38 -0500

    Update __init__.py

--- techminer2/ingest/__init__.py
+++ techminer2/ingest/__init__.py
@@ -1,5 +1,6 @@
 """Ingest and clean data."""
 
+from .create_words_thesaurus import create_words_thesaurus
 from .ingest_raw_data import ingest_raw_data
 from .raw_document_types import raw_document_types
 from .summary_sheet import summary_sheet
