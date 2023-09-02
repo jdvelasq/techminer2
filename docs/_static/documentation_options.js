4220700796e74575f0e2749ee1aeb6d7ee45bda2
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-09-02 11:52:39 -0500

    DOC: Aug 30, 2023 Version

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Ago 24, 2023',
+    VERSION: 'Aug 30, 2023',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
