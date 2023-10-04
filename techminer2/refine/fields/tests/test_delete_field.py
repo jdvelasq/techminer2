69a3e93fa5750d4b91024bc083abf8f94f711664
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-10-04 14:16:43 -0500

    Update test_delete_field.py

--- techminer2/refine/fields/tests/test_delete_field.py
+++ techminer2/refine/fields/tests/test_delete_field.py
@@ -16,17 +16,15 @@ def test_delete_field():
     """Test copy_field."""
 
     # Test data:
-    test_df = pd.DataFrame(
-        {
-            "col_a": ["a", "b", "c"],
-            "col_b": ["a", "b", "c"],
-        }
-    )
     if not os.path.exists("tmp/databases"):
         os.makedirs("tmp/databases")
-
     for file in DATABASE_FILES:
-        test_df.to_csv(file, index=False, compression="zip")
+        pd.DataFrame(
+            {
+                "col_a": ["a", "b", "c"],
+                "col_b": ["a", "b", "c"],
+            }
+        ).to_csv(file, index=False, compression="zip")
 
     # Run:
     from techminer2.refine.fields import delete_field
