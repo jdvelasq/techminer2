7b6b9a8aa7b17e37e9062deb1b83532d8b586d6b
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-10-04 14:24:22 -0500

    Update test_rename_field.py

--- techminer2/refine/fields/tests/test_rename_field.py
+++ techminer2/refine/fields/tests/test_rename_field.py
@@ -16,16 +16,14 @@ def test_rename_field():
     """Test rename_field."""
 
     # Test data:
-    test_df = pd.DataFrame(
-        {
-            "col_a": ["a", "b", "c"],
-        }
-    )
     if not os.path.exists("tmp/databases"):
         os.makedirs("tmp/databases")
-
     for file in DATABASE_FILES:
-        test_df.to_csv(file, index=False, compression="zip")
+        pd.DataFrame(
+            {
+                "col_a": ["a", "b", "c"],
+            }
+        ).to_csv(file, index=False, compression="zip")
 
     # Run:
     from techminer2.refine.fields import rename_field
