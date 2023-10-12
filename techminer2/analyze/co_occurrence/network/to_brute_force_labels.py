9f8951d8af01844af8e02a339efdd420c11c1b05
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-10-11 20:20:03 -0500

    Update to_brute_force_labels.py

--- techminer2/analyze/co_occurrence/network/to_brute_force_labels.py
+++ techminer2/analyze/co_occurrence/network/to_brute_force_labels.py
@@ -11,7 +11,7 @@ To Brute Force Labels
 
 
 >>> from techminer2.analyze.co_occurrence.network import to_brute_force_labels
->>> to_brute_force_labels(
+>>> brute_force_labels = to_brute_force_labels(
 ...     #
 ...     # PARAMS:
 ...     field='author_keywords',
@@ -31,8 +31,64 @@ To Brute Force Labels
 ...     database="main",
 ...     year_filter=(None, None),
 ...     cited_by_filter=(None, None),
-... )  # doctest: +ELLIPSIS
-{'FINTECH 31:5168': 0, 'FINANCIAL_INCLUSION 03:0590': 0, ...
+... )
+>>> from pprint import pprint
+>>> pprint(brute_force_labels)
+{'ARTIFICIAL_INTELLIGENCE 02:0327': 0,
+ 'BANKING 03:0375': 1,
+ 'BLOCKCHAIN 03:0369': 0,
+ 'BUSINESS 03:0896': 2,
+ 'CASE_STUDIES 03:0442': 0,
+ 'CROWDFUNDING 03:0335': 0,
+ 'CYBER_SECURITY 02:0342': 0,
+ 'DIGITAL 03:0434': 1,
+ 'FINANCIAL_INCLUSION 03:0590': 0,
+ 'FINANCIAL_INSTITUTION 02:0484': 1,
+ 'FINANCIAL_SERVICES 04:0667': 2,
+ 'FINANCIAL_TECHNOLOGY 04:0551': 2,
+ 'FINTECH 31:5168': 0,
+ 'FUTURE_RESEARCH 02:0691': 2,
+ 'INNOVATION 07:0911': 1,
+ 'MARKETPLACE_LENDING 03:0317': 3,
+ 'MOBILE_PAYMENT 03:0309': 0,
+ 'PEER_TO_PEER_LENDING 03:0324': 3,
+ 'SHADOW_BANKING 03:0643': 3,
+ 'TECHNOLOGIES 02:0310': 1}
+
+>>> from techminer2.analyze.co_occurrence.network import communities
+>>> print(communities(
+...     #
+...     # PARAMS:
+...     field='author_keywords',
+...     #
+...     # COLUMN PARAMS:
+...     top_n=20,
+...     occ_range=(None, None),
+...     gc_range=(None, None),
+...     custom_items=None,
+...     #
+...     # NETWORK PARAMS:
+...     algorithm_or_dict=brute_force_labels,
+...     association_index="association",
+...     #
+...     # DATABASE PARAMS:
+...     root_dir="example/", 
+...     database="main",
+...     year_filter=(None, None),
+...     cited_by_filter=(None, None),
+... ).to_markdown())
+|    | CL_0                            | CL_1                          | CL_2                         | CL_3                         |
+|---:|:--------------------------------|:------------------------------|:-----------------------------|:-----------------------------|
+|  0 | FINTECH 31:5168                 | INNOVATION 07:0911            | FINANCIAL_SERVICES 04:0667   | SHADOW_BANKING 03:0643       |
+|  1 | FINANCIAL_INCLUSION 03:0590     | DIGITAL 03:0434               | FINANCIAL_TECHNOLOGY 04:0551 | PEER_TO_PEER_LENDING 03:0324 |
+|  2 | CASE_STUDIES 03:0442            | BANKING 03:0375               | BUSINESS 03:0896             | MARKETPLACE_LENDING 03:0317  |
+|  3 | BLOCKCHAIN 03:0369              | FINANCIAL_INSTITUTION 02:0484 | FUTURE_RESEARCH 02:0691      |                              |
+|  4 | CROWDFUNDING 03:0335            | TECHNOLOGIES 02:0310          |                              |                              |
+|  5 | MOBILE_PAYMENT 03:0309          |                               |                              |                              |
+|  6 | CYBER_SECURITY 02:0342          |                               |                              |                              |
+|  7 | ARTIFICIAL_INTELLIGENCE 02:0327 |                               |                              |                              |
+
+
 
 """
 from ...._common.nx_create_co_occurrence_graph import nx_create_co_occurrence_graph
