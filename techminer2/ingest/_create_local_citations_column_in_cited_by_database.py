156a837d1a9013993e525af5be1c315a412576e9
Juan David Velásquez-Henao <8204100+jdvelasq@users.noreply.github.com>
2023-10-21 08:09:37 -0500

    fix: error in local-citations when database not exisits

--- techminer2/ingest/_create_local_citations_column_in_cited_by_database.py
+++ techminer2/ingest/_create_local_citations_column_in_cited_by_database.py
@@ -8,10 +8,11 @@ import pandas as pd
 def create_local_citations_column_in_cited_by_database(root_dir):
     """Create local citations column in cited-by database."""
 
-    # message("Creating `local_citations` column in documents database")
-
-    # counts the number of citations for each local reference
     documents_path = os.path.join(root_dir, "databases", "_cited_by.csv.zip")
+    #
+    if not os.path.exists(documents_path):
+        return
+    #
     documents = pd.read_csv(documents_path, compression="zip")
     documents["local_citations"] = 0
 
