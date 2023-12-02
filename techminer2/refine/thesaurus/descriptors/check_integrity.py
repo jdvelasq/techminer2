1934f4597f7eee98020be0feaaedcb1cac34b2a9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2023-12-01 22:14:56 -0500

    check integrity

---
+++ techminer2/refine/thesaurus/descriptors/check_integrity.py
@@ -0,0 +1,70 @@
+# flake8: noqa
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
+"""
+Apply Thesaurus 
+===============================================================================
+
+
+>>> from techminer2.refine.thesaurus.descriptors import apply_thesaurus
+>>> apply_thesaurus(
+...     #
+...     # DATABASE PARAMS:
+...     root_dir="example/", 
+... )
+--INFO-- Applying `keywords.txt` thesaurus to author/index keywords and abstract/title words
+
+"""
+
+import glob
+import os
+import os.path
+
+import pandas as pd
+
+from ...._common.thesaurus_lib import load_system_thesaurus_as_dict_reversed
+
+THESAURUS_FILE = "thesauri/descriptors.the.txt"
+
+
+def check_integrity(
+    #
+    # DATABASE PARAMS:
+    root_dir="./",
+):
+    """:meta private:"""
+
+    # pylint: disable=line-too-long
+    print("--INFO-- Checking `descriptors.the.txt` integrity.")
+
+    #
+    # Loads the terms to check
+    thesaurus_file = os.path.join(root_dir, THESAURUS_FILE)
+    thesaurus = load_system_thesaurus_as_dict_reversed(thesaurus_file)
+    terms = list(thesaurus.keys())
+
+    #
+    # Loads the descriptors
+    file = list(glob.glob(os.path.join(root_dir, "databases/_main.csv.zip")))
+    data = pd.read_csv(file, encoding="utf-8", compression="zip")
+    descriptors = data.descriptors.copy()
+    descriptors = (
+        descriptors.dropna()
+        .str.split("; ")
+        .explode()
+        .str.strip()
+        .drop_duplicates()
+        .tolist()
+    )
+
+    #
+    # Computes the set union between terms and descriptors
+    union = set(terms).union(set(descriptors))
+
+    # union must be equal to terms and equal to descriptors
+    assert len(union) == len(terms)
+    assert len(union) == len(descriptors)
