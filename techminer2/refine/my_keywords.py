bcc5783235313d3e4537cfd831249e238fc8d776
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-07-12 10:03:10 -0500

    Update my_keywords.py

--- techminer2/refine/my_keywords.py
+++ techminer2/refine/my_keywords.py
@@ -22,40 +22,40 @@ MyKeywords
 import glob
 import os
 
-from .._parameters import RootDirMixin
+# from .._parameters import RootDirMixin
 
 
-class MyKeywords(RootDirMixin):
-    """:meta private:"""
+# class MyKeywords(RootDirMixin):
+#     """:meta private:"""
 
-    def __init__(
-        self,
-        #
-        # DATABASE PARAMS:
-        root_dir="./",
-    ):
-        # initialize mixins
-        RootDirMixin.__init__(
-            self,
-            #
-            # DATABASE PARAMS:
-            root_dir=root_dir,
-        )
+#     def __init__(
+#         self,
+#         #
+#         # DATABASE PARAMS:
+#         root_dir="./",
+#     ):
+#         # initialize mixins
+#         RootDirMixin.__init__(
+#             self,
+#             #
+#             # DATABASE PARAMS:
+#             root_dir=root_dir,
+#         )
 
-    def sort_my_keywords(self):
-        """:meta private:"""
+#     def sort_my_keywords(self):
+#         """:meta private:"""
 
-        dir_path = os.path.join(self.root_dir, "my_keywords")
-        if not os.path.isdir(dir_path):
-            raise FileNotFoundError(f"The directory '{dir_path}' does not exist.")
+#         dir_path = os.path.join(self.root_dir, "my_keywords")
+#         if not os.path.isdir(dir_path):
+#             raise FileNotFoundError(f"The directory '{dir_path}' does not exist.")
 
-        for file in glob.glob(os.path.join(dir_path, "*.txt")):
-            #
-            with open(file, "r", encoding="utf-8") as in_file:
-                keywords = [line.strip() for line in in_file.readlines()]
-            keywords = sorted(set(keywords))
-            #
-            with open(file, "w", encoding="utf-8") as out_file:
-                out_file.write("\n".join(keywords))
+#         for file in glob.glob(os.path.join(dir_path, "*.txt")):
+#             #
+#             with open(file, "r", encoding="utf-8") as in_file:
+#                 keywords = [line.strip() for line in in_file.readlines()]
+#             keywords = sorted(set(keywords))
+#             #
+#             with open(file, "w", encoding="utf-8") as out_file:
+#                 out_file.write("\n".join(keywords))
 
-            print(f"--INFO-- The file {file} has been sorted.")
+#             print(f"--INFO-- The file {file} has been sorted.")
