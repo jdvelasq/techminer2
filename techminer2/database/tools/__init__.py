90a217667179a88af14ea229912b86f77f73ebd1
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-01-28 12:30:24 -0500

    Update __init__.py

--- techminer2/database/tools/__init__.py
+++ techminer2/database/tools/__init__.py
@@ -1,11 +1,11 @@
 """Database management functions"""
 
+from .coverage import Coverage
+from .query import Query
 from .record_mapping import RecordMapping
 from .record_viewer import RecordViewer
-from .tools__coverage import Coverage
-from .tools__query import Query
-from .tools__statistics import Statistics
-from .tools__summary_sheet import SummarySheet
+from .statistics import Statistics
+from .summary_sheet import SummarySheet
 
 __all__ = [
     "Coverage",
