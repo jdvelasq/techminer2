74b48877b5ddfa41b423f3418dbbd1014e2f6fbf
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-02-13 15:00:35 -0500

    partial

--- techminer2/database/TO_REVIEW/cluster_records.py
+++ techminer2/database/TO_REVIEW/cluster_records.py
@@ -41,7 +41,7 @@ import plotly.graph_objects as go  # type: ignore
 from sklearn.manifold import MDS  # type: ignore
 from sklearn.neighbors import KernelDensity  # type: ignore
 
-from .metrics.tfidf.data_frame import DataFrame as tfidf_frame
+from ..metrics.tfidf.data_frame import DataFrame as tfidf_frame
 
 
 def cluster_records(
