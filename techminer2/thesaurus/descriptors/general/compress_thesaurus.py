8a78bc7d52ec9194170cca5bdb09b8b73333c0bf
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-15 12:30:13 -0500

    Create compress_thesaurus.py

---
+++ techminer2/thesaurus/descriptors/general/compress_thesaurus.py
@@ -0,0 +1,63 @@
+# flake8: noqa
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-branches
+"""
+Compress Thesaurus
+===============================================================================
+
+
+Example:
+    >>> import sys
+    >>> from io import StringIO
+    >>> from techminer2.thesaurus.descriptors import CompressThesaurus
+
+    >>> # Redirect stderr to capture output
+    >>> original_stderr = sys.stderr
+    >>> sys.stderr = StringIO()
+
+    >>> # Create the thesaurus
+    >>> compressor = (
+    ...     CompressThesaurus()
+    ...     .where_root_directory_is("example/")
+    ... )
+    >>> compressor.run()
+
+    >>> # Capture and print stderr output
+    >>> output = sys.stderr.getvalue()
+    >>> sys.stderr = original_stderr
+    >>> print(output)
+
+
+
+    <BLANKLINE>
+    <BLANKLINE>
+
+
+"""
+
+from ...._internals.mixins import ParamsMixin
+from ...user import CompressThesaurus as UserCompressThesaurus
+
+
+#
+#
+class CompressThesaurus(
+    ParamsMixin,
+):
+    """:meta private:"""
+
+    def run(self):
+
+        (
+            UserCompressThesaurus(
+                field="raw_descriptors",
+                thesaurus_file="descriptors.the.txt",
+                root_directory=self.params.root_directory,
+                quiet=self.params.quiet,
+            ).run()
+        )
