dd4fa28b5267aad10d92e0e919a3d0ea64ecf71a
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-29 06:52:13 -0500

    Update __init__.py

--- techminer2/thesaurus/user/__init__.py
+++ techminer2/thesaurus/user/__init__.py
@@ -16,19 +16,21 @@ from .sort.sort_by_fuzzy_key_match import SortByFuzzyKeyMatch
 from .sort.sort_by_key_match import SortByKeyMatch
 from .sort.sort_by_key_order import SortByKeyOrder
 from .sort.sort_by_match import SortByMatch
+from .sort.sort_by_occurrences import SortByOccurrences
 from .sort.sort_by_starts_with_key_match import SortByStartsWithKeyMatch
 from .sort.sort_by_word_key_match import SortByWordKeyMatch
 from .translate.american_to_british_spelling import AmericanToBritishSpelling
 from .translate.british_to_american_spelling import BritishToAmericanSpelling
 
 __all__ = [
+    "AmericanToBritishSpelling",
     "ApplyThesaurus",
+    "BritishToAmericanSpelling",
     "CompressThesaurus",
     "CreateThesaurus",
     "ExplodeKeys",
     "IntegrityCheck",
     "ReduceKeys",
-    "SpellCheck",
     "ReplaceEndsWithWord",
     "ReplaceStartsWithWord",
     "ReplaceWord",
@@ -38,8 +40,8 @@ __all__ = [
     "SortByKeyMatch",
     "SortByKeyOrder",
     "SortByMatch",
+    "SortByOccurrences",
     "SortByStartsWithKeyMatch",
     "SortByWordKeyMatch",
-    "AmericanToBritishSpelling",
-    "BritishToAmericanSpelling",
+    "SpellCheck",
 ]
