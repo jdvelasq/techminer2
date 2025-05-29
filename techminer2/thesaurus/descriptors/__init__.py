a54d1ff54380d37799cb23e9c4b8a57136f1da6d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-29 06:52:01 -0500

    Update __init__.py

--- techminer2/thesaurus/descriptors/__init__.py
+++ techminer2/thesaurus/descriptors/__init__.py
@@ -3,9 +3,9 @@
 from .general.apply_thesaurus import ApplyThesaurus
 from .general.cleanup_thesaurus import CleanupThesaurus
 from .general.compress_thesaurus import CompressThesaurus
-from .general.create_thesaurus import CreateThesaurus
+from .general.create_thesaurus import CreateThesaurus, reset_thesaurus
 from .general.integrity_check import IntegrityCheck
-from .general.reduce_keys import ReduceKeys
+from .general.reduce_keys import ReduceKeys, reduce_keys
 from .remove.remove_common_initial_words import RemoveCommonInitialWords
 from .remove.remove_common_last_words import RemoveCommonLastWords
 from .remove.remove_initial_determiners import RemoveInitialDeterminers
@@ -15,14 +15,15 @@ from .replace.replace_abbreviations import ReplaceAbbreviations
 from .replace.replace_ends_with_word import ReplaceEndsWithWord
 from .replace.replace_hyphenated_words import ReplaceHyphenatedWords
 from .replace.replace_starts_with_word import ReplaceStartsWithWord
-from .replace.replace_word import ReplaceWord
+from .replace.replace_word import ReplaceWord, replace
 from .sort.find_editorials import FindEditorials
 from .sort.sort_by_ends_with_key_match import SortByEndsWithKeyMatch
-from .sort.sort_by_exact_key_match import SortByExactKeyMatch
+from .sort.sort_by_exact_key_match import SortByExactKeyMatch, exactmatch
 from .sort.sort_by_fuzzy_key_match import SortByFuzzyKeyMatch
 from .sort.sort_by_key_match import SortByKeyMatch
 from .sort.sort_by_key_order import SortByKeyOrder
-from .sort.sort_by_starts_with_key_match import SortByStartsWithKeyMatch
+from .sort.sort_by_occurrences import SortByOccurrences, sort_by_occurrences
+from .sort.sort_by_starts_with_key_match import SortByStartsWithKeyMatch, startswith
 from .sort.sort_by_word_key_match import SortByWordKeyMatch
 from .translate.american_to_british_spelling import AmericanToBritishSpelling
 from .translate.british_to_american_spelling import BritishToAmericanSpelling
@@ -49,9 +50,16 @@ __all__ = [
     "SortByFuzzyKeyMatch",
     "SortByKeyMatch",
     "SortByKeyOrder",
+    "SortByOccurrences",
     "SortByStartsWithKeyMatch",
     "SortByWordKeyMatch",
     "AmericanToBritishSpelling",
     "BritishToAmericanSpelling",
     "SortByExactKeyMatch",
+    "exactmatch",
+    "reduce_keys",
+    "replace",
+    "reset_thesaurus",
+    "sort_by_occurrences",
+    "startswith",
 ]
