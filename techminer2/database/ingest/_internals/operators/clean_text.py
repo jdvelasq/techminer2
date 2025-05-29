4b76663864068c260b59b68ba69d4b3028cafe45
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-05-29 06:52:50 -0500

    Update clean_text.py

--- techminer2/database/ingest/_internals/operators/clean_text.py
+++ techminer2/database/ingest/_internals/operators/clean_text.py
@@ -11,6 +11,7 @@ import pathlib
 
 import contractions  # type: ignore
 import pandas as pd  # type: ignore
+from nltk.tokenize import word_tokenize
 
 
 def internal__clean_text(
@@ -44,6 +45,148 @@ def internal__clean_text(
 
 
 def clean_text(text):
+    """:meta private:"""
+
+    # remove [...]
+    text = text.map(
+        lambda w: pd.NA if w[0] == "[" and w[-1] == "]" else w, na_action="ignore"
+    )
+
+    text = text.str.lower()
+
+    # remove contractions
+    text = text.map(contractions.fix, na_action="ignore")
+
+    # process puntuation
+    text = text.str.replace("’", "'", regex=False)
+    text = text.str.replace(";", ".", regex=False)
+    text = text.str.replace("`", "'", regex=False)
+
+    # remove all html tags
+    text = text.str.replace("<.*?>", "", regex=True)
+
+    # process punctuation
+    text = text.str.replace(r"&", r" and ", regex=False)  # and
+    text = text.str.replace(r"e-mail", r"email", regex=False)  # email
+    text = text.str.replace("—", "-", regex=False)
+    text = text.str.replace("-", " ", regex=False)
+
+    # tokenize text
+    text = text.map(
+        lambda w: " ".join(word_tokenize(w)) if isinstance(w, str) else w,
+        na_action="ignore",
+    )
+
+    # urls and emails
+    text = text.str.replace("http : //", "http://", regex=False)
+    text = text.str.replace("https : //", "http://", regex=False)
+    text = text.str.replace("mail : ", "mail:", regex=False)
+    text = text.str.replace(" @ ", "@", regex=False)
+
+    text = text.str.replace(
+        r"\s([a-zA-Z])\.\s",
+        r" \1 . ",
+        regex=True,
+    )
+
+    text = text.str.replace(
+        r"\s([a-zA-Z])\.([a-zA-Z])\s",
+        r" \1 . \2 ",
+        regex=True,
+    )
+    text = text.str.replace(
+        r"\s([a-zA-Z])\.([a-zA-Z])\.([a-zA-Z])\s",
+        r" \1 . \2 . \3 ",
+        regex=True,
+    )
+    text = text.str.replace(
+        r"\s([a-zA-Z])\.([a-zA-Z])\.([a-zA-Z])\.([a-zA-Z])\s",
+        r" \1 . \2 . \3 . \4 ",
+        regex=True,
+    )
+
+    text = text.str.replace(
+        r"\s([a-zA-Z][a-zA-Z])\.\s",
+        r" \1 . ",
+        regex=True,
+    )
+
+    text = text.str.replace(
+        r"\s([a-zA-Z]+)\.\s\)",
+        r" \1 . )",
+        regex=True,
+    )
+
+    text = text.str.replace(
+        r"\s\.\.\s",
+        r" . . ",
+        regex=True,
+    )
+
+    text = text.str.replace(
+        r"\s=(\d+)",
+        r" = \1",
+        regex=True,
+    )
+
+    # url_pattern = r"(http[s]?://[^\s]+"
+
+    # Replace URLs with their lower-case version
+    # text = text.str.replace(
+    #     url_pattern,
+    #     lambda m: m.group(0).lower(),
+    #     regex=True,
+    # )
+
+    # Replace email directions with their lower-case version
+    # text = text.str.replace(" email : ", " *email*:", regex=True)
+    # text = text.str.replace(" e_mail : ", " +email+:", regex=True)
+    # text = text.str.replace(" e-mail : ", " #email#:", regex=True)
+
+    # email_pattern = r"[a-zA-Z0-9._%+-]+@[_a-zA-Z0-9.-]+\.[_a-zA-Z]{2,}"
+    # text = text.str.replace(
+    #     email_pattern,
+    #     lambda m: m.group(0).lower(),
+    #     regex=True,
+    # )
+
+    text = text.str.replace(" (\d{4})\. ", r" \1 . ", regex=True)  # 2020.
+    text = text.str.replace(" et al. ", r" et al . ", regex=False)  # et al.
+    text = text.str.replace(" ltd. ", r" ltd . ", regex=False)
+    text = text.str.replace(" inc. ", r" inc . ", regex=False)
+    text = text.str.replace("_'S_", r"_", regex=False)
+    text = text.str.replace(" 's ", r" ", regex=False)
+    text = text.str.replace(" '' ", r" ' ' ", regex=False)
+    text = text.str.replace(" `` ", " ' ' ", regex=False)
+    text = text.str.replace(" .. ", " . . ", regex=False)
+    text = text.str.replace(" \.\.$", " . .", regex=True)
+
+    text = text.str.replace(
+        " trial registration : ", " trial_registration:", regex=True
+    )
+
+    # remove all non-ascii characters
+    text = text.str.normalize("NFKD")
+    text = text.str.encode("ascii", errors="ignore")
+    text = text.str.decode("utf-8")
+
+    # rempve multiple spaces
+    text = text.str.replace(r"\s+", r" ", regex=True)
+    text = text.str.strip()
+
+    return text
+
+
+# https : huggingface \. co
+# https : github \. com
+# https : doi \. org
+# https : creativecommons \. org
+# http : publichealth \. jmir\.org
+# http : doi \. org
+# http : creativecommons \. org
+
+
+def clean_text_ORIGINAL(text):
     """:meta private:"""
 
     # remove [...]
