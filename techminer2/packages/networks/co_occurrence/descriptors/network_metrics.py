7cd9fee3f1a1b9c53e9b167b717e69c1249cfda4
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-08 14:36:52 -0500

    Update network_metrics.py

--- techminer2/packages/networks/co_occurrence/descriptors/network_metrics.py
+++ techminer2/packages/networks/co_occurrence/descriptors/network_metrics.py
@@ -37,24 +37,23 @@ Example:
     ...     #
     ...     .run()
     ... ).head(15)
-                                             Degree  ...  PageRank
-    FINTECH 44:6942                              19  ...  0.137168
-    FINANCE 21:3481                              19  ...  0.078102
-    TECHNOLOGIES 15:1810                         19  ...  0.071642
-    FINANCIAL_SERVICE 12:2100                    19  ...  0.065198
-    INNOVATION 15:2741                           18  ...  0.065477
-    FINANCIAL_TECHNOLOGIES 14:2005               18  ...  0.066703
-    REGULATORS 08:0974                           18  ...  0.050151
-    BANKS 09:1133                                17  ...  0.048698
-    DATA 07:1086                                 17  ...  0.035346
-    BUSINESS_MODEL 05:1578                       17  ...  0.034291
-    THE_DEVELOPMENT 08:1173                      16  ...  0.042315
-    SERVICES 07:1226                             16  ...  0.042786
-    BANKING 07:0851                              16  ...  0.042321
-    INVESTMENT 06:1294                           16  ...  0.037517
-    THE_FINANCIAL_SERVICES_INDUSTRY 06:1237      16  ...  0.042754
-    <BLANKLINE>
-    [15 rows x 4 columns]
+                                    Degree  Betweenness  Closeness  PageRank
+    FINTECH 44:6942                     19     0.011241   1.000000  0.133687
+    FINANCE 21:3481                     19     0.011241   1.000000  0.075377
+    TECHNOLOGIES 17:1943                19     0.011241   1.000000  0.073365
+    FINANCIAL_SERVICE 12:2100           19     0.011241   1.000000  0.063524
+    REGULATORS 08:0974                  19     0.011241   1.000000  0.050305
+    INNOVATION 15:2741                  18     0.009759   0.950000  0.063445
+    FINANCIAL_TECHNOLOGIES 14:2005      18     0.007445   0.950000  0.065707
+    BANKS 09:1133                       18     0.006604   0.950000  0.047331
+    PRACTITIONER 06:1194                18     0.006604   0.950000  0.034435
+    THE_DEVELOPMENT 09:1293             17     0.008148   0.904762  0.046894
+    DATA 07:1086                        17     0.009528   0.904762  0.033658
+    BANKING 07:0851                     17     0.006714   0.904762  0.041254
+    BUSINESS_MODEL 05:1578              17     0.004242   0.904762  0.034400
+    SERVICES 07:1226                    16     0.001096   0.863636  0.040660
+    CONSUMERS 07:0925                   16     0.003093   0.863636  0.033560
+
 
 
 Example:
@@ -84,22 +83,23 @@ Example:
     ...     #
     ...     .run()
     ... ).head(15)
-                                     Degree  Betweenness  Closeness  PageRank
-    FINTECH                              19     0.016624   1.000000  0.137168
-    FINANCE                              19     0.016624   1.000000  0.078102
-    TECHNOLOGIES                         19     0.016624   1.000000  0.071642
-    FINANCIAL_SERVICE                    19     0.016624   1.000000  0.065198
-    INNOVATION                           18     0.014480   0.950000  0.065477
-    FINANCIAL_TECHNOLOGIES               18     0.011683   0.950000  0.066703
-    REGULATORS                           18     0.011300   0.950000  0.050151
-    BANKS                                17     0.007195   0.904762  0.048698
-    DATA                                 17     0.013603   0.904762  0.035346
-    BUSINESS_MODEL                       17     0.008278   0.904762  0.034291
-    THE_DEVELOPMENT                      16     0.007951   0.863636  0.042315
-    SERVICES                             16     0.003987   0.863636  0.042786
-    BANKING                              16     0.006415   0.863636  0.042321
-    INVESTMENT                           16     0.003987   0.863636  0.037517
-    THE_FINANCIAL_SERVICES_INDUSTRY      16     0.003987   0.863636  0.042754
+                            Degree  Betweenness  Closeness  PageRank
+    FINTECH                     19     0.011241   1.000000  0.133687
+    FINANCE                     19     0.011241   1.000000  0.075377
+    TECHNOLOGIES                19     0.011241   1.000000  0.073365
+    FINANCIAL_SERVICE           19     0.011241   1.000000  0.063524
+    REGULATORS                  19     0.011241   1.000000  0.050305
+    INNOVATION                  18     0.009759   0.950000  0.063445
+    FINANCIAL_TECHNOLOGIES      18     0.007445   0.950000  0.065707
+    BANKS                       18     0.006604   0.950000  0.047331
+    PRACTITIONER                18     0.006604   0.950000  0.034435
+    THE_DEVELOPMENT             17     0.008148   0.904762  0.046894
+    DATA                        17     0.009528   0.904762  0.033658
+    BANKING                     17     0.006714   0.904762  0.041254
+    BUSINESS_MODEL              17     0.004242   0.904762  0.034400
+    SERVICES                    16     0.001096   0.863636  0.040660
+    CONSUMERS                   16     0.003093   0.863636  0.033560
+
 
 
 
