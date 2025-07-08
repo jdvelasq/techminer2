0142500ad16e6c8251d988cc5f8a2bb3bb0c9de3
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-08 14:52:54 -0500

    Update integrity_check.py

--- techminer2/thesaurus/user/general/integrity_check.py
+++ techminer2/thesaurus/user/general/integrity_check.py
@@ -41,7 +41,7 @@ Example:
     >>> print(output)
     Thesaurus integrity check
       File : example/data/thesaurus/demo.the.txt
-      1793 terms checked
+      1792 terms checked
       Integrity check completed successfully
     <BLANKLINE>
     <BLANKLINE>
