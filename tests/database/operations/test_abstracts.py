4d737755b440f3cd35e5d9628cd1b9f72211afa5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-08 14:36:34 -0500

    Update test_abstracts.py

--- tests/database/operations/test_abstracts.py
+++ tests/database/operations/test_abstracts.py
@@ -48,15 +48,15 @@ Example:
     >>> print(textwrap.fill(df.values[35][0], width=80))
     background : this study aims to_clarify THE_ROLE of
     FINTECH_DIGITAL_BANKING_START_UPS in THE_FINANCIAL_INDUSTRY . we examine
-    THE_IMPACT of THE_FUNDING of SUCH_START_UPS on THE_STOCK_RETURNS of 47 INCUMBENT
-    us RETAIL_BANKS for 2010 to 2016 . METHODS : to capture THE_IMPORTANCE of
+    THE_IMPACT of THE_FUNDING of SUCH_START_UPS on THE_STOCK_RETURNS of 47 incumbent
+    us RETAIL_BANKS for 2010 to 2016 . methods : to capture THE_IMPORTANCE of
     FINTECH_START_UPS , we use DATA on BOTH_THE_DOLLAR_VOLUME of FUNDING and NUMBER
     of DEALS . we relate these to THE_STOCK_RETURNS with
     PANEL_DATA_REGRESSION_METHODS . results : OUR_RESULTS indicate a
     POSITIVE_RELATIONSHIP_EXISTS between THE_GROWTH in FINTECH_FUNDING or DEALS and
     THE_CONTEMPORANEOUS_STOCK_RETURNS of INCUMBENT_RETAIL_BANKS . conclusions :
     although THESE_RESULTS suggest COMPLEMENTARITY between FINTECH and
-    TRADITIONAL_BANKING , we NOTE that OUR_RESULTS at THE_BANKING_INDUSTRY_LEVEL are
+    TRADITIONAL_BANKING , we note that OUR_RESULTS at THE_BANKING_INDUSTRY_LEVEL are
     not statistically significant , and THAT_THE_COEFFICIENT_SIGNS for about one
     third of THE_BANKS are negative , but not statistically significant . since
     THE_FINTECH_INDUSTRY is young and our SAMPLE_PERIOD short , we can not rule out
