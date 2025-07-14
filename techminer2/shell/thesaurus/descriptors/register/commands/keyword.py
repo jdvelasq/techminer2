d6fe2cf9fa37e9c458d35c4a7c99b14488c0d714
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-13 23:35:25 -0500

    Update keyword.py

--- techminer2/shell/thesaurus/descriptors/register/commands/keyword.py
+++ techminer2/shell/thesaurus/descriptors/register/commands/keyword.py
@@ -5,11 +5,16 @@ from .....colorized_input import colorized_input
 def execute_keyword_command():
 
     print()
+    words = []
+    while True:
+        word = colorized_input(". new keyword > ").strip()
+        if word.strip() == "":
+            break
+        words.append(word.upper().strip())
 
-    word = colorized_input(". new keyword > ").strip()
-    if word == "":
+    if not words:
         print()
         return
 
     print()
-    RegisterKeyword().having_word(word).run()
+    RegisterKeyword().having_word(words).run()
