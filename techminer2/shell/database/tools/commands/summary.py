8a37663b3b77a905ec4f60b50e82432790e10f7b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-18 10:38:16 -0500

    Update summary.py

--- techminer2/shell/database/tools/commands/summary.py
+++ techminer2/shell/database/tools/commands/summary.py
@@ -50,7 +50,7 @@ def execute_summary_command():
         SummarySheet()
         #
         .where_root_directory_is("./")
-        .where_database_is(None)
+        .where_database_is("main")
         .where_record_years_range_is(None, None)
         .where_record_citations_range_is(None, None)
         #
