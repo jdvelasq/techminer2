f34f8e912b9095fd0bd20977ac89ca8db5dc3c06
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-19 12:11:12 -0500

    Create zotero_shell.py

---
+++ techminer2/shell/zotero/zotero_shell.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-branches
+"""Command line interface for the Thesaurus subsystem."""
+
+
+from ..base_shell import BaseShell
+from ..colorized_prompt import make_colorized_prompt
+from .commands import execute_update_command
+
+
+class ZoteroShell(BaseShell):
+
+    prompt = make_colorized_prompt("tm2:zotero")
+
+    def do_update(self, arg):
+        """Update Zotero database."""
+        execute_update_command()
