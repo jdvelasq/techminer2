d242e05704ac6145ec4f5f0b5ff9a2b06d689fb5
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-19 12:11:15 -0500

    Create update.py

---
+++ techminer2/shell/zotero/commands/update.py
@@ -0,0 +1,26 @@
+from ....zotero.export_record_no_to_zotero import ExportRecordNoToZotero
+from ...colorized_input import colorized_input
+
+
+def execute_update_command():
+
+    print()
+
+    zotero_api_key = colorized_input(". Zotero api key [*] > ").strip()
+    if zotero_api_key == "":
+        zotero_api_key = "MifzqqRsPK5OHvmNoW4Y9Zre"
+
+    zotero_library_id = colorized_input(". Zotero library ID > ").strip()
+    if zotero_library_id == "":
+        print()
+        return
+
+    print()
+    (
+        ExportRecordNoToZotero()
+        .using_zotero_api_key(zotero_api_key)
+        .using_zotero_library_id(zotero_library_id)
+        .using_zotero_library_type("group")
+        .where_root_directory_is("./")
+        .run()
+    )
