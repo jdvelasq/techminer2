31f7d734ee4b270f2f17b49ccaf7a4ceb8377691
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-23 19:07:21 -0500

    Add RegisterPhrases to abbreviations module
    Imported RegisterPhrases and updated __all__ to include it in the abbreviations package, making it available for external use.

--- techminer2/thesaurus/abbreviations/__init__.py
+++ techminer2/thesaurus/abbreviations/__init__.py
@@ -1,3 +1,9 @@
 """Abbreviations"""
 
 from .general.initialize_thesaurus import InitializeThesaurus
+from .register.register_phrases import RegisterPhrases
+
+__all__ = [
+    "InitializeThesaurus",
+    "RegisterPhrases",
+]
