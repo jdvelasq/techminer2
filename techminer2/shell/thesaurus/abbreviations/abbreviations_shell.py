961ab79289a1580cbe88433473cb09193e6bf872
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-23 19:07:44 -0500

    Add register command to AbbreviationsShell
    Introduces the do_register method to AbbreviationsShell, allowing users to register new noun phrases via RegisterShell. Updates imports to include RegisterShell.

--- techminer2/shell/thesaurus/abbreviations/abbreviations_shell.py
+++ techminer2/shell/thesaurus/abbreviations/abbreviations_shell.py
@@ -11,6 +11,7 @@
 from ...base_shell import BaseShell
 from ...colorized_prompt import make_colorized_prompt
 from .general.general_shell import GeneralShell
+from .register.register_shell import RegisterShell
 
 
 class AbbreviationsShell(BaseShell):
@@ -20,4 +21,9 @@ class AbbreviationsShell(BaseShell):
     def do_general(self, arg):
         """General commands."""
         GeneralShell().cmdloop()
+        self.do_help(arg)
+
+    def do_register(self, arg):
+        """Register new noun phrases in the system."""
+        RegisterShell().cmdloop()
         self.do_help(arg)
