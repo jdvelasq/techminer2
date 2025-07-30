ae8466308947469dbc5ed9c70f4288c837e7a06b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-30 12:44:25 -0500

    Update __init__.py

--- techminer2/shell/zotero/commands/__init__.py
+++ techminer2/shell/zotero/commands/__init__.py
@@ -1,7 +1,9 @@
 """Commands."""
 
+from .search import execute_search_command
 from .update import execute_update_command
 
 __all__ = [
     "execute_update_command",
+    "execute_search_command",
 ]
