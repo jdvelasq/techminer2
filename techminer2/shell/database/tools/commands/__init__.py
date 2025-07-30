0ad0c3228031d20553b26de2ed1776bb0921b1d2
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-30 12:44:51 -0500

    Update __init__.py

--- techminer2/shell/database/tools/commands/__init__.py
+++ techminer2/shell/database/tools/commands/__init__.py
@@ -1,6 +1,7 @@
 from .copyright import execute_copyright_command
 from .doctypes import execute_doctypes_command
 from .summary import execute_summary_command
+from .titles import execute_titles_command
 from .view import execute_view_command
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "execute_summary_command",
     "execute_view_command",
     "execute_doctypes_command",
+    "execute_titles_command",
 ]
