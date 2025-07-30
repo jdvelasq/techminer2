c623b9b713d70186b9ae053f8eb598edcff44637
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-07-30 12:45:00 -0500

    Create titles.py

---
+++ techminer2/shell/database/tools/commands/titles.py
@@ -0,0 +1,12 @@
+from colorama import Fore, init
+
+from techminer2.database.tools import SearchString  # type: ignore
+
+
+def execute_titles_command():
+
+    SearchString().where_root_directory_is("./").run()
+
+    print()
+    print("Scopus search string saved to: outputs/scopus_search_string.txt")
+    print()
