272c8801e6c6adac9d9ad86b20244fa401d71d5b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-08-17 23:17:22 -0500

    Update __init__.py

--- techminer2/database/_internals/db/__init__.py
+++ techminer2/database/_internals/db/__init__.py
@@ -6,6 +6,7 @@ from .drop_empty_columns import internal__drop_empty_columns
 from .load_raw_files import internal__load_raw_files
 from .remove_non_english_abstracts import internal__remove_non_english_abstracts
 from .rename_columns import internal__rename_columns
+from .check_hyphenated_form import internal__check_hyphenated_form
 
 __all__ = [
     "internal__compress_raw_files",
@@ -14,4 +15,5 @@ __all__ = [
     "internal__load_raw_files",
     "internal__rename_columns",
     "internal__remove_non_english_abstracts",
+    "internal__check_hyphenated_form",
 ]
