82d7f3570bde7fce8a415a348017e7ed7442d0bb
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-08-19 08:39:15 -0500

    Update merge.py

--- techminer2/database/_internals/operators/merge.py
+++ techminer2/database/_internals/operators/merge.py
@@ -21,34 +21,52 @@ def internal__merge(
 
     database_file = pathlib.Path(root_dir) / "data/processed/database.csv.zip"
 
-    dataframe = pd.read_csv(
+    data_frame = pd.read_csv(
         database_file,
         encoding="utf-8",
         compression="zip",
         low_memory=False,
     )
 
-    new_field = None
+    #
+    data_frame[dest] = [[] for _ in range(len(data_frame))]
+    for index, row in data_frame.iterrows():
+        for field in source:
+            if field in data_frame.columns:
+                if not pd.isna(row[field]):
+                    data_frame.at[index, dest].extend(
+                        [x.strip() for x in row[field].split("; ") if x.strip() != ""]
+                    )
 
-    for field in source:
-        if field in dataframe.columns:
-            if new_field is None:
-                new_field = dataframe[field].astype(str).str.split("; ")
-            else:
-                new_field = new_field + dataframe[field].astype(str).str.split("; ")
+    data_frame[dest] = data_frame[dest].map(lambda x: [z for z in x if z != "nan"])
+    data_frame[dest] = data_frame[dest].map(
+        lambda x: sorted(set(x)), na_action="ignore"
+    )
+    data_frame[dest] = data_frame[dest].map("; ".join, na_action="ignore")
+    data_frame[dest] = data_frame[dest].map(lambda x: x if x != "" else pd.NA)
+
+    #
+
+    ## new_field = None
+    ## for field in source:
+    ##     if field in data_frame.columns:
+    ##         if new_field is None:
+    ##             new_field = data_frame[field].astype(str).str.split("; ")
+    ##         else:
+    ##             new_field = new_field + data_frame[field].astype(str).str.split("; ")
 
     #
     # Remove duplicates and sort
-    new_field = new_field.map(lambda x: [z for z in x if z != "nan"])
-    new_field = new_field.map(lambda x: sorted(set(x)), na_action="ignore")
-    new_field = new_field.map("; ".join, na_action="ignore")
-    new_field = new_field.map(lambda x: x if x != "" else pd.NA)
+    ## new_field = new_field.map(lambda x: [z for z in x if z != "nan"])
+    ## new_field = new_field.map(lambda x: sorted(set(x)), na_action="ignore")
+    ## new_field = new_field.map("; ".join, na_action="ignore")
+    ## new_field = new_field.map(lambda x: x if x != "" else pd.NA)
 
     #
     # Create the new field
-    dataframe[dest] = new_field
+    ## dataframe[dest] = new_field
 
-    dataframe.to_csv(
+    data_frame.to_csv(
         database_file,
         sep=",",
         encoding="utf-8",
