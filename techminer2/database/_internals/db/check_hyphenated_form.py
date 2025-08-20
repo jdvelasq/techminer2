d79c916ee3169fb5e2c62178fdee4e3eab5d6dbe
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-08-20 10:53:21 -0500

    Update check_hyphenated_form.py

--- techminer2/database/_internals/db/check_hyphenated_form.py
+++ techminer2/database/_internals/db/check_hyphenated_form.py
@@ -6,10 +6,36 @@
 # pylint: disable=too-many-locals
 import os.path
 import pandas as pd
-
+from tqdm import tqdm
+from openai import OpenAI
+import os
 from techminer2._internals import Params
 from techminer2.database._internals.io import internal__load_all_records_from_database
 from techminer2.package_data.text_processing import internal__load_text_processing_terms
+import sys
+
+PROMPT = """
+INSTRUCTION:
+You will be provided with two variations of the same word: one in hyphenated form and the other in non-hyphenated form.
+
+
+TASK:
+1. Analyze the two forms and determine which is correct in scientific or technical English usage.
+2. Respond with "yes" if:
+    - The hyphenated form is the only form correct.
+    - The word is not standard English.
+    - Both forms (hyphenated and non-hyphenated) are correct.
+    - Neither form is correct.
+3. Respond "no" if:
+    - The non-hyphenated form is the only form correct.
+    - The correct form is different from both provided forms.
+
+
+WORDS:
+- Hyphenated: "{}"
+- Non-hyphenated: "{}"
+
+"""
 
 
 def collect_project_hyphenated_words(root_dir):
@@ -26,15 +52,52 @@ def collect_project_hyphenated_words(root_dir):
         if column in dataframe.columns:
             words.append(dataframe[column].dropna().copy())
 
+    # Obtain a keyword per row
     words = pd.concat(words)
     words = words.str.upper()
     words = words.str.split("; ")
     words = words.explode()
+
+    # Minimal preprocessing:
+
+    # remove all html tags
+    words = words.str.replace("<.*?>", "", regex=True)
+
+    # - Space, left parenthesis, letter
+    words = words.str.replace(" \(([A-Z0-9])", r" ( \1", regex=True)
+    words = words.str.replace(" \[([A-Z0-9])", r" [ \1", regex=True)
+    words = words.str.replace(" {([A-Z0-9])", r" { \1", regex=True)
+
+    # - Letter, right parenthesis at end of line
+    words = words.str.replace("([A-Z0-9])\)$", r"\1 )", regex=True)
+    words = words.str.replace("([A-Z0-9])\]$", r"\1 ]", regex=True)
+    words = words.str.replace("([A-Z0-9])}$", r"\1 }", regex=True)
+
+    # - Parenthesis at the beginning of line, letter
+    words = words.str.replace("^\(([A-Z0-9])", r"( \1", regex=True)
+    words = words.str.replace("^\[([A-Z0-9])", r"[ \1", regex=True)
+    words = words.str.replace("^{([A-Z0-9])", r"{ \1", regex=True)
+
+    # - Letter, left parenthesis, letter
+    words = words.str.replace("([A-Z0-9])\(([A-Z0-9])", r"\1 ( \2", regex=True)
+    words = words.str.replace("([A-Z0-9])\[([A-Z0-9])", r"\1 [ \2", regex=True)
+    words = words.str.replace("([A-Z0-9]){([A-Z0-9])", r"\1 { \2", regex=True)
+
+    # Word splitting
     words = words.str.split(" ")
     words = words.explode()
     words = words.str.strip()
+
     words = words[words.str.contains("-")]
     words = words.str.replace("-", "_", regex=False)
+    words = words[words.apply(lambda x: x != "_")]
+    words = words[~words.str.startswith("_")]
+    words = words[~words.str.endswith("_")]
+    words = words[~words.str.endswith(")")]
+    words = words[~words.str.endswith("}")]
+    words = words[words.apply(lambda x: len(x) > 6)]
+    words = words[words.str.match("^[A-Za-z0-9_]+$")]
+
     words = words.to_list()
     words = list(set(words))
 
@@ -48,13 +111,63 @@ def collect_system_hyphenated_words():
 
 
 def internal__check_hyphenated_form(root_dir):
+
+    sys.stderr.write("INFO  Checking hyphenated words\n")
+    sys.stderr.flush()
+
     project_words = collect_project_hyphenated_words(root_dir)
     system_words = collect_system_hyphenated_words()
     undetected_words = set(project_words) - set(system_words)
 
-    file_path = os.path.join(
-        root_dir, "data/my_keywords/undetected_hyphenated_words.txt"
-    )
+    if not os.getenv("OPENAI_API_KEY"):
+        raise ValueError("OPENAI_API_KEY environment variable is not set.")
+
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    hyphenated_is_incorrect = []
+    hyphenated_is_correct = []
+
+    for term in tqdm(
+        undetected_words,
+        total=len(undetected_words),
+        desc=f"  Progress",
+        ncols=80,
+    ):
+
+        if "_" not in term:
+            continue
+
+        words = term.split("_")
+        if words[0] == words[1]:
+            hyphenated_is_correct.append(term)
+            continue
+
+        query = PROMPT.format(
+            term.lower().replace("_", "-"), term.lower().replace("_", "")
+        )
+
+        try:
+            response = client.responses.create(
+                model="gpt-5-nano",
+                input=query,
+            )
+            answer = response.output_text
+            answer = answer.strip().lower()
+
+            if answer == "no":
+                hyphenated_is_incorrect.append(term)
+            else:
+                hyphenated_is_correct.append(term)
+
+        except Exception as e:
+            print(f"Error processing {e}")
+
+    file_path = os.path.join(root_dir, "data/my_keywords/hyphenated_is_incorrect.txt")
     with open(file_path, "w") as f:
-        for word in sorted(undetected_words):
+        for word in sorted(hyphenated_is_incorrect):
+            f.write(f"{word}\n")
+
+    file_path = os.path.join(root_dir, "data/my_keywords/hyphenated_is_correct.txt")
+    with open(file_path, "w") as f:
+        for word in sorted(hyphenated_is_correct):
             f.write(f"{word}\n")
