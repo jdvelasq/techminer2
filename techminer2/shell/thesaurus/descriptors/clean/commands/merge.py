7de23580cb3daacc232da487a66f04cd38150945
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-08-26 11:30:38 -0500

    Create merge.py

---
+++ techminer2/shell/thesaurus/descriptors/clean/commands/merge.py
@@ -0,0 +1,47 @@
+# flake8: noqa
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-statements
+# pylint: disable=too-many-branches
+# pylint: disable=unused-argument
+
+
+from techminer2.shell.colorized_input import colorized_input
+from techminer2.thesaurus.descriptors import MergeKeys
+
+
+# -----------------------------------------------------------------------------
+def internal__user_input():
+
+    # -------------------------------------------------------------------------
+    lead_term = colorized_input(". Enter the lead term > ").strip()
+    if lead_term == "":
+        return None
+
+    # -------------------------------------------------------------------------
+    candidate_term = colorized_input(". Enter the candidate term > ").strip()
+    if candidate_term == "":
+        return None
+
+    # -------------------------------------------------------------------------
+    return [lead_term, candidate_term]
+
+
+# -----------------------------------------------------------------------------
+def execute_merge_command():
+
+    print()
+
+    while True:
+
+        patterns = internal__user_input()
+
+        if patterns is None:
+            print()
+            return
+
+        MergeKeys().with_patterns(patterns).where_root_directory_is("./").run()
+        print()
