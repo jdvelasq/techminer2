b2fcd6d39934ea80115c832fcef7b009caa74dd6
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-08-26 11:31:01 -0500

    Update synonyms.py

--- techminer2/shell/thesaurus/descriptors/clean/commands/synonyms.py
+++ techminer2/shell/thesaurus/descriptors/clean/commands/synonyms.py
@@ -18,20 +18,29 @@ from techminer2.database.search import ConcordantSentences
 from techminer2.shell.colorized_input import colorized_input
 from techminer2.thesaurus.descriptors import GetValues, MergeKeys
 
-PROMPT = """
+# -----------------------------------------------------------------------------
+
+PROMPT_WITH_CONTEXTS = """
 ROLE:
-You are an expert in scientometrics and text mining, with experience in co-word and tech-mining studies.
+You are an expert in scientometrics and text mining, with experience in co-word 
+and tech-mining studies.
 
 
 CONTEXT:
-This task is part of a process to refine the thesaurus for co-word and tech-mining analysis in the core area <<{core_area}>>. 
-The goal is to determine whether two terms are conceptual synonyms—meaning they refer to the same concept or idea in the provided context phrases and core area.
+This task is part of a process to refine the thesaurus for co-word and 
+tech-mining analysis in the core area <<{core_area}>>. 
+The goal is to determine whether two terms are conceptual synonyms—meaning they 
+refer to the same concept or idea in the provided context phrases and core area.
 
 Important extraction assumption:
--   The corpus has been pre-processed to extract meaningful terms (noun phrases / keywords).
--   Multi-word terms are indexed separately from their headwords. Therefore, phrases for an isolated term
-    do not include occurrences that belong to its multi-word variants (e.g., phrases for "value" exclude
-    "market value", "net present value", etc.).
+-   The corpus has been pre-processed to extract meaningful terms 
+    (noun phrases / keywords).
+-   Both terms have co-word occurrences greater than zero, indicating they are 
+    present together in at least one document.
+-   Multi-word terms are indexed separately from their headwords. Therefore, 
+    phrases for an isolated term do not include occurrences that belong to its 
+    multi-word variants (e.g., phrases for "value" exclude "market value", 
+    "net present value", etc.).
 
 
 TASK:
@@ -87,24 +96,73 @@ CONTEXT PHRASES FOR THE CANDIDATE-TERM:
 
 """
 
-EXPLAIN = """
-ROLE:
-You are an expert in scientometrics and text mining, with experience in co-word and tech-mining studies.
+# -----------------------------------------------------------------------------
 
+PROMPT_WITHOUT_CONTEXTS = """
+ROLE:
+You are an expert in scientometrics and text mining, with experience in co-word 
+and tech-mining studies.
 
 CONTEXT:
-This task is part of a process to refine the thesaurus for co-word and tech-mining analysis in the core area <<{core_area}>>. 
-The provided terms are NOT conceptual synonyms—meaning they refer to different concepts or ideas in the provided context phrases and core area.
+This task is part of a process to refine the thesaurus for co-word and 
+tech-mining analysis in the core area <<{core_area}>>. The goal is to determine 
+whether two terms are conceptual synonyms—meaning they refer to the same concept 
+or idea in the core area <<{core_area}>>.
 
 Important extraction assumption:
 -   The corpus has been pre-processed to extract meaningful terms (noun phrases / keywords).
--   Multi-word terms are indexed separately from their headwords. Therefore, phrases for an isolated term
-    do not include occurrences that belong to its multi-word variants (e.g., phrases for "value" exclude
+-   Both terms have co-word occurrences greater than zero, indicating they are 
+    present together in at least one document.
+-   Multi-word terms are indexed separately from their headwords.
+
+TASK:
+Return exactly one lowercase word with no punctuation:
+-   "yes": when the LEAD-TERM and CANDIDATE-TERM are conceptual synonyms.
+-   "no": when the LEAD-TERM and CANDIDATE-TERM are not conceptual synonyms.
+
+DECISION RULES:
+- Analyze the LEAD-TERM and CANDIDATE-TERM using your scientific and domain knowledge of <<{core_area}>>.
+- Decide "yes" if both terms are very similar and likely refer to the same concept in practice, even if there are minor differences in wording, singular/plural form, or inclusion of generic words, as long as the context and domain indicate they are used interchangeably.
+- Decide "no" only if there is clear and consistent evidence that the terms refer to different concepts or ideas in <<{core_area}>>.
+- Do not guess or speculate, but allow for practical synonymy when domain usage supports it.
+- For the same terms, always return the same answer.
+
+OUTPUT:
+Return exactly one word with no quotes: yes or no.
+
+TERMS:
+LEAD-TERM: <<{lead_term}>>
+CANDIDATE-TERM: <<{candidate_term}>>
+"""
+
+# -----------------------------------------------------------------------------
+
+EXPLAIN = """
+ROLE:
+You are an expert in scientometrics and text mining, with experience in co-word 
+and tech-mining studies.
+
+
+CONTEXT:
+This task is part of a process to refine the thesaurus for co-word and 
+tech-mining analysis in the core area <<{core_area}>>. The provided terms are 
+NOT conceptual synonyms—meaning they refer to different concepts or ideas in 
+the provided context phrases and core area.
+
+Important extraction assumption:
+-   The corpus has been pre-processed to extract meaningful terms 
+    (noun phrases / keywords).
+-   Both terms have co-word occurrences greater than zero, indicating they are 
+    present together in at least one document.    
+-   Multi-word terms are indexed separately from their headwords. 
+    Therefore, phrases for an isolated term do not include occurrences that 
+    belong to its multi-word variants (e.g., phrases for "value" exclude
     "market value", "net present value", etc.).
 
 
 TASK:
-Write a paragraph explaining why the provided terms are not conceptual synonyms in the context of <<{core_area}>>.
+Write a paragraph explaining why the provided terms are not conceptual synonyms 
+in the context of <<{core_area}>>.
 
 
 REQUERIMENTS:
@@ -133,45 +191,34 @@ CONTEXT PHRASES FOR THE CANDIDATE-TERM:
 
 
 # -----------------------------------------------------------------------------
-def internal__user_input(core_area):
+def internal__user_input(core_area, n_contexts):
 
-    #
-    # Core Area
-    #
+    # -------------------------------------------------------------------------
     if core_area is None:
         answer = colorized_input(". Enter the core area > ").strip()
         if answer == "":
             return None, None, None, None
-    else:
-        answer = colorized_input(f". Enter the core area [{core_area}] > ").strip()
-
-    if answer != "":
         core_area = answer.upper()
 
-    #
-    # Lead Term
-    #
+    # -------------------------------------------------------------------------
+    if n_contexts is None:
+        n_contexts = colorized_input(
+            ". Enter the number of contexts [default: 30] > "
+        ).strip()
+        if n_contexts == "":
+            n_contexts = 30
+        else:
+            n_contexts = int(n_contexts)
+
+    # -------------------------------------------------------------------------
     lead_term = colorized_input(". Enter the lead term > ").strip()
     if lead_term == "":
         return None, None, None, None
 
-    #
-    # Candidate Term
-    #
+    # -------------------------------------------------------------------------
     candidate_term = colorized_input(". Enter the candidate term > ").strip()
     if candidate_term == "":
         return None, None, None, None
-
-    #
-    # Number of Contexts
-    #
-    n_contexts = colorized_input(
-        ". Enter the number of contexts [default: 30] > "
-    ).strip()
-    if n_contexts == "":
-        n_contexts = 30
-    else:
-        n_contexts = int(n_contexts)
 
     return core_area, lead_term, candidate_term, n_contexts
 
@@ -182,9 +229,10 @@ def internal__filter_contexts(
     contexts_lead_term,
     contexts_candidate_term,
 ):
-
-    contexts_lead_term = contexts_lead_term[:n_contexts]
-    contexts_candidate_term = contexts_candidate_term[:n_contexts]
+    if contexts_lead_term:
+        contexts_lead_term = contexts_lead_term[:n_contexts]
+    if contexts_candidate_term:
+        contexts_candidate_term = contexts_candidate_term[:n_contexts]
 
     return contexts_lead_term, contexts_candidate_term
 
@@ -214,8 +262,12 @@ def internal__get_contexts(pattern, n_contexts):
         contexts = [c for c in contexts if len(c) > 80]
         contexts = [f"- {c} ." for c in contexts]
         contexts = [c.lower().replace("_", " ") for c in contexts]
+        contexts = [c for c in contexts if pattern in c]
 
         complete_contexts.extend(contexts)
+
+        if len(complete_contexts) >= n_contexts:
+            break
 
     pattern = pattern.lower().replace("_", " ")
     complete_contexts = [c for c in complete_contexts if pattern in c]
@@ -227,7 +279,7 @@ def internal__get_contexts(pattern, n_contexts):
 
 
 # -----------------------------------------------------------------------------
-def internal__execute_query(
+def internal__execute_query_with_contexts(
     core_area,
     lead_term,
     candidate_term,
@@ -237,17 +289,18 @@ def internal__execute_query(
 
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    query = PROMPT.format(
+    if isinstance(contexts_lead, list):
+        contexts_lead = "\n".join(contexts_lead)
+    if isinstance(contexts_candidate, list):
+        contexts_candidate = "\n".join(contexts_candidate)
+
+    query = PROMPT_WITH_CONTEXTS.format(
         lead_term=lead_term,
         candidate_term=candidate_term,
-        contexts_lead="\n".join(contexts_lead),
-        contexts_candidate="\n".join(contexts_candidate),
+        contexts_lead=contexts_lead,
+        contexts_candidate=contexts_candidate,
         core_area=core_area,
     )
-
-    # print()
-    # print(query)
-    # print()
 
     answer = []
 
@@ -274,6 +327,47 @@ def internal__execute_query(
         print()
         print(f"Error processing the query: {e}")
         print()
+        return None
+
+
+# -----------------------------------------------------------------------------
+def internal__execute_query_without_contexts(
+    core_area,
+    lead_term,
+    candidate_term,
+):
+
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    query = PROMPT_WITHOUT_CONTEXTS.format(
+        lead_term=lead_term,
+        candidate_term=candidate_term,
+        core_area=core_area,
+    )
+
+    answer = []
+
+    try:
+
+        for _ in range(3):
+            response = client.responses.create(
+                model="gpt-4o",
+                input=query,
+                temperature=0,
+            )
+
+            response = response.output_text
+            response = response.strip().lower()
+            answer.append(response)
+
+        yes_count = answer.count("yes")
+
+        if yes_count < 3:
+            return "no"
+        return "yes"
+
+    except openai.OpenAIError as e:
+        print(f"Error processing the query: {e}")
         return None
 
 
@@ -335,11 +429,21 @@ def internal__explain(
 
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+    if contexts_lead:
+        contexts_lead = "\n".join(contexts_lead)
+    else:
+        contexts_lead = "N/A"
+
+    if contexts_candidate:
+        contexts_candidate = "\n".join(contexts_candidate)
+    else:
+        contexts_candidate = "N/A"
+
     query = EXPLAIN.format(
         lead_term=lead_term,
         candidate_term=candidate_term,
-        contexts_lead="\n".join(contexts_lead),
-        contexts_candidate="\n".join(contexts_candidate),
+        contexts_lead=contexts_lead,
+        contexts_candidate=contexts_candidate,
         core_area=core_area,
     )
 
@@ -372,11 +476,12 @@ def execute_synonyms_command():
 
     print()
     core_area = None
+    n_contexts = None
 
     while True:
 
         core_area, lead_term, candidate_term, n_contexts = internal__user_input(
-            core_area
+            core_area, n_contexts
         )
 
         print()
@@ -389,23 +494,37 @@ def execute_synonyms_command():
         internal__print("  Building contexts for the lead term...")
         contexts_lead = internal__get_contexts(lead_term, n_contexts)
         if not contexts_lead:
-            internal__print_answer("na-lead")
-            continue
+            internal__print(
+                "  No sufficient contextual information found for the lead term."
+            )
 
-        internal__print("  Building contexts for the candidate term...")
-        contexts_candidate = internal__get_contexts(candidate_term, n_contexts)
-        if not contexts_candidate:
-            internal__print_answer("na-candidate")
-            continue
-
-        contexts_lead, contexts_candidate = internal__filter_contexts(
-            n_contexts, contexts_lead, contexts_candidate
-        )
+        if contexts_lead:
+            internal__print("  Building contexts for the candidate term...")
+            contexts_candidate = internal__get_contexts(candidate_term, n_contexts)
+            if not contexts_candidate:
+                internal__print(
+                    "  No sufficient contextual information found for the candidate term."
+                )
+        else:
+            contexts_candidate = None
 
         internal__print("  Executing the query...")
-        answer = internal__execute_query(
-            core_area, lead_term, candidate_term, contexts_lead, contexts_candidate
-        )
+        if not contexts_lead and not contexts_candidate:
+            answer = internal__execute_query_without_contexts(
+                core_area, lead_term, candidate_term
+            )
+        else:
+            contexts_lead, contexts_candidate = internal__filter_contexts(
+                n_contexts, contexts_lead, contexts_candidate
+            )
+            if not contexts_lead:
+                contexts_lead = "N/A"
+            if not contexts_candidate:
+                contexts_candidate = "N/A"
+            answer = internal__execute_query_with_contexts(
+                core_area, lead_term, candidate_term, contexts_lead, contexts_candidate
+            )
+
         internal__print("  Evaluation process completed successfully.")
 
         internal__print_answer(answer)
