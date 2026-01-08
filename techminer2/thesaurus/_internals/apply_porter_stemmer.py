9edf5fb7f4cb4ad7888ced7916d37ab289e68e05
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-01-08 15:46:50 -0500

    Update apply_porter_stemmer.py

--- techminer2/thesaurus/_internals/apply_porter_stemmer.py
+++ techminer2/thesaurus/_internals/apply_porter_stemmer.py
@@ -1,5 +1,4 @@
-"""Porter stemmer."""
-from functools import lru_cache  # type: ignore
+from functools import lru_cache
 
 from nltk.stem import PorterStemmer  # type: ignore
 
@@ -7,5 +6,6 @@ stemmer = PorterStemmer()
 
 
 @lru_cache(maxsize=None)
-def internal__apply_porter_stemmer(word):
+def internal__apply_porter_stemmer(word: str) -> str:
+    """Apply Porter stemming algorithm with caching."""
     return stemmer.stem(word)
