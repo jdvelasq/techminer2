dd44a2ada6893a8173d1fe9ac285fc59b60dcf69
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-02-03 15:09:13 -0500

    Extract noun phrases class

--- techminer2/review/__init__.py
+++ techminer2/review/__init__.py
@@ -1,7 +1,9 @@
 from .extract_abstract_suffixes import ExtractAbstractSuffixes
 from .extract_colon_phrases import ExtractColonPhrases
+from .extract_noun_phrases import ExtractNounPhrases
 
 __all__ = [
     "ExtractColonPhrases",
     "ExtractAbstractSuffixes",
+    "ExtractNounPhrases",
 ]
