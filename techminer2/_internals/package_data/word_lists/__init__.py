e94338f06efde85477b0c481435b5dc3486f75a0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-02-17 23:37:27 -0500

    update

---
+++ techminer2/_internals/package_data/word_lists/__init__.py
@@ -0,0 +1,7 @@
+from .load_word_list import load_word_list
+from .save_text_processing_terms import save_text_processing_terms
+
+__all__ = [
+    "load_word_list",
+    "save_text_processing_terms",
+]
