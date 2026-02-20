9ad872b90ad3fb7baa9f6e3ff35fa9208d5f9a66
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-02-20 08:14:35 -0500

    Update __init__.py

--- techminer2/refine/descriptors/__init__.py
+++ techminer2/refine/descriptors/__init__.py
@@ -1,8 +1,8 @@
 from .create_thesaurus import CreateThesaurus
 from .fuzzy_cutoff_0_word_match import FuzzyCutoffZeroWordMatch
 from .fuzzy_cutoff_1_word_match import FuzzyCutoffOneWordMatch
-from .pre_process_thesaurus import PreProcessThesaurus
-from .starts_with_match import StartsWithMatch
+from .preprocess_thesaurus import PreProcessThesaurus
+from .startswith_match import StartsWithMatch
 
 __all__ = [
     "StartsWithMatch",
