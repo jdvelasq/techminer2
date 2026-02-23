9f34e7cadee8392cfd9b72f55dc7b44317cc1f67
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-02-22 22:16:33 -0500

    Update __init__.py

--- techminer2/_internals/package_data/word_lists/__init__.py
+++ techminer2/_internals/package_data/word_lists/__init__.py
@@ -2,7 +2,6 @@ from .load_builtin_word_list import load_builtin_word_list
 from .save_text_processing_terms import save_text_processing_terms
 
 __all__ = [
-    "add_new_words_to_builtin_word_list",
     "load_builtin_word_list",
     "save_text_processing_terms",
 ]
