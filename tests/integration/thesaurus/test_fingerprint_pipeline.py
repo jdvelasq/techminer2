e9aaf54db66e8137c0635a4fa98cb35b8f0658db
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-02-22 22:17:58 -0500

    clean tests

--- tests/integration/thesaurus/test_fingerprint_pipeline.py
+++
@@ -1,8 +0,0 @@
-from techminer2.refine.thesaurus_old._internals.create_fingerprint import (
-    internal__create_fingerprint,
-)
-
-
-def test_full_pipeline():
-    result = internal__create_fingerprint("MACHINE_LEARNING")
-    assert result == "learning machine"
