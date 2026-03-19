f13a56ef4b1f9ccc5e5a761116d3c52eb02fdc2c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-03-18 21:01:19 -0500

    delete previous versions

--- tm2p/ingest/datab/_intern/phases/p90_auth/s01_auth_full_name_openalex.py
+++
@@ -1,5 +0,0 @@
-# pylint: disable=unused-argument
-
-
-def s01_auth_full_name_openalex(root_directory: str) -> int:
-    return 0
--- tm2p/ingest/datab/_intern/phases/p90_auth/s96_orcid_scopus.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=unused-argument
-# from tm2p._intern.data_access import load_main_csv_zip, save_main_csv_zip
-# from tm2p.enum import Field
-
-
-def s04_orcid_scopus(root_directory: str) -> int:
-
-    return 0
