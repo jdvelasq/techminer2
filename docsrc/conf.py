e9fa87900ea85c21cf90fc51ee961674c68c4f69
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-03-31 11:30:48 -0500

    customize left bar in furo

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -171,6 +171,10 @@ html_title = """TechMiner 2+<br>
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static", "_generated"]
 
+html_css_files = [
+    "custom.css",
+]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
