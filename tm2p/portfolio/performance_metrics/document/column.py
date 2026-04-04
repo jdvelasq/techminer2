0b6a29b5a2fb904cb9e6d94125ae0881cd063520
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-04 00:04:36 -0500

    Create column.py

---
+++ tm2p/portfolio/performance_metrics/document/column.py
@@ -0,0 +1,8 @@
+from tm2p.enum import Field
+
+AUTH = Field.AUTH_NORM.value
+GCS = Field.GCS.value
+LCS = Field.LCS.value
+REC_ID = Field.REC_ID.value
+TITLE = Field.TITLE_RAW.value
+YEAR = Field.YEAR.value
