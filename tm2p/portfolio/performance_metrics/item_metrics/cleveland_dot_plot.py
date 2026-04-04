c5e0e56aa8d17105f1f6c2c53355b9d0ec2496a9
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-04 00:05:08 -0500

    Create cleveland_dot_plot.py

---
+++ tm2p/portfolio/performance_metrics/item_metrics/cleveland_dot_plot.py
@@ -0,0 +1,64 @@
+"""
+ClevelandDotPlot
+===============================================================================
+
+.. raw:: html
+
+    <iframe src="../_generated/px.portfolio.performance_metrics.item_metrics.cleveland_dot_plot.html"
+    height="600px" width="100%" frameBorder="0"></iframe>
+
+Smoke tests:
+    >>> from tm2p import Field, ItemOrderBy
+    >>> from tm2p.portfolio.performance_metrics.item_metrics import ClevelandDotPlot
+    >>> plot = (
+    ...     ClevelandDotPlot()
+    ...     #
+    ...     # FIELD:
+    ...     .with_source_field(Field.AUTHKW_NORM)
+    ...     #
+    ...     # TERMS:
+    ...     .having_items_in_top(10)
+    ...     .having_items_ordered_by(ItemOrderBy.OCC)
+    ...     .having_item_occurrences_between(None, None)
+    ...     .having_item_citations_between(None, None)
+    ...     .having_items_in(None)
+    ...     #
+    ...     # PLOT:
+    ...     .using_title_text("Cleveland Dot Plot")
+    ...     .using_xaxes_title_text("Author Keywords")
+    ...     .using_yaxes_title_text("OCC")
+    ...     #
+    ...     # DATABASE:
+    ...     .where_root_directory("tests/scopus/")
+    ...     .where_record_years_range(None, None)
+    ...     .where_record_citations_range(None, None)
+    ...     #
+    ...     .run()
+    ... )
+    >>> type(plot).__name__
+    'Figure'
+    >>> plot.write_html("docsrc/_generated/px.portfolio.performance_metrics.item_metrics.cleveland_dot_plot.html")
+
+"""
+
+from tm2p._intern import ParamsMixin
+from tm2p._intern.plot.cleveland_dot_plot import cleveland_dot_plot
+
+from .metrics import Metrics
+
+
+class ClevelandDotPlot(
+    ParamsMixin,
+):
+    """:meta private:"""
+
+    def run(self):
+
+        df = Metrics().update(**self.params.__dict__).run()
+        fig = cleveland_dot_plot(params=self.params, dataframe=df)
+
+        return fig
+
+
+#
+#
