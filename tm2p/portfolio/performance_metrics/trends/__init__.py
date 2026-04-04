7ea66057d161c3fa92994076e38d5a7007c7d35d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-03 23:59:57 -0500

    Create __init__.py

---
+++ tm2p/portfolio/performance_metrics/trends/__init__.py
@@ -0,0 +1,11 @@
+from .cumulative_trends import CumulativeTrends
+from .cumulative_trends_plot import CumulativeTrendsPlot
+from .gantt_plot import GanttPlot
+from .trends import Trends
+
+__all__ = [
+    "CumulativeTrends",
+    "CumulativeTrendsPlot",
+    "GanttPlot",
+    "Trends",
+]
