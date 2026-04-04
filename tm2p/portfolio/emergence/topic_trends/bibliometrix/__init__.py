65b0495b4eae817c8c9edc1158f2d90c8a0eb07b
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-04 00:02:07 -0500

    Create __init__.py

---
+++ tm2p/portfolio/emergence/topic_trends/bibliometrix/__init__.py
@@ -0,0 +1,11 @@
+from tm2p.portfolio.emergence.topic_trends.bibliometrix.topic_dynamics import (
+    TopicDynamics,
+)
+from tm2p.portfolio.emergence.topic_trends.bibliometrix.topic_dynamics_plot import (
+    TopicDynamicsPlot,
+)
+
+__all__ = [
+    "TopicDynamicsPlot",
+    "TopicDynamics",
+]
