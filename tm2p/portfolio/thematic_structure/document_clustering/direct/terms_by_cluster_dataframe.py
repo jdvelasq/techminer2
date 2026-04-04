af39a98e0ffa62ad6705959a16bbaa506283be90
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-03 23:56:49 -0500

    Create terms_by_cluster_dataframe.py

---
+++ tm2p/portfolio/thematic_structure/document_clustering/direct/terms_by_cluster_dataframe.py
@@ -0,0 +1,77 @@
+"""
+Terms by Cluster Dataframe
+===============================================================================
+
+
+Smoke tests:
+    >>> from sklearn.cluster import KMeans
+    >>> kmeans = KMeans(
+    ...     n_clusters=4,
+    ...     init="k-means++",
+    ...     n_init=10,
+    ...     max_iter=300,
+    ...     tol=0.0001,
+    ...     algorithm="lloyd",
+    ...     random_state=0,
+    ... )
+    >>> from tm2p.packages.document_clustering import TermsByClusterDataFrame
+    >>> df = (
+    ...     TermsByClusterDataFrame()
+    ...     #
+    ...     # FIELD:
+    ...     .with_field("raw_keywords")
+    ...     .having_items_in_top(50)
+    ...     .having_items_ordered_by("OCC")
+    ...     .having_item_occurrences_between(None, None)
+    ...     .having_item_citations_between(None, None)
+    ...     .having_items_in(None)
+    ...     #
+    ...     # COUNTERS:
+    ...     .using_counters(True)
+    ...     #
+    ...     # TFIDF:
+    ...     .using_binary_item_frequencies(False)
+    ...     .using_row_normalization(None)
+    ...     .using_idf_reweighting(False)
+    ...     .using_idf_weights_smoothing(False)
+    ...     .using_sublinear_tf_scaling(False)
+    ...     #
+    ...     # CLUSTERING:
+    ...     .using_clustering_algorithm_or_dict(kmeans)
+    ...     #
+    ...     # DATABASE:
+    ...     .where_root_directory("tests/scopus/")
+    ...     .where_database("main")
+    ...     .where_record_years_range(None, None)
+    ...     .where_record_citations_range(None, None)
+    ...     .where_records_match(None)
+    ...     #
+    ...     .run()
+    ... )
+    >>> df.head(10)
+
+
+"""
+
+import pandas as pd  # type: ignore
+
+from tm2p._intern import ParamsMixin
+from tm2p.portfolio.thematic_structure.document_clustering.direct.clusters_to_terms_mapping import (
+    ClustersToTermsMapping,
+)
+
+
+class TermsByClusterDataFrame(
+    ParamsMixin,
+):
+    """:meta private:"""
+
+    def run(self):
+
+        mapping = ClustersToTermsMapping().update(**self.params.__dict__).run()
+        frame = pd.DataFrame.from_dict(mapping, orient="index").T
+        frame = frame.fillna("")
+        frame = frame.sort_index(axis=1)
+        return frame
+        frame = frame.sort_index(axis=1)
+        return frame
