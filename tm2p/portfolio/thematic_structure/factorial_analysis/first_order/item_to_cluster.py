f4c3b4b7be62eb44de19693627d3088292189611
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-03 23:55:58 -0500

    Update item_to_cluster.py

--- tm2p/portfolio/thematic_structure/factorial_analysis/first_order/item_to_cluster.py
+++ tm2p/portfolio/thematic_structure/factorial_analysis/first_order/item_to_cluster.py
@@ -15,7 +15,7 @@ Smoke test:
     ...     random_state=0,
     ... )
     >>> from tm2p import Field, ItemOrderBy
-    >>> from tm2p.papers.thematic.first_order_factors import ItemToCluster
+    >>> from tm2p.portfolio.thematic_structure.factorial_analysis.first_order import ItemToCluster
     >>> mapping = (
     ...     ItemToCluster()
     ...     #
