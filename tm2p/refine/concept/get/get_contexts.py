c9bbed08d4fd89ec0a74dde4029f778f0dd9e643
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-03 23:55:23 -0500

    Update get_contexts.py

--- tm2p/refine/concept/get/get_contexts.py
+++ tm2p/refine/concept/get/get_contexts.py
@@ -31,9 +31,7 @@ Smoke tests:
 """
 
 from tm2p._intern import ParamsMixin
-from tm2p.portfolio.thematic_structure.co_occurrence.concordance import (
-    SentenceConcordance,
-)
+from tm2p.explore.concordance import SentenceConcordance
 
 
 class GetContexts(
