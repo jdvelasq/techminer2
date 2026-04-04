03fc98fb049dc24e880cd6b66f3b46fee83562c3
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-03 23:57:51 -0500

    Update bubble_plot.py

--- tm2p/portfolio/thematic_structure/co_occurrence/matrix/bubble_plot.py
+++ tm2p/portfolio/thematic_structure/co_occurrence/matrix/bubble_plot.py
@@ -4,12 +4,12 @@ BubbleChart
 
 .. raw:: html
 
-    <iframe src="../_generated/px.discov.co_occur_matrix.bubble_plot.html"
+    <iframe src="../_generated/px.portfolio.thematic_structure.co_occurrence.matrix.bubble_plot.html"
     height="600px" width="100%" frameBorder="0"></iframe>
 
 Smoke tests:
     >>> from tm2p import Field, ItemOrderBy
-    >>> from tm2p.discover.co_occurrence import BubblePlot
+    >>> from tm2p.portfolio.thematic_structure.co_occurrence.matrix import BubblePlot
     >>> fig = (
     ...     BubblePlot()
     ...     #
@@ -38,7 +38,7 @@ Smoke tests:
     ... )
     >>> type(fig).__name__
     'Figure'
-    >>> fig.write_html("docsrc/_generated/px.discov.co_occur_matrix.bubble_plot.html")
+    >>> fig.write_html("docsrc/_generated/px.portfolio.thematic_structure.co_occurrence.matrix.bubble_plot.html")
 
 
 """
