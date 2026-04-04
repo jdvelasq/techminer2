c829e9d507f6ea12aacaa198990cbb4e3ca001a1
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-04 00:03:29 -0500

    Delete __init__.py

--- tm2p/portfolio/performance_mapping/lotka/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Public API."""
-
-from tm2p.portfolio.performance_mapping.lotka.distribution import Distribution
-from tm2p.portfolio.performance_mapping.lotka.distribution_plot import DistributionPlot
-
-__all__ = [
-    "Distribution",
-    "DistributionPlot",
-]
