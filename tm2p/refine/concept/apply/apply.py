898afeb07b3f7b91403c1f71bad698168c0f4167
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2026-04-05 20:57:47 -0500

    update

--- tm2p/refine/concept/apply/apply.py
+++ tm2p/refine/concept/apply/apply.py
@@ -15,7 +15,7 @@ Smoke tests:
 import sys
 
 from tm2p._intern import ParamsMixin
-from tm2p.enum import Field, ThFile
+from tm2p._intern.enum import Field, ThFile
 from tm2p.refine._intern.apply import BaseApply
 
 
